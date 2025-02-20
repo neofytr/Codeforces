da09e4222b351963fc9015ef6e23be5cf9172dc4
neofytr <imagination12344321.com@gmail.com>
2025-02-20 23:04:39 +0530

    preparing olympiad

--- 1400/preparing_olympiad.c
+++ 1400/preparing_olympiad.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 int main()
 {
@@ -20,24 +21,26 @@ int main()
     // - x: minimum difference between hardest and easiest
     // - difficulties[]: array containing all problem difficulties
 
+    int ways = 0;
+
     for (int set = 0; set < (1 << n); set++)
     {
-        if (set == 1 || !(set & (set - 1)))
+        if (!(set & (set - 1)) || !set) // check if set is a power of two; since at least two problems are required
         {
             continue;
         }
         int difficulty = 0;
         int exceeded = false;
-        int min = difficulties[0];
-        int max = difficulties[0];
+        int min = INT32_MAX;
+        int max = INT32_MIN;
         for (int test = 0; test < n; test++)
         {
             difficulty += ((1 << test) & set) ? difficulties[test] : 0;
-            if (difficulties[test] < min)
+            if (((1 << test) & set) && difficulties[test] < min)
             {
                 min = difficulties[test];
             }
-            if (difficulties[test] > max)
+            if (((1 << test) & set) && difficulties[test] > max)
             {
                 max = difficulties[test];
             }
@@ -48,10 +51,12 @@ int main()
             }
         }
 
-        if (!exceeded && difficulty &&)
+        if (!exceeded && difficulty <= r && difficulty >= l && (max - min) >= x)
         {
+            ways++;
         }
     }
 
+    fprintf(stdout, "%d\n", ways);
     return 0;
 }
