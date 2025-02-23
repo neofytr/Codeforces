f83fb99f7060feb844e60eb65bb9d71fcddea584
neofytr <imagination12344321.com@gmail.com>
2025-02-23 22:04:05 +0530

    first 900 prob

--- 900/clockwork.cpp
+++ 900/clockwork.cpp
@@ -6,40 +6,33 @@
 
 using namespace std;
 
-void simulate_second(vector<int64_t> &state)
-{
-    for (int64_t &value : state)
-    {
-        value--;
-    }
-}
-
-bool is_the_step_correct(vector<int64_t> &state)
-{
-    for (int64_t &value : state)
-    {
-        if (value <= 0)
-        {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 int main()
 {
-    int64_t t, n;
-    scanf("%zu", &t);
+    int64_t t;
+    scanf("%ld", &t);
 
     for (int64_t counter = 0; counter < t; counter++)
     {
-        scanf("%zu", &n);
+        int64_t n;
+        scanf("%ld", &n);
         vector<int64_t> init_state(n);
         for (int64_t &value : init_state)
         {
-            scanf("%zu", &value);
+            scanf("%ld", &value);
         }
+
+        bool possible = true;
+
+        for (size_t index = 0; index < n; index++)
+        {
+            if (init_state[index] <= 2 * max(index, n - 1 - index))
+            {
+                possible = false;
+                break;
+            }
+        }
+
+        printf("%s\n", possible ? "YES" : "NO");
     }
     return EXIT_SUCCESS;
 }
