9b400395b03df56cf477f5e63d32ce1ea24d51bd
neofytr <imagination12344321.com@gmail.com>
2025-02-24 13:33:51 +0530

    colored marbles completed

--- 900/colored_marbles.cpp
+++ 900/colored_marbles.cpp
@@ -1,23 +1,91 @@
 #include <iostream>
-#include <stdlib.h>
 #include <vector>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
+int solve(vector<int> &marbles)
+{
+    int n = marbles.size();
+
+    unordered_map<int, int> freq;
+    for (int x : marbles)
+    {
+        freq[x]++;
+    }
+
+    map<int, vector<int>> freq_to_colors;
+    for (auto &[color, count] : freq)
+    {
+        freq_to_colors[count].push_back(color);
+    }
+
+    int score = 0;
+    bool aliceTurn = true; // true for Alice's turn, false for Bob's
+    int remaining_moves = n;
+
+    for (auto &[frequency, colors] : freq_to_colors)
+    {
+        for (int color : colors)
+        {
+            if (frequency == 1)
+            {
+                if (aliceTurn)
+                {
+                    score += 2;
+                }
+                aliceTurn = !aliceTurn;
+                remaining_moves--;
+            }
+            else
+            {
+                int marbles_alice_can_get;
+                if (aliceTurn)
+                {
+                    marbles_alice_can_get = (frequency + 1) / 2;
+                }
+                else
+                {
+                    marbles_alice_can_get = frequency / 2;
+                }
+
+                if (marbles_alice_can_get > 0)
+                {
+                    score += 1;
+                    if (marbles_alice_can_get == frequency)
+                    {
+                        score += 1;
+                    }
+                }
+
+                aliceTurn = ((remaining_moves - frequency) % 2 == 0) ? aliceTurn : !aliceTurn;
+                remaining_moves -= frequency;
+            }
+        }
+    }
+
+    return score;
+}
+
 int main()
 {
-    int t, n;
+    int t;
     cin >> t;
 
-    for (int counter = 0; counter < t; counter++)
+    for (int test_case = 0; test_case < t; test_case++)
     {
+        int n;
         cin >> n;
         vector<int> marbles(n);
-        for (int &value : marbles)
+        for (int &x : marbles)
         {
-            cin >> value;
+            cin >> x;
         }
 
-        
+        cout << solve(marbles) << endl;
     }
+
+    return 0;
 }
