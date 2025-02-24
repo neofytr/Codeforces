a56aa927e505652cbc60654ca8ed4a64e18c4532
neofytr <imagination12344321.com@gmail.com>
2025-02-24 21:52:26 +0530

    tres

--- 900/everyone_loves_tres.cpp
+++ 900/everyone_loves_tres.cpp
@@ -3,3 +3,27 @@
 #include <vector>
 
 using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+
+    while (t--)
+    {
+        cin >> n;
+        vector<int> num(n);
+
+        num[n - 1] = 6;
+
+        if (n == 1)
+        {
+            cout << 6 << endl;
+        }
+
+        for (int counter = 0; counter < n - 1; counter++)
+        {
+            
+        }
+    }
+}
