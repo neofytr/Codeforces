e99501e27dc51b32c1c55ad2fa897db7f294f7fe
neofytr <imagination12344321.com@gmail.com>
2025-02-24 13:12:13 +0530

    colored marbles

---
+++ 900/colored_marbles.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+
+    for (int counter = 0; counter < t; counter++)
+    {
+        cin >> n;
+        vector<int> marbles(n);
+        for (int &value : marbles)
+        {
+            cin >> value;
+        }
+
+        
+    }
+}
