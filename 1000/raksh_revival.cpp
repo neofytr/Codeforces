36ab4bb4c82e0d253f44651d77a4afa83906dd68
neofytr <imagination12344321.com@gmail.com>
2025-02-25 00:37:04 +0530

    another 1000; raksh revival

--- 1000/raksh_revival.cpp
+++ 1000/raksh_revival.cpp
@@ -12,11 +12,49 @@ int main()
     while (t--)
     {
         cin >> n >> m >> k;
-        vector<int> body(n);
 
-        for (int &val : body)
+        string s;
+        cin >> s;
+
+        vector<int> body(n);
+        for (int i = 0; i < n; i++)
         {
-            cin >> val;
+            body[i] = s[i] - '0';
         }
+
+        int timar = 0;
+        int index = 0;
+        int start = -1;
+
+        while (index < n)
+        {
+            if (!body[index] && start == -1)
+            {
+                start = index;
+            }
+
+            if (body[index] && index - start < m && start != -1)
+            {
+                start = -1;
+            }
+
+            if (!body[index] && index - start + 1 == m && start != -1)
+            {
+
+                timar++;
+                for (int counter = index; counter < index + k && counter < n; counter++)
+                {
+                    body[counter] = 1;
+                }
+
+                start = -1;
+                index += k;
+                continue;
+            }
+
+            index++;
+        }
+
+        cout << timar << endl;
     }
 }
