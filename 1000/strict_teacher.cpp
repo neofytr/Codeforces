7dd17f5fdb804dd73003aa69cbdce7f8e86feee4
neofytr <imagination12344321.com@gmail.com>
2025-02-25 16:43:22 +0530

    strict teach

--- 1000/strict_teacher.cpp
+++ 1000/strict_teacher.cpp
@@ -6,121 +6,78 @@ using namespace std;
     cin.tie(nullptr);
 
 using ll = long long;
-using ld = long double;
 using vi = vector<int>;
-using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define rev(i, a, b) for (int i = (a); i >= (b); --i)
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-#ifdef LOCAL
-#define debug(x) cerr << #x << " = " << (x) << endl;
-#else
-#define debug(x)
-#endif
-
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9 + 7;
-
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-
-ll power(ll a, ll b, ll m = MOD)
-{
-    ll res = 1;
-    while (b)
-    {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
-
-bool is_caught(int t_one, int t_two, int david)
-{
-    return abs(t_one - david) == 1 || abs(t_two - david) == 1;
-}
 
 void solve()
 {
     int n, m, q;
-
     cin >> n >> m >> q;
 
-    int t_one_curr, t_two_curr, david_curr;
+    int t1, t2, d;
+    cin >> t1 >> t2 >> d;
 
-    cin >> t_one_curr >> t_two_curr;
-    cin >> david_curr;
+    int a = max(t1, t2); // rightmost teacher
+    int b = min(t1, t2); // leftmost teacher
 
-    int moves = 0;
-
-    do
+    if (d < b)
     {
-        // ideal david move
-        // increase his distance from the closest teacher, or don't move if you can't
+        // David is before the leftmost teacher
+        // David will eventually be stuck at position 1
+        // The teacher will reach him in (b - 1) moves
+        cout << b - 1 << endl;
+    }
+    else if (d > a)
+    {
+        // David is after the rightmost teacher
+        // David will eventually be stuck at position n
+        // The teacher will reach him in (n - a) moves
+        cout << n - a << endl;
+    }
+    else if (d >= b && d <= a)
+    {
+        // David is somewhere between the two teachers (or at one of their positions)
 
-        int dist_one = t_one_curr - david_curr;
-        int dist_two = t_two_curr - david_curr;
+        // Proof by contradiction that David will be caught at the middle point (a+b)/2:
+        // Let's define the middle point k = (a+b)/2
+        int k = (a + b) / 2;
 
-        if (abs(dist_one) > abs(dist_two))
-        {
-            if (dist_one > 0 && david_curr)
-            {
-                david_curr--;
-            }
-            else if (dist_one < 0 && david_curr != n - 1)
-            {
-                david_curr++;
-            }
-        }
-        else if (abs(dist_one) < abs(dist_two))
-        {
-            if (dist_two > 0 && david_curr)
-            {
-                david_curr--;
-            }
-            else if (dist_two < 0 && david_curr != n - 1)
-            {
-                david_curr++;
-            }
-        }
+        // Assumption: David is not caught at point k
+        // If this is true, then David must be caught either to the left or to the right of k
 
-        if (dist_one > 0)
-        {
-            t_one_curr--;
-        }
-        else if (dist_one < 0)
-        {
-            t_one_curr++;
-        }
+        // Case 1: Suppose David is caught to the left of k (at some position p where b ≤ p < k)
+        // This would mean teacher b caught David at position p
+        // For this to happen, David must have been at position p while teacher b was at position p+1
+        // But if David plays optimally, he would have moved right in this situation
+        // Since the rightmost teacher a is still far enough away (as a > k > p)
+        // This contradicts our assumption that David was caught at position p < k
 
-        if (dist_two > 0)
+        // Case 2: Suppose David is caught to the right of k (at some position p where k < p ≤ a)
+        // This would mean teacher a caught David at position p
+        // For this to happen, David must have been at position p while teacher a was at position p-1
+        // But if David plays optimally, he would have moved left in this situation
+        // Since the leftmost teacher b is still far enough away (as b < k < p)
+        // This contradicts our assumption that David was caught at position p > k
+
+        // Since both cases lead to contradiction, our original assumption must be false
+        // Therefore, David will be caught at the middle point k = (a+b)/2
+
+        // The number of moves needed is the minimum of:
+        // 1. How long it takes for teacher a to reach k: (a - k) moves
+        // 2. How long it takes for teacher b to reach k: (k - b) moves
+        int movesToCapture;
+
+        // Special case: if David is already at the same position as one of the teachers
+        if (d == a || d == b)
         {
-            t_two_curr--;
+            movesToCapture = 0;
         }
-        else if (dist_two < 0)
+        else
         {
-            t_two_curr++;
+            movesToCapture = min(a - k, k - b);
         }
 
-        moves++;
-    } while (!is_caught(t_one_curr, t_two_curr, david_curr));
-
-    printf("%d\n", moves);
+        cout << movesToCapture << endl;
+    }
 }
 
 int main()
@@ -128,7 +85,6 @@ int main()
     FAST_IO;
     int t;
     cin >> t;
-
     while (t--)
     {
         solve();
