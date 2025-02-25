6598328acd65dfbe58324d1dae41698341f633d8
neofytr <imagination12344321.com@gmail.com>
2025-02-25 12:48:48 +0530

    scanf

---
+++ test/scanf.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d\n", &N);
+
+    while (N--)
+    {
+        char x[110];
+        scanf("0.[^,...]\n", x);
+    }
+}
