0d0c0597779deb58bf1d0f208ddeef68b01fb575
neofytr <imagination12344321.com@gmail.com>
2025-02-27 22:12:10 +0530

    num of ways

--- problem/num_of_ways.cpp
+++ problem/num_of_ways.cpp
@@ -1,68 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FAST_IO                  \
-    ios::sync_with_stdio(false); \
-    cin.tie(nullptr);
-
-using ll = long long;
-using ld = long double;
-using vi = vector<int>;
-using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define rev(i, a, b) for (int i = (a); i >= (b); --i)
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-#ifdef LOCAL
-#define debug(x) cerr << #x << " = " << (x) << endl;
-#else
-#define debug(x)
-#endif
-
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9 + 7;
-
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-
-ll power(ll a, ll b, ll m = MOD)
+int solve(int s, int e)
 {
-    ll res = 1;
-    while (b)
-    {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
+    if (s == e)
+        return 1;
+    if (s > e)
+        return 0;
 
-void solve()
-{
+    return solve(s + 1, e) + solve(s + 2, e) + solve(s + 3, e);
 }
 
 int main()
 {
-    FAST_IO;
-    int t;
-    cin >> t;
-
-    while (t--)
-    {
-        solve();
-    }
+    int s, e;
+    cin >> s >> e;
+    cout << solve(s, e) << endl;
     return 0;
 }
