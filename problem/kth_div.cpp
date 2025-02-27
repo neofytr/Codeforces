8e894cd3e58a22b401c29acfa6861760738c5737
neofytr <imagination12344321.com@gmail.com>
2025-02-27 23:59:59 +0530

    sort

--- problem/kth_div.cpp
+++ problem/kth_div.cpp
@@ -5,92 +5,32 @@ using namespace std;
     ios::sync_with_stdio(false); \
     cin.tie(nullptr);
 
-using ull = unsigned long long;
 using ll = long long;
-using ld = long double;
-using vi = vector<int>;
-using vull = vector<ull>;
 using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define rev(i, a, b) for (int i = (a); i >= (b); --i)
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-#ifdef LOCAL
-#define debug(x) cerr << #x << " = " << (x) << endl;
-#else
-#define debug(x)
-#endif
-
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9 + 7;
-
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-
-ll power(ll a, ll b, ll m = MOD)
-{
-    ll res = 1;
-    while (b)
-    {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
-
-vull get_divisors(ull n)
-{
-    vull divs;
-    for (ull index = 0; index * index <= n; index++)
-    {
-        if (n % index)
-        {
-            divs.push_back(index);
-            if (n / index != n)
-            {
-                divs.push_back(index);
-            }
-        }
-    }
-
-    return divs;
-}
 
 void solve()
 {
-    size_t n, k;
+    ll n, k;
     cin >> n >> k;
 
-    size_t div_no = 0;
+    vll divisors;
 
-    for (size_t index = 1; index < n; index++)
+    for (ll d = 1; d * d <= n; d++)
     {
-        if (!(n % index))
+        if (n % d == 0)
         {
-            div_no++;
-            if (div_no == k)
-            {
-                cout << index << endl;
-                return;
-            }
+            divisors.push_back(d);
+            if (d != n / d)
+                divisors.push_back(n / d);
         }
     }
 
-    cout << -1 << endl;
+    sort(divisors.begin(), divisors.end());
+
+    if (k > divisors.size())
+        cout << -1 << endl;
+    else
+        cout << divisors[k - 1] << endl;
 }
 
 int main()
