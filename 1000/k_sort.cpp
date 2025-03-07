af06645c55245597e204ba617d5b7d1c93f1fb18
neofytr <imagination12344321.com@gmail.com>
2025-03-08 01:01:34 +0530

    k sort

--- 1000/k_sort.cpp
+++ 1000/k_sort.cpp
@@ -1,64 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int t;
     cin >> t;
-
-    while (t--)
-    {
+    while (t--) {
         int n;
         cin >> n;
-
-        vector<ll> a(n);
-        for (int i = 0; i < n; i++)
-        {
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
             cin >> a[i];
         }
-
-        // Find the target non-decreasing array
-        vector<ll> target = a;
-        for (int i = 1; i < n; i++)
-        {
-            target[i] = max(target[i], target[i - 1]);
+        if (n == 0) {
+            cout << 0 << '\n';
+            continue;
         }
-
-        // Count how many times each position needs to be incremented
-        vector<ll> increments;
-        for (int i = 0; i < n; i++)
-        {
-            ll diff = target[i] - a[i];
-            if (diff > 0)
-            {
-                // Add this position diff times to our list
-                for (ll j = 0; j < diff; j++)
-                {
-                    increments.push_back(i);
-                }
+        long long sum_delta = 0;
+        int max_delta = 0;
+        int adjusted_prev = a[0];
+        for (int i = 1; i < n; ++i) {
+            int required = max(a[i], adjusted_prev);
+            int delta_i = required - a[i];
+            sum_delta += delta_i;
+            if (delta_i > max_delta) {
+                max_delta = delta_i;
             }
+            adjusted_prev = required;
         }
-
-        ll answer = 0;
-
-        // Process in batches - each batch is one operation
-        for (int i = 0; i < increments.size(); i += n)
-        {
-            // For each operation, we take min(n, remaining) positions
-            ll k = min(n, (ll)increments.size() - i);
-            answer += k + 1; // Cost of the operation is k+1
-        }
-
-        cout << answer << "\n";
+        cout << sum_delta + max_delta << '\n';
     }
-
     return 0;
 }
