5ff29b654d724dbff91edfddea206787f96286f8
neofytr <imagination12344321.com@gmail.com>
2025-03-21 20:21:18 +0530

    smthng

--- 1000/raspberries.cpp
+++ 1000/raspberries.cpp
@@ -5,117 +5,103 @@ using namespace std;
     ios::sync_with_stdio(false); \
     cin.tie(nullptr);
 
-using ull = unsigned long long;
-using ll = long long;
-using ld = long double;
-using vi = vector<int>;
-using vull = vector<ull>;
-using vll = vector<ll>;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define pb push_back
-#define eb emplace_back
-#define fi first
-#define se second
-#define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define rev(i, a, b) for (int i = (a); i >= (b); --i)
-#define sz(x) (int)(x).size()
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-
-#ifdef LOCAL
-#define debug(x) cerr << #x << " = " << (x) << endl;
-#else
-#define debug(x)
-#endif
-
-const int INF = 1e9;
-const ll LINF = 1e18;
-const int MOD = 1e9 + 7;
-
-ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-
-ll power(ll a, ll b, ll m = MOD)
-{
-    ll res = 1;
-    while (b)
-    {
-        if (b & 1)
-            res = res * a % m;
-        a = a * a % m;
-        b >>= 1;
-    }
-    return res;
-}
-
-void get_divisors(ull n, vull &divs)
-{
-    for (ull index = 1; index * index <= n; index++)
-    {
-        if (n % index)
-        {
-            divs.push_back(index);
-            if (n / index != index)
-            {
-                divs.push_back(index);
-            }
-        }
-    }
-
-    sort(all(divs));
-}
-
-int compute_product(vector<int> &rem)
-{
-    int mul = 1;
-    int n = rem.size();
-    for (int index = 0; index < n; index++)
-    {
-        mul *= rem[index];
-    }
-}
-
 void solve()
 {
-    int n, k;
+    size_t n, k;
     cin >> n >> k;
 
-    vector<int> num(n);
-    for (int &val : num)
+    vector<size_t> num(n);
+
+    for (size_t &val : num)
     {
         cin >> val;
     }
 
-    vector<int> rem(n);
-    for (int index = 0; index < n; index++)
+    vector<size_t> rem_count(k, 0); // Stores count of each remainder mod k
+
+    for (size_t &val : num)
     {
-        rem[index] = num[index] % k;
+        size_t rem = val % k;
+        rem_count[rem]++;
     }
 
-    if (!compute_product(rem))
+    // If any number is already divisible by k, answer is 0
+    if (rem_count[0] > 0)
     {
         cout << 0 << endl;
+        return;
     }
 
-    for (int index = 0; index < n; index++)
+    // Handling k = 2
+    if (k == 2)
     {
-        rem[index]++;
-        if (!compute_product(rem))
+        cout << 1 << endl;
+        return;
+    }
+
+    // Handling k = 3
+    if (k == 3)
+    {
+        if (rem_count[2] > 0)
         {
             cout << 1 << endl;
         }
-        rem[index]--;
+        else
+        {
+            cout << 2 << endl; // Only 1s exist
+        }
+        return;
+    }
+
+    // Handling k = 4
+    if (k == 4)
+    {
+        if (rem_count[2] >= 2)
+        {
+            cout << 0 << endl;
+            return;
+        }
+        if (rem_count[3] > 0)
+        {
+            cout << 1 << endl;
+            return;
+        }
+        if (rem_count[2] == 1)
+        {
+            cout << 1 << endl;
+            return;
+        }
+        cout << 3 << endl;
+        return;
+    }
+
+    // Handling k = 5
+    if (k == 5)
+    {
+        if (rem_count[4] > 0)
+        {
+            cout << 1 << endl;
+            return;
+        }
+        if (rem_count[3] > 0)
+        {
+            cout << 2 << endl;
+            return;
+        }
+        if (rem_count[2] > 0)
+        {
+            cout << 3 << endl;
+            return;
+        }
+        cout << 4 << endl;
+        return;
     }
 }
 
 int main()
 {
     FAST_IO;
-    int t;
+    size_t t;
     cin >> t;
 
     while (t--)
