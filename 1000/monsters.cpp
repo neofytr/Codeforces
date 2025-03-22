34d0325966f84b71105a1077bec78e61c1590ed4
neofytr <imagination12344321.com@gmail.com>
2025-03-22 21:16:31 +0530

    monsters

--- 1000/monsters.cpp
+++ 1000/monsters.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <set>
 using namespace std;
 
 void solve()
@@ -6,50 +8,35 @@ void solve()
     int64_t n, k;
     cin >> n >> k;
 
-    vector<pair<int64_t, int64_t>> health(n);
+    multiset<pair<int64_t, int64_t>> health;
 
-    int64_t temp;
     for (int64_t index = 0; index < n; index++)
     {
+        int64_t temp;
         cin >> temp;
-        health[index] = {temp, index};
+        health.insert({temp, index});
     }
 
-    sort(health.begin(), health.end(), [](const pair<int64_t, int64_t> &a, const pair<int64_t, int64_t> &b)
-         { return a.first < b.first; });
-
-    while (true)
+    while (!health.empty())
     {
-        if (health[n - 1].first <= 0)
-        {
+        auto it = prev(health.end());
+        int64_t max_health = it->first;
+        int64_t min_index = it->second;
+        health.erase(it);
+
+        if (max_health <= 0)
             break;
-        }
 
-        int64_t max = health[n - 1].first;
-        int64_t index_of_min_index = n - 1;
-        for (int64_t index = n - 1; index >= 0; index--)
+        max_health -= k;
+
+        if (max_health > 0)
         {
-            if (health[index].first == max)
-            {
-                if (health[index].second < health[index_of_min_index].second)
-                {
-                    index_of_min_index = index;
-                }
-            }
-            else
-            {
-                break;
-            }
+            health.insert({max_health, min_index});
         }
-
-        health[index_of_min_index].first -= k;
-        if (health[index_of_min_index].first <= 0)
+        else
         {
-            cout << health[index_of_min_index].second + 1 << " ";
+            cout << min_index + 1 << " ";
         }
-
-        sort(health.begin(), health.end(), [](const pair<int64_t, int64_t> &a, const pair<int64_t, int64_t> &b)
-             { return a.first < b.first; });
     }
 
     cout << endl;
@@ -59,11 +46,9 @@ int main()
 {
     int t;
     cin >> t;
-
     while (t--)
     {
         solve();
     }
-
     return 0;
 }
