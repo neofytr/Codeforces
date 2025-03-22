a916433c3a1102ce35fc7ae40376f61a487538e0
neofytr <imagination12344321.com@gmail.com>
2025-03-22 12:26:59 +0530

    Thinking smthng

--- 1000/two_bin_str.cpp
+++ 1000/two_bin_str.cpp
@@ -8,31 +8,21 @@ void solve()
 
     int len = a.length(); // len >= 2 (given)
 
-    int max_common_zero = 0;
-    for (int index = 1; index < len; index++)
-    {
-        if (a[index] == '0' && b[index] == '0')
-        {
-            max_common_zero = index;
-        }
-    }
+    vector<pair<int, int>> eq; // number, index
 
-    int min_common_one = len - 1;
-    for (int index = len - 2; index >= 0; index--)
+    for (int index = 0; index < len; index++)
     {
-        if (a[index] == '1' && b[index] == '1')
+        if (a[index] == b[index])
         {
-            min_common_one = index;
+            if (a[index] == '0')
+            {
+                eq.push_back(pair(0, index));
+            }
+            else
+            {
+                eq.push_back(pair(1, index));
+            }
         }
-    }
-
-    if ((min_common_one - max_common_zero >= 2))
-    {
-        cout << "no" << endl;
-    }
-    else
-    {
-        cout << "yes" << endl;
     }
 }
 
