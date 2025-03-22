61034026e1295a3379bad464f414bbc1c6b331a9
neofytr <imagination12344321.com@gmail.com>
2025-03-22 21:22:46 +0530

    monsters completed

--- 1000/monsters.cpp
+++ 1000/monsters.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
 
 void solve()
@@ -8,35 +6,61 @@ void solve()
     int64_t n, k;
     cin >> n >> k;
 
-    multiset<pair<int64_t, int64_t>> health;
+    vector<pair<int64_t, int64_t>> health(n);
 
+    int64_t temp;
     for (int64_t index = 0; index < n; index++)
     {
-        int64_t temp;
         cin >> temp;
-        health.insert({temp, index});
+        health[index] = {temp, index};
     }
 
-    while (!health.empty())
+    sort(health.begin(), health.end(), [](const pair<int64_t, int64_t> &a, const pair<int64_t, int64_t> &b)
+         { return a.first < b.first; });
+
+    while (true)
     {
-        auto it = prev(health.end());
-        int64_t max_health = it->first;
-        int64_t min_index = it->second;
-        health.erase(it);
-
-        if (max_health <= 0)
+        if (health[n - 1].first <= 0)
+        {
             break;
-
-        max_health -= k;
-
-        if (max_health > 0)
-        {
-            health.insert({max_health, min_index});
         }
-        else
+
+        int64_t max = health[n - 1].first;
+        int64_t index_of_min_index = n - 1;
+
+        for (int64_t index = n - 1; index >= 0; index--)
         {
-            cout << min_index + 1 << " ";
+            if (health[index].first == max)
+            {
+                if (health[index].second < health[index_of_min_index].second)
+                {
+                    index_of_min_index = index;
+                }
+            }
+            else
+            {
+                break;
+            }
         }
+
+        health[index_of_min_index].first -= k;
+
+        if (health[index_of_min_index].first <= 0)
+        {
+            cout << health[index_of_min_index].second + 1 << " ";
+        }
+
+        // find the right position to insert the modified element
+        pair<int64_t, int64_t> modified = health[index_of_min_index];
+        int64_t pos = index_of_min_index;
+
+        // move the element to its correct position in the sorted array
+        while (pos > 0 && modified.first < health[pos - 1].first)
+        {
+            health[pos] = health[pos - 1];
+            pos--;
+        }
+        health[pos] = modified;
     }
 
     cout << endl;
@@ -46,9 +70,11 @@ int main()
 {
     int t;
     cin >> t;
+
     while (t--)
     {
         solve();
     }
+
     return 0;
 }
