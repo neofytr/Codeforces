62d2149cef6fadcab077cee3b40891345a46c8be
neofytr <imagination12344321.com@gmail.com>
2025-03-22 15:45:39 +0530

    max cost deletion

--- 1000/max_cost_delt.cpp
+++ 1000/max_cost_delt.cpp
@@ -5,147 +5,38 @@ void solve()
 {
     int n, a, b;
     string str;
-
     cin >> n >> a >> b >> str;
-    int len = str.length();
 
-    if (!a)
+    if (b >= 0)
     {
-        cout << b << endl;
+        cout << a * n + b * n << endl;
         return;
     }
 
-    if (a > 0)
+    int zero_segments = 0, one_segments = 0;
+    for (int i = 0; i < n; i++)
     {
-        // a * l + b is an increasing function
-        if (b >= 0)
+        if (i == 0 || str[i] != str[i - 1])
         {
-            // remove everthing one by one
-            // this will give the max points
-
-            cout << a * len + b * len << endl;
-            return;
-        }
-        else
-        {
-            // b is negative
-            // minimize the number of deletions themselves in this case
-
-            int zerocount = 0;
-            int onecount = 0;
-            for (int index = 0; index < len; index++)
-            {
-                if (str[index] == '0')
-                {
-                    zerocount++;
-                }
-                else
-                {
-                    onecount++;
-                }
-            }
-
-            if (onecount > zerocount)
-            {
-                bool found = false;
-                int currlen = 0;
-                vector<int> zeroes;
-
-                for (int index = 0; index < len; index++)
-                {
-                    if (str[index] == '0')
-                    {
-                        if (!found)
-                        {
-                            found = true;
-                            currlen = 1;
-                        }
-                        else
-                        {
-                            currlen++;
-                        }
-                    }
-                    else if (found) // str[index] == '1'
-                    {
-                        zeroes.push_back(currlen);
-                        found = false;
-                        currlen = 0;
-                    }
-                }
-
-                if (found)
-                {
-                    zeroes.push_back(currlen);
-                }
-
-                int pts = 0;
-                pts += (onecount * a + b);
-
-                for (int index = 0; index < zeroes.size(); index++)
-                {
-                    pts += (zeroes[index] * a + b);
-                }
-
-                cout << pts << endl;
-                return;
-            }
-            else if (zerocount > onecount)
-            {
-                bool found = false;
-                int currlen = 0;
-                vector<int> ones;
-
-                for (int index = 0; index < len; index++)
-                {
-                    if (str[index] == '1')
-                    {
-                        if (!found)
-                        {
-                            found = true;
-                            currlen = 1;
-                        }
-                        else
-                        {
-                            currlen++;
-                        }
-                    }
-                    else if (found) // str[index] == '0'
-                    {
-                        ones.push_back(currlen);
-                        found = false;
-                        currlen = 0;
-                    }
-                }
-
-                if (found)
-                {
-                    ones.push_back(currlen);
-                }
-
-                int pts = 0;
-                pts += (zerocount * a + b);
-
-                for (int index = 0; index < ones.size(); index++)
-                {
-                    pts += (ones[index] * a + b);
-                }
-
-                cout << pts << endl;
-                return;
-            }
+            if (str[i] == '0')
+                zero_segments++;
+            else
+                one_segments++;
         }
     }
+
+    int min_segments = min(zero_segments, one_segments);
+    int ans = a * n + b * (min_segments + 1);
+    cout << ans << endl;
 }
 
 int main()
 {
     int t;
     cin >> t;
-
     while (t--)
     {
         solve();
     }
-
     return 0;
 }
