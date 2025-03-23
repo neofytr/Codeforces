7a58489471513b4e6107189a15767c3e9a9630b6
neofytr <imagination12344321.com@gmail.com>
2025-03-23 11:39:53 +0530

    gregarray

---
+++ 1400/gregarr.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/contest/295/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
