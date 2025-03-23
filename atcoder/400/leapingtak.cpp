e492271ddca9040f8d034feaa8f8e2be5b3bc1f0
neofytr <imagination12344321.com@gmail.com>
2025-03-23 15:34:23 +0530

    computing unions over linear set

--- atcoder/400/leapingtak.cpp
+++ atcoder/400/leapingtak.cpp
@@ -8,7 +8,13 @@ void solve()
     int n, k;
     cin >> n >> k;
 
-    vector<int> selections(n + 1);
+    vector<int> selections(n + 1, 0);
+
+    // for computing the union
+    // for each segment, we put 1 in the corresponding segment in the selection
+    // to put the one, we use propagation
+    // since the segments are given to be non-intersecting, the indexes in the final union will
+    // all have exactly 1 as their value; in general, they are positive
 
     int l, r;
     for (int index = 0; index < k; index++)
@@ -17,6 +23,7 @@ void solve()
 
         // convert to zero based index
         l--, r--;
+
         selections[l] += 1;
         selections[r + 1] -= 1;
     }
@@ -27,9 +34,9 @@ void solve()
         selections[index] += selections[index - 1];
     }
 
-    for (int val : selections)
+    for (int index = 0; index < n; index++)
     {
-        cout << val << " ";
+        cout << selections[index] << " ";
     }
 
     cout << endl;
