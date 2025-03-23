39a45d26263a60287ccfb7c2630fbea1fb2e0260
neofytr <imagination12344321.com@gmail.com>
2025-03-23 20:56:41 +0530

    prefsumadd:

---
+++ 1200/prefsumaddicts.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/1738/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
