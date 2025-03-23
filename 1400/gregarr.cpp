cb1f90e1e2fda4e0aa8495d318e9c309ee9015f2
neofytr <imagination12344321.com@gmail.com>
2025-03-23 11:50:09 +0530

    greg arrays

--- 1400/gregarr.cpp
+++ 1400/gregarr.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 void solve()
 {
+    size_t n, m, k;
+    cin >> n >> m >> k;
+
+    vector<size_t> arr(n);
+    for (size_t &val : arr)
+    {
+        cin >> val;
+    }
+
+    // l and r follow one based indexing; we change them to zero based by doing l--, r--;
+    vector<tuple<size_t, size_t, size_t>> operations(m); // l, r, d
+
+    for (auto &trips : operations)
+    {
+        
+    }
 }
 
 int main()
