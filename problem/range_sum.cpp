b1505005ef8a217298dca58d547470d741bbb0ce
neofytr <imagination12344321.com@gmail.com>
2025-03-23 10:36:17 +0530

    range sum

--- problem/range_sum.cpp
+++ problem/range_sum.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
+using namespace std;
 
 int main()
 {
-    
+    int n, q;
+    cin >> n >> q;
+
+    vector<size_t> prefix(n + 1);
+    prefix[0] = 0;
+
+    size_t temp;
+    for (int index = 1; index < n + 1; index++)
+    {
+        cin >> temp;
+        prefix[index] = prefix[index - 1] + temp;
+    }
+
+    int a, b;
+    while (q--)
+    {
+        cin >> a >> b;
+        cout << prefix[b + 1] - prefix[a] << "\n";
+    }
+
+    return EXIT_SUCCESS;
 }
