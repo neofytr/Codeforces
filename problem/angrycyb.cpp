b81f1534df65119b9c4bd10461e9d0e4d3a69c71
neofytr <imagination12344321.com@gmail.com>
2025-03-23 12:28:36 +0530

    codechef angry cyborg

---
+++ problem/angrycyb.cpp
@@ -0,0 +1,31 @@
+// https://www.codechef.com/BYTR20B/problems/AGCY?tab=statement
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n, q;
+    cin >> n >> q;
+
+    vector<long long> pref_one(n, 0); // init to zero
+    vector<long long> pref_two(n, 0); // init to zero
+
+    int l, r;
+    while (q--)
+    {
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
