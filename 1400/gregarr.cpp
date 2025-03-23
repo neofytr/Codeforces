7aae1b0a7a87b898b2cc549687e99e37a1b8f301
neofytr <imagination12344321.com@gmail.com>
2025-03-23 12:04:21 +0530

    greg array complete

--- 1400/gregarr.cpp
+++ 1400/gregarr.cpp
@@ -1,5 +1,3 @@
-// https://codeforces.com/contest/295/problem/A
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,30 +6,70 @@ void solve()
     size_t n, m, k;
     cin >> n >> m >> k;
 
-    vector<size_t> arr(n);
-    for (size_t &val : arr)
+    vector<long long> arr(n);
+    for (auto &val : arr)
     {
         cin >> val;
     }
 
-    // l and r follow one based indexing; we change them to zero based by doing l--, r--;
-    vector<tuple<size_t, size_t, size_t>> operations(m); // l, r, d
-
+    vector<tuple<size_t, size_t, long long>> operations(m); // l, r, d
     for (auto &trips : operations)
     {
-        
+        cin >> get<0>(trips) >> get<1>(trips) >> get<2>(trips);
+        get<0>(trips)--;
+        get<1>(trips)--;
     }
+
+    // count how many times each operation is applied
+    vector<long long> op_counts(m, 0);
+
+    for (size_t query = 0; query < k; query++)
+    {
+        size_t x, y;
+        cin >> x >> y;
+        x--, y--;
+
+        // increment count for operations x through y
+        for (size_t i = x; i <= y; i++)
+        {
+            op_counts[i]++;
+        }
+    }
+
+    vector<long long> prefix(n, 0);
+
+    for (size_t i = 0; i < m; i++)
+    {
+        if (!op_counts[i])
+            continue;
+
+        auto [l, r, d] = operations[i];
+        prefix[l] += d * op_counts[i];
+        if (r + 1 < n)
+        {
+            prefix[r + 1] -= d * op_counts[i];
+        }
+    }
+
+    for (size_t i = 1; i < n; i++)
+    {
+        prefix[i] += prefix[i - 1];
+    }
+
+    for (size_t i = 0; i < n; i++)
+    {
+        arr[i] += prefix[i];
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
 }
 
 int main()
 {
-    int t;
-    cin >> t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    while (t--)
-    {
-        solve();
-    }
-
+    solve();
     return 0;
 }
