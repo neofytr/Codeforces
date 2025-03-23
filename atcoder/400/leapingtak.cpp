094af3d8f41c6373ec50b12cc39f0e996f9516b5
neofytr <imagination12344321.com@gmail.com>
2025-03-23 15:22:30 +0530

    leaping

--- atcoder/400/leapingtak.cpp
+++ atcoder/400/leapingtak.cpp
@@ -5,17 +5,40 @@ using namespace std;
 
 void solve()
 {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> selections(n + 1);
+
+    int l, r;
+    for (int index = 0; index < k; index++)
+    {
+        cin >> l >> r;
+
+        // convert to zero based index
+        l--, r--;
+        selections[l] += 1;
+        selections[r + 1] -= 1;
+    }
+
+    // propagate
+    for (int index = 1; index < n; index++)
+    {
+        selections[index] += selections[index - 1];
+    }
+
+    for (int val : selections)
+    {
+        cout << val << " ";
+    }
+
+    cout << endl;
 }
 
 int main()
 {
-    int t;
-    cin >> t;
 
-    while (t--)
-    {
-        solve();
-    }
+    solve();
 
     return 0;
 }
