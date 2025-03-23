c386a8823a4f2e79f81fbcdf7b1bdbc8d8f0b44f
neofytr <imagination12344321.com@gmail.com>
2025-03-23 15:14:00 +0530

    leapingtak

---
+++ atcoder/400/leapingtak.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc179/tasks/abc179_d
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
