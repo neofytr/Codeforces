c6b257f740e7ab69621dcc04bb666ce72f7d45db
neofytr <imagination12344321.com@gmail.com>
2025-03-23 14:49:24 +0530

    angry cyborg

--- problem/angrycyb.cpp
+++ problem/angrycyb.cpp
@@ -14,7 +14,43 @@ void solve()
     int l, r;
     while (q--)
     {
+        cin >> l >> r;
+
+        // change to zero based indexing
+        l--, r--;
+
+        pref_one[l] += 1;
+        if (r + 1 < n)
+        {
+            pref_one[r + 1] += -1;
+            pref_two[r + 1] -= (r - l + 1);
+        }
     }
+
+    // propogate pref_one
+    for (int index = 1; index < n; index++)
+    {
+        pref_one[index] += pref_one[index - 1];
+    }
+
+    // copy into pref_two
+    for (int index = 0; index < n; index++)
+    {
+        pref_two[index] += pref_one[index];
+    }
+
+    // propagate pref_two
+    for (int index = 1; index < n; index++)
+    {
+        pref_two[index] += pref_two[index - 1];
+    }
+
+    for (int val : pref_two)
+    {
+        cout << val << " ";
+    }
+
+    cout << endl;
 }
 
 int main()
