16eab54964ff6d94545bfd0216d3c05d49480fb2
neofytr <imagination12344321.com@gmail.com>
2025-04-07 15:30:31 +0530

    weighted quick union

---
+++ AlgorithmsOne/unionFind/weightedQuickUnion/WeightedQuickUnion.java
@@ -0,0 +1,52 @@
+
+public class WeightedQuickUnion {
+
+    private final int[] arr;
+    private final int numOfObjects;
+    private final int[] size; // size[i] is the size of the tree rooted at i
+
+    public WeightedQuickUnion(int numOfObjects) {
+        arr = new int[numOfObjects];
+        size = new int[numOfObjects];
+        this.numOfObjects = numOfObjects;
+        for (int index = 0; index < numOfObjects; index++) {
+            arr[index] = index; // each element initially is a root
+            size[index] = 1;
+        }
+    }
+
+    private int getRoot(int index) {
+        while (arr[index] != index) {
+            index = arr[index];
+        }
+
+        return index;
+    }
+
+    public boolean union(int firstNode, int secondNode) {
+        if (firstNode >= this.numOfObjects || secondNode >= this.numOfObjects) {
+            return false;
+        }
+
+        int rootOne = getRoot(firstNode);
+        int rootTwo = getRoot(secondNode);
+
+        if (rootOne == rootTwo) {
+            return true;
+        }
+
+        if (size[rootOne] > size[rootTwo]) {
+            arr[rootTwo] = rootOne;
+            size[rootOne] += size[rootTwo];
+        } else {
+            arr[rootOne] = rootTwo;
+            size[rootTwo] += size[rootOne];
+        }
+
+        return true;
+    }
+
+    public boolean connected(int firstNode, int secondNode) {
+        return getRoot(firstNode) == getRoot(secondNode);
+    }
+}
