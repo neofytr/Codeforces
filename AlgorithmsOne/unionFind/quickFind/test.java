e6e63b481ca8cf03fba7a708e9fb4ca13967d4e7
neofytr <imagination12344321.com@gmail.com>
2025-04-07 13:35:34 +0530

    try with block

--- AlgorithmsOne/unionFind/quickFind/test.java
+++ AlgorithmsOne/unionFind/quickFind/test.java
@@ -4,22 +4,21 @@ import java.util.Scanner;
 public class test {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int numOfObjects = scanner.nextInt();
-        UnionFind unionFind = new UnionFind(numOfObjects);
+        try (Scanner scanner = new Scanner(System.in)) { // automatically closes the interface scanner after the block completes or returns in an error
+            int numOfObjects = scanner.nextInt();
+            UnionFind unionFind = new UnionFind(numOfObjects);
 
-        while (scanner.hasNextLine()) {
-            int firstNode = scanner.nextInt();
-            int secondNode = scanner.nextInt();
+            while (scanner.hasNextLine()) {
+                int firstNode = scanner.nextInt();
+                int secondNode = scanner.nextInt();
 
-            if (!unionFind.union(firstNode, secondNode)) {
-                System.err.println("Invalid Nodes " + firstNode + " and " + secondNode + ". Must be in the range 0 to " + (numOfObjects - 1));
-                return;
+                if (!unionFind.union(firstNode, secondNode)) {
+                    System.err.println("Invalid Nodes " + firstNode + " and " + secondNode + ". Must be in the range 0 to " + (numOfObjects - 1));
+                    return;
+                }
             }
+
+            System.out.println(unionFind.connected(12, 34));
         }
-
-        System.out.println(unionFind.connected(12, 34));
-
-        scanner.close();
     }
 }
