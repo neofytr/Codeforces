4ae6134ef362e39bc567b4b1e12f12dc6c0d7b60
neofytr <imagination12344321.com@gmail.com>
2025-04-07 12:56:16 +0530

    union function

--- AlgorithmsOne/unionFind/UnionFind.java
+++ AlgorithmsOne/unionFind/UnionFind.java
@@ -8,7 +8,31 @@ public class UnionFind {
     UnionFind(int numOfObjects) {
         arr = new ArrayList<>(numOfObjects);
         for (int i = 0; i < numOfObjects; i++) {
-            arr.add(0);
+            arr.add(-1);
         }
+    }
+
+    boolean union(int nodeOne, int nodeTwo) {
+        int size = arr.size();
+        if (nodeOne >= size || nodeTwo >= size) {
+            return false;
+        }
+
+        int valOne = arr.get(nodeOne);
+        int valTwo = arr.get(nodeTwo);
+
+        if (valOne == -1 && valTwo != -1) {
+            arr.set(nodeOne, valTwo);
+        } else if (valTwo == -1 && valOne != -1) {
+            arr.set(nodeTwo, valOne);
+        } else {
+            for (int index = 0; index < size; index++) {
+                if (arr.get(index) == valTwo) {
+                    arr.set(index, valOne);
+                }
+            }
+        }
+
+        return true;
     }
 }
