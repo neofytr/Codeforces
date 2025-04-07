a6cb0ff2380521c5853ee9f6ba9503daef17aa70
neofytr <imagination12344321.com@gmail.com>
2025-04-07 20:34:48 +0530

    trying to do with quickUnion

--- AlgorithmsOne/unionFind/interview/Successor.java
+++ AlgorithmsOne/unionFind/interview/Successor.java
@@ -1,43 +1,82 @@
 
-public class Successor {
+import java.util.Scanner;
 
-    private final int[] arr;
-    private final int numOfElements;
+class quickUnion {
 
-    public Successor(int numOfElements) {
-        this.numOfElements = numOfElements;
-        arr = new int[numOfElements];
-        for (int index = 0; index < numOfElements; index++) {
-            if (index == numOfElements - 1) {
-                arr[index] = index;
-            } else {
-                arr[index] = index + 1;
-            }
+    private final int arr[];
+    private final int size[];
+    private int components;
+    private final int numOfObjects;
+
+    public quickUnion(int numOfObjects) {
+        this.numOfObjects = numOfObjects;
+        arr = new int[numOfObjects];
+        size = new int[numOfObjects];
+        components = numOfObjects;
+        for (int index = 0; index < numOfObjects; index++) {
+            arr[index] = index; // each element is a tree of single root, with the element at the root
+            size[index] = 1;
         }
     }
 
-    public boolean delete(int element) {
-        if (element >= this.numOfElements) {
+    private int getRoot(int index) {
+        int initIndex = index;
+        while (index != arr[index]) {
+            index = arr[index];
+        }
+
+        // path compression loop
+        int temp;
+        while (initIndex != arr[initIndex]) {
+            temp = arr[initIndex]; // store the current parent of the current node
+            arr[initIndex] = index; // set the parent of the current node to the root
+            initIndex = temp;
+        }
+
+        return index;
+    }
+
+    public boolean union(int firstNode, int secondNode) {
+        if (firstNode >= this.numOfObjects || secondNode >= this.numOfObjects) {
             return false;
         }
 
-        arr[element] = -1;
-        if (element > 0 && element < numOfElements - 1) {
-            arr[element - 1] = element + 1;
-        } else if (element == numOfElements - 1) {
-            if (element - 1 >= 0) {
-                arr[element - 1] = element - 1;
-            }
+        int firstRoot = getRoot(firstNode);
+        int secondRoot = getRoot(secondNode);
+
+        if (firstRoot == secondRoot) {
+            return true;
         }
+
+        if (size[firstRoot] > size[secondRoot]) {
+            arr[secondRoot] = firstRoot;
+            size[firstRoot] += size[secondRoot];
+        } else {
+            arr[firstRoot] = secondRoot;
+            size[secondRoot] += size[firstRoot];
+        }
+
+        components--; // we connected two unconnected component sets; reduce the number of components by 1
 
         return true;
     }
 
-    public int getSuccessor(int element) {
-        if (element >= this.numOfElements) {
-            return -1;
-        }
+    public boolean connected(int firstNode, int secondNode) {
+        return getRoot(firstNode) == getRoot(secondNode);
+    }
 
-        return arr[element];
+    public boolean isFullyConnected() {
+        return components == 1;
+    }
+
+}
+
+public class Successor {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int numOfElements = scanner.nextLine();
+            quickUnion set = new quickUnion(numOfElements);
+        }
     }
 }
