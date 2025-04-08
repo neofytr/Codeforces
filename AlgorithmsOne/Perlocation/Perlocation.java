8abb18553eca355fc2554c88c2b4ee8df9dd8cd3
neofytr <imagination12344321.com@gmail.com>
2025-04-08 10:39:40 +0530

    quick union in percolation

--- AlgorithmsOne/Perlocation/Perlocation.java
+++ AlgorithmsOne/Perlocation/Perlocation.java
@@ -2,4 +2,46 @@ class quickUnion {
     private final int[] parent; // parent[i] contains the parent of the node i
     private final int[] rank; // rank[i] contains the number of elements in the tree rooted at i
     private final int numOfObjects;
+
+    public quickUnion(int numOfObjects) {
+        parent = new int[numOfObjects];
+        rank = new int[numOfObjects];
+        this.numOfObjects = numOfObjects;
+
+        for (int index = 0; index < numOfObjects; index++) {
+            parent[index] = index;
+            rank[index] = 0;
+        }
+    }
+
+    private int getRoot(int node) {
+        if (node < 0 || node >= this.numOfObjects) {
+            throw new IllegalArgumentException("Invalid index: " + node);
+        }
+
+        int initNode = node;
+        while(node != parent[node]) {
+            node = parent[node];
+        }
+
+        // node now contains the root node of node
+        // we now do the path compression loop
+        int temp;
+        while(initNode != parent[initNode] ) {
+            temp = parent[initNode];
+            parent[initNode] = node; // path compress; change the parent node of all the nodes in the path to from node to the root node to the root node
+            initNode = temp;
+        }
+
+        return node;
+    }
+
+    public void union(int firstNode, int secondNode) {
+        if (firstNode < 0 || secondNode < 0 || firstNode >= this.numOfObjects || secondNode >= this.numOfObjects) {
+            throw new IllegalArgumentException("Invalid node arguments");
+        }
+
+        int firstRoot = getRoot(firstNode);
+        int secondRoot = getRoot(secondNode);
+    }
 }
