70d9b2eeaf2a1a553dfb870d8d1b4227175d1992
neofytr <imagination12344321.com@gmail.com>
2025-04-08 11:10:28 +0530

    percolation almost done

--- AlgorithmsOne/Perlocation/Perlocation.java
+++ AlgorithmsOne/Perlocation/Perlocation.java
@@ -9,7 +9,7 @@ class quickUnion {
         this.numOfObjects = numOfObjects;
 
         for (int index = 0; index < numOfObjects; index++) {
-            parent[index] = index;
+            parent[index] = index; // each element is in it's own component set initially; each component set is represented by a root node for which parent[index] = index
             rank[index] = 0;
         }
     }
@@ -36,7 +36,7 @@ class quickUnion {
         return node;
     }
 
-    public void union(int firstNode, int secondNode) {
+    public void union(int firstNode, int secondNode) { // union of the component set containing firstNode and secondNode
         if (firstNode < 0 || secondNode < 0 || firstNode >= this.numOfObjects || secondNode >= this.numOfObjects) {
             throw new IllegalArgumentException("Invalid node arguments");
         }
@@ -70,22 +70,91 @@ class quickUnion {
 }
 
 public class Perlocation {
-    private final int numOfObjects;
+    private final int gridSize;
     private final quickUnion grid;
-    private final boolean[] open;
+    private final boolean[] siteOpen;
+    private int numOfOpenSites;
+    private final int topVirtualSite;
+    private final int bottomVirtualSite;
 
     public Perlocation(int gridSize) {
         if (gridSize <= 0) {
-            throw new IllegalArgumentException("Invalid grid size; Must be positive!")
+            throw new IllegalArgumentException("Invalid grid size; Must be positive!");
         }
 
-        this.numOfObjects = gridSize * gridSize;
-        grid = new quickUnion(this.numOfObjects);
+        this.gridSize = gridSize;
+        grid = new quickUnion(gridSize * gridSize + 2); // + 2 for two virtual nodes
 
-        for (int index = 0; index < this.numOfObject; index++) {
+        topVirtualSite = gridSize * gridSize;
+        bottomVirtualSite = gridSize * gridSize + 1;
 
+        // all the top row sites are connected to the topVirtualSite
+        // and all the bottom row sites are connected to the bottomVirtualSite
+        for (int index = 0; index < gridSize; index++) {
+            grid.union(topVirtualSite, index);
+            grid.union(bottomVirtualSite, gridSize * gridSize - index - 1);
+        }
+
+        siteOpen = new boolean[gridSize * gridSize]; // automatically initialized to all false
+        numOfOpenSites = 0;
+    }
+
+    public void isOpen(int row, int column) {
+        row--, column--; // coversion to 0-based indexing
+        int index = row * this.gridSize + column;
+        if (index < 0 && index >= this.gridSize) {
+            throw new IllegalArgumentException("Invalid row and column arguments");
+        }
+
+        return siteOpen[index];
+    }
+
+    public void open(int row, int column) { // row and column are given in 1-based indexing
+        row--, column--; // coversion to 0-based indexing
+        int index = row * this.gridSize + column;
+        if (index < 0 && index >= this.gridSize) {
+            throw new IllegalArgumentException("Invalid row and column arguments");
+        }
+
+        if (siteOpen[index]) {
+            return;
+        }
+
+        siteOpen[index] = true;
+        numOfOpenSites++;
+
+        if (index - 1 >= 0 && siteOpen[index - 1]) {
+            grid.union(index, index - 1);
+        }
+
+        if (index + 1 < this.gridSize && siteOpen[index + 1]) {
+            grid.union(index, index - 1);
+        }
+
+        if (index - this.gridSize >= 0 && siteOpen[index - this.gridSize]) {
+            grid.union(index, index - this.gridSize);
+        }
+
+        if (index + this.gridSize < this.gridSize) {
+            grid.union(index, index + this.gridSize);
         }
     }
 
+    public int numberOfOpenSites() {
+        return this.numOfOpenSites;
+    }
 
+    public void isFull(int row, int column) {
+        row--, column--; // coversion to 0-based indexing
+        int index = row * this.gridSize + column;
+        if (index < 0 && index >= this.gridSize) {
+            throw new IllegalArgumentException("Invalid row and column arguments");
+        }
+
+        return grid.isConnected(topVirtualSite, index);
+    }
+
+    public boolean percolates() { // does the system percolate
+        return grid.isConnected(topVirtualSite, bottomVirtualSite);
+    }
 }
