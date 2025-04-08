7aa3f4433aea1f0a5870eb9722f4819bd432fa69
neofytr <imagination12344321.com@gmail.com>
2025-04-08 10:33:33 +0530

    percolation working

--- AlgorithmsOne/Perlocation/Perlocation.java
+++ AlgorithmsOne/Perlocation/Perlocation.java
@@ -1,6 +1,5 @@
-public class Perlocation {
-    private final int[][] grid;
-    public Perlocation(int gridSize) {
-
-    }
+class quickUnion {
+    private final int[] parent; // parent[i] contains the parent of the node i
+    private final int[] rank; // rank[i] contains the number of elements in the tree rooted at i
+    private final int numOfObjects;
 }
