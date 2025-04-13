177049a9a25c2d1ae0de7fce89760ba921cf6f5b
neofytr <imagination12344321.com@gmail.com>
2025-04-13 21:17:35 +0530

    subset sum

---
+++ topics/bitwise/subset_sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int &val : arr)
+    {
+        cin >> val;
+    }
+
+    
+}
