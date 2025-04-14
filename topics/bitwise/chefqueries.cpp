1e208f5444050c7f4ccad4f3cc1674bb30c051e0
neofytr <imagination12344321.com@gmail.com>
2025-04-14 11:24:12 +0530

    chef and queries

---
+++ topics/bitwise/chefqueries.cpp
@@ -0,0 +1,11 @@
+// https://www.codechef.com/problems/CHEFQUE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    size_t set = 0; // initially no element in the set
+
+    size_t q, s, a, b;
+}
