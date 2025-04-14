d37d43aa19a20f9969f5ddd4535335fb3ba206c0
neofytr <imagination12344321.com@gmail.com>
2025-04-14 12:13:56 +0530

    chef queries

--- topics/bitwise/chefqueries.cpp
+++ topics/bitwise/chefqueries.cpp
@@ -5,7 +5,36 @@ using namespace std;
 
 int main()
 {
-    size_t set = 0; // initially no element in the set
-
     size_t q, s, a, b;
+    cin >> q >> s >> a >> b;
+
+    bitset<1ULL << 31> set; // initially empty
+    uint64_t sum = 0;
+
+    for (size_t i = 0; i < q; i++)
+    {
+        size_t num = s / 2;
+
+        if (s & 1)
+        {
+            if (!set[num])
+            {
+                set[num] = true;
+                sum += num;
+            }
+        }
+        else
+        {
+            if (set[num])
+            {
+                set[num] = false;
+                sum -= num;
+            }
+        }
+
+        s = (s * a + b) & 0xFFFFFFFFULL; // mask to 2^32
+    }
+
+    cout << sum << endl;
+    return 0;
 }
