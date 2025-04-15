bf437c169ebe70cd3728519c96637e8f4cfeced9
neofytr <imagination12344321.com@gmail.com>
2025-04-15 20:23:49 +0530

    subset sum

---
+++ topics/recursion/subsetSum.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sums;
+
+void helper(vector<int> &arr, int curr_sum, int index)
+{
+    if (index >= arr.size())
+    {
+        sums.push_back(curr_sum);
+        return;
+    }
+
+    // either select the current element or not
+    helper(arr, curr_sum + arr[index], index + 1);
+    helper(arr, curr_sum, index + 1);
+}
+
+void getSums(vector<int> &arr)
+{
+    helper(arr, 0, 0);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int &val : arr)
+    {
+        cin >> val;
+    }
+
+    getSums(arr);
+    sort(sums.begin(), sums.end());
+
+    for (int val : sums)
+    {
+        cout << val << " ";
+    }
+
+    cout << endl;
+    return EXIT_SUCCESS;
+}
