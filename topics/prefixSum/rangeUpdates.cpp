f13a4125d8e1e96b66fd48d903329a4613b44348
neofytr <imagination12344321.com@gmail.com>
2025-04-16 01:36:31 +0530

    range updates

---
+++ topics/prefixSum/rangeUpdates.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n, q;
+    cin >> n >> q;
+
+    vector<long long> arr(n);
+    for (long long &val : arr)
+    {
+        cin >> val;
+    }
+
+    vector<long long> updates(n + 1, 0);
+    int l, r, x;
+    while (q--)
+    {
+        cin >> l >> r >> x;
+        // convert to zero-based indexing
+
+        updates[l] = x;
+        updates[r + 1] = -x;
+    }
+
+    // accumulate all the updates
+    for (int index = 1; index <= n; index++)
+    {
+        updates[index] += updates[index - 1];
+    }
+
+    // perform range update
+    for (int index = 0; index < n; index++)
+    {
+        arr[index] += updates[index];
+    }
+
+    return EXIT_SUCCESS;
+}
