eec779a6e5449c2d45f3cafcb5c4d2c953a06d4f
neofytr <imagination12344321.com@gmail.com>
2025-04-15 17:16:34 +0530

    completed palindrome

--- topics/recursion/palindrome.cpp
+++ topics/recursion/palindrome.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 bool isPalindrome(string str, int start, int end)
 {
+    if (start >= end)
+    {
+        return true;
+    }
+
+    if (str[start] == str[end])
+    {
+        return isPalindrome(str, start + 1, end - 1);
+    }
 }
 
 int main()
