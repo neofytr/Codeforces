83a1a3eac929c1685fdbb977c0fcbc4e000acfa6
neofytr <imagination12344321.com@gmail.com>
2025-04-15 17:36:29 +0530

    subsequences

--- topics/recursion/subsequence.cpp
+++ topics/recursion/subsequence.cpp
@@ -11,8 +11,27 @@ void printVector(vector<int> &arr)
     cout << endl;
 }
 
+void helper(int index, vector<int> &arr, vector<int> &current)
+{
+    if (index == arr.size())
+    {
+        printVector(current);
+        return;
+    }
+
+    // include the current index
+    current.push_back(arr[index]);
+    helper(index + 1, arr, current);
+
+    // exclude the current element
+    current.pop_back();
+    helper(index + 1, arr, current);
+}
+
 void subsequence(vector<int> &arr)
 {
+    vector<int> current;
+    helper(0, arr, current);
 }
 
 int main()
