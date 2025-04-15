6c11004ea4871e26d262dbe7eeccc3e613a6f95e
neofytr <imagination12344321.com@gmail.com>
2025-04-15 17:15:29 +0530

    is palindrome

---
+++ topics/recursion/palindrome.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPalindrome(string str, int start, int end)
+{
+}
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    cout << isPalindrome(str, 0, str.length() - 1) << endl;
+}
