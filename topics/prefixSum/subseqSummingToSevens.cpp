f2fedc323de6678b59fa46f0a7576f934f0a4f79
neofytr <imagination12344321.com@gmail.com>
2025-04-16 14:51:31 +0530

    subsequences summing to seven

---
+++ topics/prefixSum/subseqSummingToSevens.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> cows(n);
+    for (int &val : cows)
+    {
+        cin >> val;
+    }
+}
