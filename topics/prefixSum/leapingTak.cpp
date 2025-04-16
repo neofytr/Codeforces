e8c5523531c7923ddc10d7e741eebc886feaacb4
neofytr <imagination12344321.com@gmail.com>
2025-04-16 14:37:01 +0530

    leaping tak

--- topics/prefixSum/leapingTak.cpp
+++ topics/prefixSum/leapingTak.cpp
@@ -1,5 +1,3 @@
-// https://atcoder.jp/contests/abc179/tasks/abc179_d
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,36 +5,36 @@ using namespace std;
 
 int main()
 {
-    int n, k;
-    cin >> n >> k;
+	long long n, k;
+	cin >> n >> k;
 
-    vector<int> arr(n + 1, 0);
-    arr[1] = 1;
+	vector<pair<long long, long long>> segments(k);
+	for (auto &segment : segments)
+	{
+		cin >> segment.first >> segment.second;
+	}
 
-    vector<pair<int, int>> segments(k);
-    for (auto &segment : segments)
-    {
-        cin >> segment.first >> segment.second;
-    }
+	vector<long long> arr(n); // arr[i] = number of ways to reach index i
+	vector<long long> prefix(n + 1); // prefix[i] = sum(arr[0..i-1])
+	arr[0] = 1;
+	prefix[1] = 1;
 
-    vector<int> prefix(n + 2, 0);
-    prefix[1] = 1;
-    for (int index = 2; index <= n; index++)
-    {
-        for (auto [l, r] : segments)
-        {
-            int left = max(1, index - r);
-            int right = index - l;
-            if (right < 1)
-                continue;
+	for (long long index = 1; index < n; index++)
+	{
+		long long add = 0;
+		for (auto segment : segments)
+		{
+			long long left = max(0LL, index - segment.second);
+			long long right = index - segment.first;
+			if (right < 0)
+				continue;
 
-            int add = (prefix[right] - prefix[left - 1] + MOD) % MOD;
-            arr[index] = (arr[index] + add) % MOD;
-        }
+			add = (add + prefix[right + 1] - prefix[left] + MOD) % MOD;
+		}
 
-        prefix[index] = (prefix[index - 1] + arr[index]) % MOD;
-    }
+		arr[index] = add;
+		prefix[index + 1] = (prefix[index] + arr[index]) % MOD;
+	}
 
-    cout << arr[n] << endl;
-    return EXIT_SUCCESS;
+	cout << arr[n - 1] << endl;
 }
