5d2b76fae7a3753a75417b0cf1ffa0b95ef57e90
neofytr <imagination12344321.com@gmail.com>
2025-04-16 15:27:33 +0530

    subsequences summing to seven

--- topics/prefixSum/subseqSummingToSevens.cpp
+++ topics/prefixSum/subseqSummingToSevens.cpp
@@ -6,9 +6,29 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> cows(n);
-    for (int &val : cows)
+    vector<long long> prefix(n + 1);
+    prefix[0] = 0;
+
+    unordered_map<int, long long> map;
+    map[0] = 0;
+
+    long long maxlen = 0;
+    long long val;
+
+    for (int index = 1; index <= n; index++)
     {
         cin >> val;
+        prefix[index] = (prefix[index - 1] + val) % 7;
+        if (map.count(prefix[index]))
+        {
+            // current remainder exists already
+            maxlen = max(maxlen, index - prefix[index]);
+        }
+        else
+        {
+            map[prefix[index]] = index;
+        }
     }
+
+    cout << maxlen << endl;
 }
