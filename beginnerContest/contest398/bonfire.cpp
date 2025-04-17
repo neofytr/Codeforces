a060f26f62fadf644d24ef93d74a1755b57cc7c5
neofytr <imagination12344321.com@gmail.com>
2025-04-17 22:59:49 +0530

    did bonfire with hashsets

--- beginnerContest/contest398/bonfire.cpp
+++ beginnerContest/contest398/bonfire.cpp
@@ -8,86 +8,42 @@ int main()
     string str;
     cin >> str;
 
-    vector<vector<int>> grid(2 * n + 1, vector<int>(2 * n + 1));
-#define GRID(i, j) grid[i + n][j + n]
+    set<pair<int, int>> smokes;
+    smokes.insert({0, 0});
 
-    vector<pair<int, int>> smokes;
-    smokes.push_back(pair(0, 0)); // initially only one smoke
-
-    for (int index = 1; index <= n; index++)
+    for (int i = 0; i < n; ++i)
     {
-        switch (str[index - 1])
-        {
-        case 'N':
-        {
-            for (auto &smoke : smokes)
-            {
-                smoke.first--;
-            }
-            break;
-        }
-        case 'W':
-        {
-            for (auto &smoke : smokes)
-            {
-                smoke.second--;
-            }
-            break;
-        }
-        case 'S':
-        {
-            for (auto &smoke : smokes)
-            {
-                smoke.first++;
-            }
-            break;
-        }
-        case 'E':
-        {
-            for (auto &smoke : smokes)
-            {
-                smoke.second++;
-            }
-            break;
-        }
-        }
+        set<pair<int, int>> new_smokes;
 
-        bool found = false;
-        for (auto &smoke : smokes)
+        for (auto &[x, y] : smokes)
         {
-            if (smoke.first == 0 && smoke.second == 0)
+            switch (str[i])
             {
-                found = true;
+            case 'N':
+                new_smokes.insert({x - 1, y});
+                break;
+            case 'S':
+                new_smokes.insert({x + 1, y});
+                break;
+            case 'W':
+                new_smokes.insert({x, y - 1});
+                break;
+            case 'E':
+                new_smokes.insert({x, y + 1});
                 break;
             }
         }
 
-        if (!found)
-        {
-            smokes.push_back(pair(0, 0));
-        }
+        if (!new_smokes.count({0, 0}))
+            new_smokes.insert({0, 0});
 
-        found = false;
+        smokes = move(new_smokes);
 
-        for (auto &smoke : smokes)
-        {
-            if (smoke.first == r && smoke.second == c)
-            {
-                found = true;
-                break;
-            }
-        }
-
-        if (found)
-        {
+        if (smokes.count({r, c}))
             cout << 1;
-        }
         else
-        {
             cout << 0;
-        }
     }
-
-    cout << endl;
-    return EXIT_SUCCESS;
+    cout << '\n';
+    return 0;
 }
