b92f01ed8f2097b1bf8b0f31a4c80bafc88c40e8
neofytr <imagination12344321.com@gmail.com>
2025-04-18 14:30:24 +0530

    solved but not in time limit

--- beginnerContest/contest397/cube.cpp
+++ beginnerContest/contest397/cube.cpp
@@ -3,5 +3,51 @@ using namespace std;
 
 int main()
 {
-    
+    uint64_t n;
+    cin >> n;
+
+    uint64_t delta = 1;
+    uint64_t cube = delta * delta * delta;
+    while (cube < n)
+    {
+        if ((n - cube) % delta)
+        {
+            delta++;
+            cube = delta * delta * delta;
+            continue;
+        }
+
+        if (((n - cube) / delta) % 3)
+        {
+            delta++;
+            cube = delta * delta * delta;
+            continue;
+        }
+
+        uint64_t prod = ((n - cube) / delta) / 3;
+        uint64_t x = prod, y;
+
+        while (x >= 1)
+        {
+            if (prod % x)
+            {
+                x--;
+                continue;
+            }
+
+            y = prod / x;
+            if (x >= y && x - y == delta)
+            {
+                cout << x << " " << y << endl;
+                return EXIT_SUCCESS;
+            }
+            x--;
+        }
+
+        delta++;
+        cube = delta * delta * delta;
+    }
+
+    cout << -1 << endl;
+    return EXIT_SUCCESS;
 }
