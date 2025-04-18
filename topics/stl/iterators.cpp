d3cf9e87cb7f77efd7ebed08a2c07edbc88e290d
neofytr <imagination12344321.com@gmail.com>
2025-04-18 19:30:29 +0530

    reverse iterators

--- topics/stl/iterators.cpp
+++ topics/stl/iterators.cpp
@@ -17,4 +17,13 @@ int main()
     // iterator to the element following the last element of the array
     // this acts as a placeholder; attempting to access it results in undefined behavior
     vector<int>::iterator ending = arr.end();
+
+    // container is empty iff arr.begin() == arr.end()
+
+    cout << *starting << endl; // dereferencing the iterator
+
+    vector<int>::reverse_iterator last = arr.rbegin(); // reverse iterator; points to the last element of the container
+    // incrementing a reverse iterator increments it in reverse
+
+    cout << *(last + 1) << endl;
 }
