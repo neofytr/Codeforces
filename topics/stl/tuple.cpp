1e286be3ff0e514d0730c8f92798308be92771db
neofytr <imagination12344321.com@gmail.com>
2025-04-18 19:58:32 +0530

    tuples

---
+++ topics/stl/tuple.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    tuple<int, string, char> tupleOne = {69, "raj", 'A'};
+    tuple<int, int> tupleTwo(20, 10);
+
+    cout << get<0>(tupleOne) << endl;
+    cout << get<1>(tupleOne) << endl;
+
+    get<0>(tupleOne) = 100;
+    cout << get<0>(tupleOne) << endl;
+}
