ad42226d9968fad06cc1f73bd512b302416254be
neofytr <imagination12344321.com@gmail.com>
2025-04-18 17:19:11 +0530

    tried to implement quicksort

--- topics/sorting/quickSort.cpp
+++ topics/sorting/quickSort.cpp
@@ -1,9 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void quickSort(vector<int> &arr, int left, int right)
+void quickSort(vector<int> &arr, int start, int len)
 {
-    
+    if (len <= 0 || len == 1)
+    {
+        // do nothing; already sorted
+        return;
+    }
+
+    int pivot = arr[start]; // choose first element as the pivot
+
+    // first find the correct place of the pivot in the array
+    int placeOfPivot = start;
+    for (int index = start; index < start + len; index++)
+    {
+        if (arr[index] < pivot)
+        {
+            placeOfPivot++;
+        }
+    }
+
+    // place smaller elements (smaller than pivot) before the pivot and larger elements (>= pivot) after it
+    int smallerCurr = start;
+    int largerCurr = placeOfPivot + 1;
+    for (int index = start; index < start + len; index++)
+    {
+        if (arr[index] < pivot)
+        {
+            swap(arr[smallerCurr++], arr[index]);
+        }
+        else
+        {
+            swap(arr[largerCurr++], arr[index]);
+        }
+    }
+
+    arr[placeOfPivot] = pivot;
+
+    // do recursion
+    quickSort(arr, start, placeOfPivot - start);
+    quickSort(arr, placeOfPivot + 1, largerCurr - placeOfPivot);
 }
 
 int main()
@@ -17,7 +54,7 @@ int main()
         cin >> val;
     }
 
-    quickSort(arr, 0, n - 1);
+    quickSort(arr, 0, n);
     for (int val : arr)
     {
         cout << val << " ";
