e3f0d36b8631447228b15ea5a8bc436868859935
neofytr <imagination12344321.com@gmail.com>
2025-04-21 15:13:19 +0530

    valid parenthesis

--- topics/stl/stack/validParenthesis.cpp
+++ topics/stl/stack/validParenthesis.cpp
@@ -1,7 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+class Solution
 {
-    
-}
+public:
+    bool isValid(string s)
+    {
+        stack<char> bracket;
+        unordered_map<char, char> matching = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}};
+
+        for (char ch : s)
+        {
+            if (ch == '(' || ch == '[' || ch == '{')
+            {
+                bracket.push(ch);
+            }
+            else
+            {
+                if (bracket.empty() || bracket.top() != matching[ch])
+                {
+                    return false;
+                }
+                bracket.pop();
+            }
+        }
+        return bracket.empty();
+    }
+};
