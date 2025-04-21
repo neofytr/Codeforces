77b63e4f90d62a62b9feb54cc4d5218594a69421
neofytr <imagination12344321.com@gmail.com>
2025-04-21 20:57:17 +0530

    reasons

--- topics/heap/binaryTree.c
+++ topics/heap/binaryTree.c
@@ -30,51 +30,54 @@ int *conv_to_binary_arr(binary_tree_t *binary_tree)
     }
 
     size_t max_nodes = (1ULL << (binary_tree->height + 1)) - 1;
-    int *arr = (int *)calloc(sizeof(int), max_nodes);
+    int *arr = (int *)calloc(max_nodes, sizeof(int));
     if (!arr)
     {
         fprintf(stderr, "ERROR: array allocation failed: %s\n", strerror(errno));
         return NULL;
     }
 
-    // traverse the tree level wise from left to right (BFS)
     struct search_t
     {
         binary_node_t *binary_node;
         size_t index;
     };
 
+    // we just free the entire copy of the search_t structure that the queue has
+    // and not the binary node struct pointer we have inside it
+    // so we use generic free on the search_t struct and thus NULL is passed
     queue_t *queue = create_queue(sizeof(struct search_t), NULL, NULL);
     if (!queue)
     {
         fprintf(stderr, "ERROR: tree traversal failed\n");
+        free(arr);
         return NULL;
     }
 
     struct search_t root = {.binary_node = binary_tree->top_node, .index = 0};
     enqueue(queue, &root);
 
-    struct search_t temp;
-    binary_node_t *binary_node;
-
     while (!isEmpty(queue))
     {
-        dequeue(queue, &temp); // the data is copied into temp upon dequeue (and the queue's copy is freed)
-        binary_node = temp.binary_node;
-        arr[temp.index] = binary_node->data;
+        struct search_t temp;
+        dequeue(queue, &temp);
 
-        if (binary_node->left_node) // left node is available, put it into the queue
+        arr[temp.index] = temp.binary_node->data;
+
+        if (temp.binary_node->left_node)
         {
-            root.binary_node = binary_node->left_node;
-            root.index = temp.index * 2;
-            enqueue(queue, &root);
+            struct search_t left = {
+                .binary_node = temp.binary_node->left_node,
+                .index = temp.index * 2};
+            enqueue(queue, &left);
         }
 
-        if (binary_node->right_node)
+        if (temp.binary_node->right_node)
         {
-            root.binary_node = binary_node->right_node;
-            root.index = temp.index * 2 + 1;
-            enqueue(queue, &root);
+            struct search_t right = {
+                .binary_node = temp.binary_node->right_node,
+                .index = temp.index * 2 + 1};
+            enqueue(queue, &right);
         }
     }
 
