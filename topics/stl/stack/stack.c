307dbc41a6109615e975d23872877a8de8ae3b3f
neofytr <imagination12344321.com@gmail.com>
2025-04-21 14:53:16 +0530

    a nice stack implementation

--- topics/stl/stack/stack.c
+++ topics/stl/stack/stack.c
@@ -59,9 +59,57 @@ bool pop(stack_t* stack)
         return false;
     }
 
+    stack->length--;
+}
 
+int top(stack_t* stack)
+{
+    if (!stack || !stack->arr)
+    {
+        fprintf(stderr, "ERROR: invalid stack argument\n");
+        return false;
+    }
+
+    if (isEmpty(stack))
+    {
+        fprintf(stderr, "ERROR: invalid top operation; stack is empty\n");
+        return false;
+    }
+
+    return stack->arr[length - 1];
+}
+
+int length(stack_t* stack)
+{
+    if (!stack)
+    {
+        fprintf(stderr, "ERROR: invalid stack argument\n");
+        return false;
+    }
+
+    return stack->length;
 }
 
 bool push(stack_t* stack, int element)
 {
+    if (!stack || !stack->arr)
+    {
+        fprintf(stderr, "ERROR: invalid stack argument\n");
+        return false;
+    }
+
+    if (stack->length >= stack->capacity)
+    {
+        void* newStack = realloc((void*)stack->arr, 2 * stack->capacity);
+        if (!newStack)
+        {
+            fprintf(stderr, "ERROR: stack reallocation failed: %s\n", strerror(errno));
+            return false;
+        }
+
+        stack->capacity = 2 * stack->capacity;
+        stack->arr = newStack;
+    }
+
+    stack->arr[stack->length++] = element;
 }
