24f8669e7136b3e9e929f7775215a31a8699cf50
neofytr <imagination12344321.com@gmail.com>
2025-04-22 17:21:30 +0530

    insertion into the list complete

--- topics/linkedList/linked_list.c
+++ topics/linkedList/linked_list.c
@@ -38,7 +38,27 @@ bool list_insert(list_t *list, int index, TYPE *element) {
     }
 
     if (list->arr->is_empty) {
+        // dyn_arr_set will copy arr.num bytes from the location element and store inside it internally (as a copy) at index
         dyn_arr_set(list->arr, 0, element);
+        const int next = -1;
+        dyn_arr_set(list->next, 0, &next);
+        return true;
     }
+
+    if (index > list->arr->last_index) {
+        fprintf(stderr, "ERROR: index out of bounds of the list\n");
+        return false;
+    }
+
+    dyn_arr_append(list->arr, element);
+    if (index > 0) {
+        // set the element at the position index - 1 to point to the new element
+        dyn_arr_set(list->next, index - 1, &list->arr->last_index);
+    }
+
+    // set the new element to point to the element at index
+    dyn_arr_set(list->next, list->arr->last_index, &index);
+
+    // this completes the insertion
     return true;
 }
