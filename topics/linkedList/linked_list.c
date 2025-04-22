a0736b5756bf5597f03e962dfbe096ab4fd433ca
neofytr <imagination12344321.com@gmail.com>
2025-04-22 17:10:29 +0530

    some changes

--- topics/linkedList/linked_list.c
+++ topics/linkedList/linked_list.c
@@ -9,5 +9,36 @@ list_t *list_create() {
         return NULL;
     }
 
-    list->arr = dyn_arr_create(MIN_SIZE, sizeof());
+    list->arr = dyn_arr_create(MIN_SIZE, sizeof(TYPE), NULL);
+    if (!list->arr) {
+        fprintf(stderr, "ERROR: list allocation failed: %s\n", strerror(errno));
+        free(list);
+        return NULL;
+    }
+
+    list->next = dyn_arr_create(MIN_SIZE, sizeof(size_t), NULL);
+    if (!list->next) {
+        fprintf(stderr, "ERROR: list allocation failed: %s\n", strerror(errno));
+        free(list);
+        return NULL;
+    }
+
+    return list;
+}
+
+bool list_insert(list_t *list, int index, TYPE *element) {
+    if (!list || !list->arr || !list->next) {
+        fprintf(stderr, "ERROR: invalid list argument\n");
+        return false;
+    }
+
+    if (!element) {
+        fprintf(stderr, "ERROR: invalid element argument\n");
+        return false;
+    }
+
+    if (list->arr->is_empty) {
+        dyn_arr_set(list->arr, 0, element);
+    }
+    return true;
 }
--- topics/linkedList/linked_list.h
+++ topics/linkedList/linked_list.h
@@ -8,19 +8,21 @@
 #include <string.h>
 #include <errno.h>
 
+#define TYPE int
+
 // this is a singly linked list
 typedef struct list_ {
     dyn_arr_t *arr;
     dyn_arr_t *next;
 } list_t;
 
-bool list_append(list_t *list, void *element);
+bool list_append(list_t *list, TYPE *element);
 
-bool list_pop(list_t *list, void *element);
+bool list_pop(list_t *list, TYPE *element);
 
-bool list_insert(list_t *list, int index, void *element);
+bool list_insert(list_t *list, int index, TYPE *element);
 
-bool list_remove(list_t *list, int index, void *element);
+bool list_remove(list_t *list, int index, TYPE *element);
 
 bool list_empty(list_t *list);
 
