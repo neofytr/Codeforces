d8b6835afbd6c3f55a99eff6691a8fef624aa12f
neofytr <imagination12344321.com@gmail.com>
2025-04-23 16:13:53 +0530

    did valeriy

--- topics/dequeue/valeriy.cpp
+++ topics/dequeue/valeriy.cpp
@@ -8,30 +8,54 @@ int main() {
     long long max_elem = -1;
     int max_index = -1;
 
-    for (int i = 0; i < n; ++i) {
-        long long x;
-        cin >> x;
-        dq.push_back(x);
-        if (x > max_elem) {
-            max_elem = x;
-            max_index = i;
+    long long element;
+    for (int index = 0; index < n; index++) {
+        cin >> element;
+        dq.push_back(element);
+        if (element > max_elem) {
+            max_elem = element;
+            max_index = index;
         }
     }
 
-    vector<pair<long long, long long> > operations;
-    while (dq.front() != max_elem) {
-        long long a = dq.front();
-        dq.pop_front();
-        long long b = dq.front();
-        dq.pop_front();
-        operations.push_back({a, b});
+    // the maximum element will come in the front after exactly max_index operations
+    // after that, the 2nd element repeats in a cycle of n - 1
 
-        if (a > b) {
-            dq.push_front(a);
-            dq.push_back(b);
+    vector<pair<long long, long long> > operations(max_index + 1); // for 1-based indexing
+    for (int index = 0; index < max_index; index++) {
+        operations[index + 1] = make_pair(dq[0], dq[1]);
+        const long long first = dq.front();
+        dq.pop_front();
+        const long long second = dq.front();
+        dq.pop_front();
+
+        if (first > second) {
+            dq.push_front(first);
+            dq.push_back(second);
         } else {
-            dq.push_front(b);
-            dq.push_back(a);
+            dq.push_front(second);
+            dq.push_back(first);
         }
     }
+
+    // if operations are in the range 1 <= op <= max_index, we return operations[op]
+    // after that the second element goes into a cycle of n - 1
+
+    uint64_t m;
+    while (q--) {
+        cin >> m;
+
+        if (m <= max_index) {
+            cout << operations[m].first << " " << operations[m].second << endl;
+        } else {
+            uint64_t cyclic_offset = m - max_index; // how many steps into the cycle we are
+            uint64_t cycle_length = n - 1; // the length of the cycle (excluding max_elem)
+            uint64_t zero_based_index = (cyclic_offset - 1) % cycle_length; // zero-based index into the cycle
+            uint64_t index_in_deque = zero_based_index + 1; // actual index in deque (starts from dq[1])
+
+            cout << max_elem << " " << dq[index_in_deque] << endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
