5acb7714fe7aa4c8533dd03ec6e95553e60f16a4
neofytr <imagination12344321.com@gmail.com>
2025-04-24 18:10:33 +0530

    stl map

---
+++ topics/maps/maps.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // map
+    /*
+     *An associative container that contains key-value pairs with unique keys (sorted)
+     *A user-provided compare can be supplied to change the ordering(sorting)
+     *Search, removal, and insertion times are logarithmic
+     *Usually implemented as RB trees
+     *template<
+     *class Key,
+     *class T,
+     *class Compare = std::less<Key>,
+     *class Allocator = std::allocator<std::pair<const Key, T>>,
+     *> class map;
+     */
+}
