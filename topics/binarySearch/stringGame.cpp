e5e0f50d46160988267a654fdc060800e168e991
neofytr <imagination12344321.com@gmail.com>
2025-04-26 17:47:06 +0530

    string game

--- topics/binarySearch/stringGame.cpp
+++ topics/binarySearch/stringGame.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 bool func(const vector<int> &arr, const string &t, const string &p, const int index) {
+    if (index <= -1) {
+        return true;
+    } else if (index >= t.size()) {
+        return false;
+    }
+
     vector<bool> deleted(t.length(), false);
     for (int i = 0; i <= index; i++) {
         deleted[arr[i]] = true;
@@ -29,7 +35,13 @@ int main() {
         arr[i]--;
     }
 
-    int left = -1, right = t_len;
+    // if index is such that element deleting elements arr[0], ..., arr[index] from t, we cannot generate p from t, then
+    // the same is true for position index + 1, for all but the last position
+
+    // we are to find the largest index upto which the above is not true
+
+    // we maintain the invariant that func(left) is true and func(right) is false
+    int left = -1, right = t_len; // artificial elements
     while (right > left + 1) {
         int mid = left + (right - left) / 2;
         if (func(arr, t, p, mid)) {
