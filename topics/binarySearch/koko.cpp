fc2e0094f362960a014d1d443bad158fd85aa3c8
neofytr <imagination12344321.com@gmail.com>
2025-04-28 22:03:26 +0530

    explaning the logic

--- topics/binarySearch/koko.cpp
+++ topics/binarySearch/koko.cpp
@@ -4,7 +4,30 @@ using namespace std;
 class Solution {
 public:
     static int minEatingSpeed(const vector<int> &piles, const int h) {
-        int left = 0;
+        // h >= n
+
+        // if koko can eat all bananas with speed k, she can finish all with speed k + 1; for all k >= 0
+        // if koko cannot finish all bananas with speed k, she cannot finish all with speed k - 1; for all k > 0
+
+        // f[k] = 1 ; if koko can finish all bananas with speed k
+        //      = 0 ; otherwise if she cannot finish all bananas with speed k
+
+        // f is monotonically increasing
+        // f[0] = 0
+        // f[max(piles)] = 1
+
+        // so there exists a min k for which f[k] = 1, i.e, there is a k in the range 0 to max(piles) such that
+        // f[r] = 0 for all r < k
+        // and f[r] = 1 for all r >= k
+        // this is what we find with the following binary search
+
+        // we maintain the invariant
+        // f[r] = 0 for all r <= left
+        // and f[r] = 1 for all r >= right
+
+        // so when right = left + 1, we will have the min k for which f[k] = 1 which will be right
+
+        int left = 1;
         int right = *max_element(piles.begin(), piles.end());
 
         while (right != left + 1) {
