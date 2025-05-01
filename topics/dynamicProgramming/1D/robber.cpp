d17370dbac292d3051d4fa46981924cf21de0aed
neofytr <imagination12344321.com@gmail.com>
2025-05-01 16:20:12 +0530

    all three solutions

--- topics/dynamicProgramming/1D/robber.cpp
+++ topics/dynamicProgramming/1D/robber.cpp
@@ -22,6 +22,39 @@ public:
     static int rob(const vector<int> &arr) {
         const int n = static_cast<int>(arr.size());
         vector<int> dp(n, -1); // dp[i] stores the maximum loot we can get starting from the house at index i
-        return loot(arr, 0, dp);
+        int maxLootMemo = loot(arr, 0, dp);
+
+        // now for the tabulated solution
+        if (n == 1) {
+            return arr[0];
+        }
+
+        vector<int> tab(n); // tab[i] stores the max loot we can get until index i
+        tab[0] = arr[0];
+        tab[1] = max(arr[0], arr[1]);
+
+        for (int index = 2; index < n; index++) {
+            // max loot until position index is maximum of
+            // max loot until position index - 2 + arr[index] (if we're choosing current index to loot, we need to add 2 indexes before)
+            // and max loot until position index - 1 (if we're choosing to skip the current index, we need to max loot until the prev position)
+            dp[index] = max(dp[index - 2] + arr[index], dp[index - 1]);
+        }
+
+        int maxLootTab = dp[n - 1];
+
+        // now for space optimized
+        if (n == 1) {
+            return arr[0];
+        }
+
+        int prev = 0;
+        int prev_prev = 0;
+        for (int val: arr) {
+            int curr = max(prev, prev_prev + val);
+            prev_prev = prev;
+            prev = curr;
+        }
+
+        return prev;
     }
 };
