1a79d5a55c3d02f2f44bf6e6a825890eb315c498
neofytr <imagination12344321.com@gmail.com>
2025-05-02 02:08:32 +0530

    some documentation

--- topics/dynamicProgramming/1D/combination_sum.cpp
+++ topics/dynamicProgramming/1D/combination_sum.cpp
@@ -12,6 +12,8 @@ int main() {
     vector<int> dp(target + 1, 0);
     dp[0] = 1;
     for (int sum = 1; sum <= target; sum++) {
+        // for each possible sum, we count the ways it can be made using previous sums
+        // and a coin
         for (int index = 0; index < n; index++) {
             dp[sum] += (sum - arr[index] >= 0 ? dp[sum - arr[index]] : 0);
         }
