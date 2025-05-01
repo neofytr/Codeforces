abc9f6aee6c51906e27610564aa638e9f0944ab4
neofytr <imagination12344321.com@gmail.com>
2025-05-01 17:11:46 +0530

    tabulation approach

--- topics/dynamicProgramming/1D/unique_paths.cpp
+++ topics/dynamicProgramming/1D/unique_paths.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Solution {
 public:
     static int solve(vector<vector<int> > &dp, const int x, const int y) {
+        // returns the number of ways to get to (x, y) from the origin
         if (x == y && !x) {
             return 1;
         }
@@ -32,7 +33,22 @@ public:
         // from the initial point
 
         vector<vector<int> > dp(m, vector<int>(n, -1));
-        solve(dp, m - 1, n - 1);
+        const int ans = solve(dp, m - 1, n - 1);
+
+        // approach with tabulation
+        for (int x = 0; x < m; x++) {
+            for (int y = 0; y < n; y++) {
+                if (x && y) {
+                    dp[x][y] = dp[x - 1][y] + dp[x][y - 1];
+                } else if (y) {
+                    dp[x][y] = dp[x][y - 1];
+                } else if (x) {
+                    dp[x][y] = dp[x - 1][y];
+                } else {
+                    dp[x][y] = 1;
+                }
+            }
+        }
 
         return dp[m - 1][n - 1];
     }
