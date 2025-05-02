c6036492593abb3e4883d5ec8eb88ef852b19df5
neofytr <imagination12344321.com@gmail.com>
2025-05-02 17:17:01 +0530

    both the best solution and second best

--- topics/dynamicProgramming/1D/partition.cpp
+++ topics/dynamicProgramming/1D/partition.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+ *
+#include <bits/stdc++.h>
+using namespace std;
+
 #define MAX_TARGET (200 * 100)
 
 class Solution {
@@ -20,5 +25,28 @@ public:
         }
 
         return false;
+    }
+};
+ */
+
+class Solution {
+public:
+    static bool canPartition(const vector<int> &arr) {
+        const int total = accumulate(arr.begin(), arr.end(), 0);
+        if (total & 1) return false;
+
+        const int target = total / 2;
+        vector<bool> dp(target + 1, false);
+        // dp[r] is true if there is a subset with sum r, 0 <= r <= target; false otherwise
+        dp[0] = true; // we can make sum 0 (by not taking anything in the subset)
+
+        for (const int num: arr) {
+            // we want to choose an element only once, so we start from the back
+            for (int sum = target; sum >= num; sum--) {
+                dp[sum] = dp[sum] || dp[sum - num];
+            }
+        }
+
+        return dp[target];
     }
 };
