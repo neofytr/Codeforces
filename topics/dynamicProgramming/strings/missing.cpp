d1d06415f1561e12f87b93f30380690b14b4d70d
neofytr <imagination12344321.com@gmail.com>
2025-05-03 22:19:18 +0530

    missing

---
+++ topics/dynamicProgramming/strings/missing.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(const string &str, const int left, const int right, vector<vector<int> > &dp) {
+        if (left >= right) return 0;
+        if (dp[left][right] != -1) return dp[left][right];
+
+        if (str[left] == str[right]) {
+            dp[left][right] = solve(str, left + 1, right - 1, dp);
+        } else {
+            dp[left][right] = 1 + min(
+                                  solve(str, left + 1, right, dp),
+                                  solve(str, left, right - 1, dp)
+                              );
+        }
+        return dp[left][right];
+    }
+
+    int minInsertions(string s) {
+        int n = s.size();
+        vector<vector<int> > dp(n, vector<int>(n, -1));
+        return solve(s, 0, n - 1, dp);
+    }
+};
