2ec51944e10112cf8f9ad08a91d108733e2c808a
neofytr <imagination12344321.com@gmail.com>
2025-05-13 06:47:32 +0530

    added commentary

--- topics/dynamicProgramming/colin_galen_set/lecture.cpp
+++ topics/dynamicProgramming/colin_galen_set/lecture.cpp
@@ -15,13 +15,16 @@ int main()
         cin >> val;
 
     int base = 0;
+
+    // this is the base; when we take all the already available lecture sum
     for (int i = 0; i < n; i++)
     {
         if (behavior[i] == 1)
             base += theorems[i];
     }
 
-    // Build prefix sum for asleep minutes
+    // build prefix sum for asleep minutes
+    // this is how much we will gain if we include all the asleep ones too in the range [0, index - 1]
     vector<int> asleep_prefix(n + 1, 0);
     for (int i = 1; i <= n; i++)
     {
@@ -31,9 +34,11 @@ int main()
     int max_extra = 0;
     for (int i = 0; i <= n - k; i++)
     {
+        // what if we include all the asleep ones from [i, i + k - 1]
         int extra = asleep_prefix[i + k] - asleep_prefix[i];
         max_extra = max(max_extra, extra);
     }
 
     cout << base + max_extra << endl;
+    return EXIT_SUCCESS;
 }
