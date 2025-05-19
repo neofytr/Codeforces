4d176e7bc59c20437dfb443076b3e2c1b30de1b8
neofytr <imagination12344321.com@gmail.com>
2025-05-19 09:15:45 +0530

    Done with final boss

--- topics/binarySearch/problems/final_boss.cpp
+++ topics/binarySearch/problems/final_boss.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool canBeat(long long x, long long h, const vector<long long> &attack, const vector<long long> &cooldown)
+{
+    for (size_t i = 0; i < attack.size(); i++)
+    {
+        long long hits = (x + cooldown[i] - 1) / cooldown[i];
+        h -= hits * attack[i];
+        if (h <= 0)
+            return true;
+    }
+    return false;
+}
+
 void solve()
 {
     long long h, n;
@@ -9,22 +21,29 @@ void solve()
     vector<long long> attack(n);
     vector<long long> cooldown(n);
 
-    for (long long &val : attack)
+    for (long long i = 0; i < n; i++)
+        cin >> attack[i];
+    for (long long i = 0; i < n; i++)
+        cin >> cooldown[i];
+
+    long long left = 0, right = 1e15;
+
+    while (right != left + 1)
     {
-        cin >> val;
+        long long mid = left + (right - left) / 2;
+        if (canBeat(mid, h, attack, cooldown))
+            right = mid;
+        else
+            left = mid;
     }
 
-    for (long long &val : cooldown)
-    {
-        cin >> val;
-    }
+    cout << right << '\n';
 }
 
 int main()
 {
     int t;
     cin >> t;
-
     while (t--)
     {
         solve();
