29c14184073e5c6de6f7810669714a5962d10b91
neofytr <imagination12344321.com@gmail.com>
2025-05-19 13:10:51 +0530

    soln is correct

---
+++ topics/binarySearch/problems/bomb.cpp
@@ -0,0 +1,54 @@
+// https://codeforces.com/contest/1996/problem/F
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long n, k;
+    cin >> n >> k;
+
+    vector<long long> a(n);
+    vector<long long> b(n);
+
+    for (long long &val : a)
+        cin >> val;
+    for (long long &val : b)
+        cin >> val;
+
+    // max-heap of pairs: (value, index)
+    priority_queue<pair<long long, long long>> pq;
+
+    for (long long i = 0; i < n; i++)
+    {
+        if (a[i] > 0)
+            pq.push({a[i], i});
+    }
+
+    long long score = 0;
+
+    for (long long op = 0; op < k && !pq.empty(); op++)
+    {
+        auto [val, idx] = pq.top();
+        pq.pop();
+
+        score += val;
+
+        long long next_val = val - b[idx];
+        if (next_val > 0)
+            pq.push({next_val, idx});
+    }
+
+    cout << score << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
