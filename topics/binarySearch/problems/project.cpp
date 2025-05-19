d7e95f51c522d3f2258298688b0349e6042cf989
neofytr <imagination12344321.com@gmail.com>
2025-05-19 08:04:36 +0530

    project

---
+++ topics/binarySearch/problems/project.cpp
@@ -0,0 +1,52 @@
+// https://atcoder.jp/contests/abc227/tasks/abc227_d
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canMake(long long x, vector<long long> &emp, long long k)
+{
+    // we need to complete x projects in total
+
+    long long total = 0;
+    for (long long num : emp)
+    {
+        // we can take at most min(num, x) employees from this dept to put into
+        // min(num, x) projects
+
+        total += min(num, x);
+    }
+
+    // see if these total employees are sufficient for completing x projects
+    return total >= x * k;
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<long long> emp(n);
+    for (long long &val : emp)
+    {
+        cin >> val;
+    }
+
+    long long left = 0;
+    long long right = 1e15;
+
+    while (right != left + 1)
+    {
+        long long mid = left + (right - left) / 2;
+        if (canMake(mid, emp, k))
+        {
+            left = mid;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    cout << left << endl;
+    return EXIT_SUCCESS;
+}
