155a1eef6c8a9a9f219479b9b8373b9cdbd060a7
neofytr <imagination12344321.com@gmail.com>
2025-05-21 08:35:23 +0530

    working small set

--- topics/twoPointer/EDU/step2/small_set.cpp
+++ topics/twoPointer/EDU/step2/small_set.cpp
@@ -14,23 +14,30 @@ int main()
 
     int left = 0;
     int right = 0;
-    int count = 0;
+    long long count = 0;
+    int size = 0;
 
     unordered_map<long long, int> freq;
 
     while (left < n)
     {
         // move to the maximum right until we can have <= k unique elements
-        while (right < n && freq.size() + (!freq.count(arr[right]) ? 1 : 0) <= k)
+        while (right < n && (size + (!freq[arr[right]] ? 1 : 0)) <= k)
         {
-            freq[arr[right++]]++;
+            if (!freq[arr[right]])
+            {
+                size++;
+            }
+
+            freq[arr[right]]++;
+            right++;
         }
 
         count += (right - left);
 
         if (!(--freq[arr[left]]))
         {
-            freq.erase(freq.find(arr[left]));
+            size--;
         }
 
         left++;
