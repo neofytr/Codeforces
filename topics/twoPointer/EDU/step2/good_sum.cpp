5620f7c3bed2c31638c95be61bdeec7193312bb9
neofytr <imagination12344321.com@gmail.com>
2025-05-21 07:24:23 +0530

    perfect good sum implementation

---
+++ topics/twoPointer/EDU/step2/good_sum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    long long s;
+    cin >> n >> s;
+
+    vector<long long> arr(n);
+    for (long long &val : arr)
+    {
+        cin >> val;
+    }
+
+    // a segment of the array (l, r), where 0 <= l <= r < n, is good if the
+    // sum of the elements in this segment is <= s
+
+    // we need to find the longest good segment, i.e, we need to find
+    // max (r - l + 1, where 0 <= l <= r < n such that  summation (l <= k <= r) of arr[k] <= s)
+
+    // a good segment can start either at index 0, or at 1, ..., or at n - 1
+    // these cases are mutually exclusive and exhaustive
+
+    int left = 0;
+    int right = 0;
+    int max_len = 0;
+    long long sum = 0;
+
+    while (left < n)
+    {
+        while (right < n && sum + arr[right] <= s)
+        {
+            sum += arr[right++];
+        }
+
+        max_len = max(max_len, right - left);
+        sum -= arr[left++];
+    }
+
+    cout << max_len << endl;
+    return EXIT_SUCCESS;
+}
