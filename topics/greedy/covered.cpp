056118ba24f1cb3516e70576ec2e49985a6b9037
neofytr <imagination12344321.com@gmail.com>
2025-05-23 08:46:38 +0530

    checking subsequence of a string efficiently using jump tables

--- topics/greedy/covered.cpp
+++ topics/greedy/covered.cpp
@@ -2,18 +2,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ALPHABET 26
+
+// checks if substr is a subsequence of str using the precomputed jump table
+bool check(vector<char> &str, vector<char> &substr, vector<vector<int>> &table)
+{
+    // start from the beginning of str
+    int next = 0;
+
+    // go through each character in the substr
+    for (char c : substr)
+    {
+        // if we've gone past the end of str, it's not a subsequence
+        if (next >= m)
+        {
+            return false;
+        }
+
+        // jump to the next occurrence of c in str at or after index 'next'
+        next = table[c][next];
+
+        // move to the next character position in str
+        next++;
+    }
+
+    // all characters in substr were found in order in str
+    return true;
+}
+
 void solve()
 {
     int n, k, m;
     cin >> n >> k >> m;
 
     vector<char> str(m);
+    char temp;
     for (char &val : str)
     {
-        cin >> val;
+        cin >> temp;
+        val = temp - 'a';
     }
 
-    
+    // create a jump table to efficiently check whether a string x is a subsequence
+    // of str
+
+    vector<vector<int>> table(ALPHABET, vector<int>(m));
+    // table[c][r] gives the next index of character c in str on and after r; 0 <= r < m; 0 <= c < 26
+
+    for (int c = 0; c < ALPHABET; ++c)
+    {
+        int next = m; // start with m, meaning "not found"
+        for (int i = m - 1; i >= 0; --i)
+        {
+            if (str[i] == c)
+                next = i;
+            table[c][i] = next;
+        }
+    }
 }
 
 int main()
