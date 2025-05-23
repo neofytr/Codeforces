fe08d05db14b76d435be18290b876f401cad6e30
neofytr <imagination12344321.com@gmail.com>
2025-05-23 09:08:59 +0530

    reasonably efficient soln

--- topics/greedy/covered.cpp
+++ topics/greedy/covered.cpp
@@ -5,22 +5,28 @@ using namespace std;
 #define ALPHABET 26
 
 // checks if substr is a subsequence of str using the precomputed jump table
-bool check(vector<char> &str, vector<char> &substr, vector<vector<int>> &table)
+bool check(int len, vector<char> &str, vector<char> &substr, vector<vector<int>> &table)
 {
     // start from the beginning of str
     int next = 0;
 
     // go through each character in the substr
-    for (char c : substr)
+    for (int index = 0; index < len; index++)
     {
         // if we've gone past the end of str, it's not a subsequence
-        if (next >= m)
+        if (next >= str.size())
         {
             return false;
         }
 
         // jump to the next occurrence of c in str at or after index 'next'
-        next = table[c][next];
+        next = table[substr[index]][next];
+
+        // if character not found, return false
+        if (next >= str.size())
+        {
+            return false;
+        }
 
         // move to the next character position in str
         next++;
@@ -30,22 +36,52 @@ bool check(vector<char> &str, vector<char> &substr, vector<vector<int>> &table)
     return true;
 }
 
+bool backtrack(int pos, int n, int k, vector<char> &current, vector<char> &str, vector<vector<int>> &table)
+{
+    if (pos >= n)
+    {
+        return true;
+    }
+
+    for (int index = 0; index < k; index++)
+    {
+        current[pos] = index;
+        if (!check(pos + 1, str, current, table))
+        {
+            cout << "NO\n";
+            for (int i = 0; i <= pos; i++)
+            {
+                cout << (char)(current[i] + 'a'); // Convert back to character
+            }
+            cout << endl;
+            return false;
+        }
+
+        if (!backtrack(pos + 1, n, k, current, str, table))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 void solve()
 {
     int n, k, m;
     cin >> n >> k >> m;
 
+    string input;
+    cin >> input;
+
     vector<char> str(m);
-    char temp;
-    for (char &val : str)
+    for (int i = 0; i < m; i++)
     {
-        cin >> temp;
-        val = temp - 'a';
+        str[i] = input[i] - 'a';
     }
 
     // create a jump table to efficiently check whether a string x is a subsequence
     // of str
-
     vector<vector<int>> table(ALPHABET, vector<int>(m));
     // table[c][r] gives the next index of character c in str on and after r; 0 <= r < m; 0 <= c < 26
 
@@ -59,10 +95,19 @@ void solve()
             table[c][i] = next;
         }
     }
+
+    vector<char> current(n);
+    if (backtrack(0, n, k, current, str, table))
+    {
+        cout << "YES\n";
+    }
 }
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
 
@@ -70,4 +115,6 @@ int main()
     {
         solve();
     }
+
+    return 0;
 }
