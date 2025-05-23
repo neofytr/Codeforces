66c951512b672923bc50ba46db7423605a824bab
neofytr <imagination12344321.com@gmail.com>
2025-05-23 20:19:17 +0530

    total union

--- topics/greedy/setsAndUnion.cpp
+++ topics/greedy/setsAndUnion.cpp
@@ -1,126 +1,76 @@
-// https://codeforces.com/contest/1882/problem/B
-
 #include <bits/stdc++.h>
 using namespace std;
-
-void merge(vector<int> &first, vector<int> &second)
-{
-    vector<int> result;
-    int i = 0, j = 0;
-    while (i < first.size() && j < second.size())
-    {
-        if (first[i] < second[j])
-        {
-            result.push_back(first[i++]);
-        }
-        else
-        {
-            result.push_back(second[j++]);
-        }
-    }
-    while (i < first.size())
-        result.push_back(first[i++]);
-    while (j < second.size())
-        result.push_back(second[j++]);
-    first = move(result);
-}
-
-int dounion(vector<int> &first, vector<int> &second)
-{
-    // first and second are already sorted and each contain all elements different
-
-    int count = 0;
-    int i = 0, j = 0;
-
-    while (i < first.size() && j < second.size())
-    {
-        if (first[i] < second[j])
-        {
-            i++;
-        }
-        else if (first[i] > second[j])
-        {
-            j++;
-        }
-        else
-        {
-            i++, j++;
-        }
-        count++;
-    }
-
-    while (i < first.size())
-    {
-        count++;
-        i++;
-    }
-
-    while (j < second.size())
-    {
-        count++;
-        j++;
-    }
-
-    return count;
-}
 
 void solve()
 {
     int n;
     cin >> n;
 
-    vector<vector<int>> sets(n, vector<int>());
-    unordered_map<int, int> freq;
+    vector<uint64_t> sets(n, 0);
     int k, temp;
-    for (int index = 0; index < n; index++)
+
+    for (int i = 0; i < n; i++)
     {
         cin >> k;
         while (k--)
         {
             cin >> temp;
-            freq[temp]++;
-            sets[index].push_back(temp); // all the sets are already sorted
+            sets[i] |= (1ULL << temp);
         }
     }
 
-    int unique = freq.size();
-
-    vector<bool> chosen(n, false);
-    vector<int> first;
-
-    while (true)
+    uint64_t totalUnion = 0;
+    for (int i = 0; i < n; i++)
     {
-        int max_index = -1;
-        int max_count = INT_MIN;
-        for (int index = 0; index < n; index++)
+        totalUnion |= sets[i];
+    }
+
+    vector<uint64_t> maximal;
+    for (int i = 0; i < n; i++)
+    {
+        bool isSubset = false;
+        for (int j = 0; j < n; j++)
         {
-            if (!chosen[index])
+            if (i != j && (sets[i] | sets[j]) == sets[j])
             {
-                int ans = dounion(first, sets[index]);
-                if (ans > max_count)
-                {
-                    max_count = ans;
-                    max_index = index;
-                }
+                isSubset = true;
+                break;
+            }
+        }
+        if (!isSubset)
+        {
+            maximal.push_back(sets[i]);
+        }
+    }
+
+    int maxSize = 0;
+
+    for (int exclude = 0; exclude < maximal.size(); exclude++)
+    {
+        uint64_t currentUnion = 0;
+        for (int i = 0; i < maximal.size(); i++)
+        {
+            if (i != exclude)
+            {
+                currentUnion |= maximal[i];
             }
         }
 
-        if (max_count >= unique)
+        if (currentUnion != totalUnion)
         {
-            cout << first.size() << endl;
-            return;
+            maxSize = max(maxSize, __builtin_popcountll(currentUnion));
         }
-
-        chosen[max_index] = true;
-        merge(first, sets[max_index]);
     }
+
+    maxSize = max(maxSize, 0);
+
+    cout << maxSize << endl;
 }
 
 int main()
 {
     int t;
     cin >> t;
-
     while (t--)
     {
         solve();
