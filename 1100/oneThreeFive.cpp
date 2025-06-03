a954ed8abdebabe4cc18758cd467a7bc6f40e6f7
neofytr <imagination12344321.com@gmail.com>
2025-06-03 17:33:12 +0530

    one three five

---
+++ 1100/oneThreeFive.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int answer(vector<int> &dp, int num)
+{
+    if (num <= 0)
+        return 0;
+    if (dp[num] != -1)
+        return dp[num];
+
+    switch (num)
+    {
+    case 1:
+        return dp[num] = 1;
+    case 2:
+        return dp[num] = 2;
+    case 3:
+        return dp[num] = 3;
+    case 4:
+        return dp[num] = 1;
+    case 5:
+        return dp[num] = 0;
+    default:
+        return dp[num] = min({1 + answer(dp, num - 1),
+                              answer(dp, num - 3),
+                              answer(dp, num - 5)});
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> dp(n + 1, -1);
+    cout << answer(dp, n) << '\n';
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
