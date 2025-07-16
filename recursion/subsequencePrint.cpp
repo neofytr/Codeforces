1811221998a2d71e8ee17b4cf861a6daa0317590
neofytr <imagination12344321.com@gmail.com>
2025-07-16 21:43:48 +0530

    subsequence printing

--- recursion/subsequencePrint.cpp
+++ recursion/subsequencePrint.cpp
@@ -1,14 +1,36 @@
 #include <bits/stdc++.h>
-#include <cstdlib>
-#include <vector>
 using namespace std;
 
+void printSubsequenceWithSum(int index, int k, int currSum, vector<int> &arr, vector<int> &sub) {
+    int size = (int)arr.size();
+    if (index >= size) {
+        // we have a subsequence now
+        if (currSum == k) {
+            // print the subsequence
+            if (!sub.size())
+                cout << "{}";
+            else
+                for (int val : sub)
+                    cout << val << " ";
+            cout << endl;
+        }
+        return;
+    }
+
+    // we can either choose this index or not
+    sub.push_back(arr[index]);
+    printSubsequenceWithSum(index + 1, k, currSum + arr[index], arr, sub);
+    sub.pop_back();
+    printSubsequenceWithSum(index + 1, k, currSum, arr, sub);
+    return;
+}
+
 void printSubsequence(int index, vector<int> &arr, vector<int> &sub) {
-    int size = (int)sub.size();
+    int size = (int)arr.size();
     if (index >= size) {
         // the subsequence has been built now
         // it can be empty too
-        if (!size)
+        if (!sub.size())
             cout << "{}";
         else
             for (int val : sub)
@@ -23,6 +45,7 @@ void printSubsequence(int index, vector<int> &arr, vector<int> &sub) {
     printSubsequence(index + 1, arr, sub);
     sub.pop_back();
     printSubsequence(index + 1, arr, sub);
+    return;
 }
 
 int main() {
@@ -33,6 +56,7 @@ int main() {
     for (int &val : arr)
         cin >> val;
 
-    printSubsequence(0, arr, sub);
+    // printSubsequence(0, arr, sub);
+    printSubsequenceWithSum(0, 10, 0, arr, sub);
     return EXIT_SUCCESS;
 }
