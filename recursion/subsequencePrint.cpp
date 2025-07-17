69a6dd7ea7c9aa14eecdc4f489faeb9f77c50565
neofytr <imagination12344321.com@gmail.com>
2025-07-17 22:38:20 +0530

    count subsequence with sum

--- recursion/subsequencePrint.cpp
+++ recursion/subsequencePrint.cpp
@@ -1,9 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int countSubsequenceWithSum(int index, int target, vector<int> &arr) {
+    if (index >= (int)arr.size()) {
+        if (!target) {
+            return 1; // found
+        } else
+            return 0;
+    }
+
+    int include = countSubsequenceWithSum(index + 1, target - arr[index], arr);
+    int exclude = countSubsequenceWithSum(index + 1, target, arr);
+    return include + exclude;
+}
+
+bool printSomeSubsequenceWithSum(int index, int k, int currSum, vector<int> &arr, vector<int> &sub) {
+    if (index >= (int)arr.size()) {
+        // we have a subsequence now
+        if (currSum == k) {
+            // print the subsequence
+            if (!sub.size())
+                cout << "{}";
+            else
+                for (int val : sub)
+                    cout << val << " ";
+            cout << endl;
+            return true; // propagate true when found
+        } else
+            return false;
+    }
+
+    sub.push_back(arr[index]);
+    if (printSomeSubsequenceWithSum(index + 1, k, currSum + arr[index], arr, sub)) {
+        // found, no need to continue
+        return true;
+    }
+
+    // not found, try not taking the element
+    sub.pop_back();
+    return printSomeSubsequenceWithSum(index + 1, k, currSum, arr, sub);
+}
+
 void printSubsequenceWithSum(int index, int k, int currSum, vector<int> &arr, vector<int> &sub) {
-    int size = (int)arr.size();
-    if (index >= size) {
+    if (index >= (int)arr.size()) {
         // we have a subsequence now
         if (currSum == k) {
             // print the subsequence
