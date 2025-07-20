3a5f5c1345446090a41bcc0a745be127174525c2
neofytr <imagination12344321.com@gmail.com>
2025-07-20 15:25:48 +0530

    did rotting oranges on my own

--- graph/rottingOranges.cpp
+++ graph/rottingOranges.cpp
@@ -1,11 +1,10 @@
+// https://leetcode.com/problems/rotting-oranges/description/
+
 #include <bits/stdc++.h>
-#include <deque>
-#include <utility>
-#include <vector>
 using namespace std;
 
-#define ROTTEN (2)
-#define FRESH (1)
+#define FRESH 1
+#define ROTTEN 2
 
 class Solution {
   public:
@@ -14,55 +13,80 @@ class Solution {
         int n = (int)grid[0].size();
         int minutes = 0;
         int freshCount = 0;
+        deque<pair<int, int>> que;
+
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 if (grid[row][col] == FRESH) {
                     freshCount++;
+                } else if (grid[row][col] == ROTTEN) {
+                    que.push_back({row, col});
                 }
             }
         }
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        deque<pair<int, int>> rottenBox;
 
-        while (true) {
-            for (int row = 0; row < m; row++) {
-                for (int col = 0; col < n; col++) {
-                    if (!visited[row][col] && grid[row][col] == ROTTEN) {
-                        visited[row][col] = true;
-                        if (row > 0 && grid[row - 1][col] == FRESH) {
-                            rottenBox.push_back(make_pair(row - 1, col));
-                        }
-                        if (row < m - 1 && grid[row + 1][col] == FRESH) {
-                            rottenBox.push_back(make_pair(row + 1, col));
-                        }
-                        if (col > 0 && grid[row][col - 1] == FRESH) {
-                            rottenBox.push_back(make_pair(row, col - 1));
-                        }
-                        if (col < n - 1 && grid[row][col + 1] == FRESH) {
-                            rottenBox.push_back(make_pair(row, col + 1));
-                        }
-                    }
-                }
-            }
+        if (!freshCount) {
+            // if there are no fresh oranges initially, we are done
+            return 0;
+        }
 
-            if (rottenBox.empty() && !freshCount)
-                break;
-            if (rottenBox.empty() && freshCount)
-                return -1;
-
-            while (!rottenBox.empty()) {
-                auto elt = rottenBox.front();
-                rottenBox.pop_front();
-
+        while (!que.empty()) {
+            int size = (int)que.size();
+            bool infected = false;
+            // finish the current level
+            while (size--) {
+                auto elt = que.front();
+                que.pop_front();
                 int row = elt.first;
                 int col = elt.second;
-                grid[row][col] = ROTTEN;
-                freshCount--;
+
+                if (row > 0 && grid[row - 1][col] == FRESH) {
+                    que.push_back({row - 1, col});
+                    // since this pushed elt won't be analyzed until later, it may so happen
+                    // that it's pushed again; so, we edit it's metadata
+                    grid[row - 1][col] = ROTTEN;
+                    freshCount--;
+                    infected = true;
+                }
+
+                if (row < m - 1 && grid[row + 1][col] == FRESH) {
+                    que.push_back({row + 1, col});
+                    // since this pushed elt won't be analyzed until later, it may so happen
+                    // that it's pushed again; so, we edit it's metadata
+                    grid[row + 1][col] = ROTTEN;
+                    freshCount--;
+                    infected = true;
+                }
+
+                if (col > 0 && grid[row][col - 1] == FRESH) {
+                    que.push_back({row, col - 1});
+                    // since this pushed elt won't be analyzed until later, it may so happen
+                    // that it's pushed again; so, we edit it's metadata
+                    grid[row][col - 1] = ROTTEN;
+                    freshCount--;
+                    infected = true;
+                }
+
+                if (col < n - 1 && grid[row][col + 1] == FRESH) {
+                    que.push_back({row, col + 1});
+                    // since this pushed elt won't be analyzed until later, it may so happen
+                    // that it's pushed again; so, we edit it's metadata
+                    grid[row][col + 1] = ROTTEN;
+                    freshCount--;
+                    infected = true;
+                }
             }
 
-            minutes++;
+            if (infected) {
+                // this is so that minutes isn't incremented in the last round when no new
+                // fresh oranges are added in the queue
+                minutes++;
+            }
         }
 
-        return minutes;
+        if (!freshCount)
+            return minutes;
+        else
+            return -1;
     }
 };
