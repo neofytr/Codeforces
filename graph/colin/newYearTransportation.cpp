2ced07e6791a73c172cb0d9d47702009d46c7e3f
neofytr <imagination12344321.com@gmail.com>
2025-07-22 22:05:14 +0530

    did first problem in colin galen problem set

---
+++ graph/colin/newYearTransportation.cpp
@@ -0,0 +1,29 @@
+// https://codeforces.com/gym/317667/problem/A
+
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, t;
+    cin >> n >> t;
+
+    vector<int> arr(n + 1, 0);
+    for (int index = 1; index <= n - 1; index++) {
+        cin >> arr[index];
+    }
+
+    int pos = 1;
+    while (pos != n) {
+        if (pos == t) {
+            cout << "YES\n";
+            return EXIT_SUCCESS;
+        }
+
+        pos += arr[pos];
+    }
+
+    cout << "NO\n";
+    return EXIT_SUCCESS;
+}
