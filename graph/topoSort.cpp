a68fadb12a9c323b5ed318d50035b41ce68cb664
neofytr <imagination12344321.com@gmail.com>
2025-07-23 17:22:50 +0530

    topological sorting using dfs

--- graph/topoSort.cpp
+++ graph/topoSort.cpp
@@ -1,53 +1,55 @@
 #include <bits/stdc++.h>
 #include <cstdlib>
+#include <stack>
 #include <vector>
 using namespace std;
 
-void traverse(int node, vector<bool> &visited, vector<vector<int>> &graph, stack<int> &st) {
+void dfs(int node, vector<bool> &visited, vector<vector<int>> &graph, stack<int> &st) {
     visited[node] = true;
     for (int v : graph[node]) {
         if (!visited[v]) {
-            traverse(v, visited, graph, st);
+            dfs(v, visited, graph, st);
         }
     }
-
-    st.push(node);
+    st.push(node); // starts pushing from the last node
     return;
 }
 
 int main() {
-    // directed, acyclic graph
-    // 0-indexed nodes
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> graph(n, vector<int>());
+
+    // directed acyclic graph
+    vector<vector<int>> graph(n + 1, vector<int>());
     int a, b;
     while (m--) {
         cin >> a >> b; // a -> b
         graph[a].push_back(b);
     }
 
-    vector<int> topoSort;
-    // topological ordering for the nodes of a graph is a linear ordering of the nodes of
-    // the graph such that if there are nodes a and b in the graph such that a -> b, then, in the
-    // ordering, if i and j are the indexed of a and b respectively, then i < j
+    // a topological ordering for a directed graph is a linear ordering of all of it's nodes
+    // such that if there are nodes a and b in the graph such that a -> b, then, a comes before b
+    // in the ordering
 
-    vector<bool> visited(n, false);
+    // a directed graph has a topological ordering only if it doesn't have any cycles
+
     stack<int> st;
-    for (int node = 0; node < n; node++) {
+    vector<bool> visited(n + 1, false);
+    for (int node = 1; node <= n; node++) {
         if (!visited[node]) {
-            traverse(node, visited, graph, st);
+            dfs(node, visited, graph, st);
         }
     }
 
+    vector<int> topoSort;
     while (!st.empty()) {
         topoSort.push_back(st.top());
         st.pop();
     }
 
-    for (int node : topoSort) {
-        cout << node << " ";
+    for (int v : topoSort) {
+        cout << v << " ";
     }
-    cout << endl;
+    cout << "\n";
     return EXIT_SUCCESS;
 }
