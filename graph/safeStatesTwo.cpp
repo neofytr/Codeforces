4f0ffbe0a180c4cdfcdb25bad7a08960eb208b68
neofytr <imagination12344321.com@gmail.com>
2025-07-28 12:52:30 +0530

    did eventual safe states using bfs

--- graph/safeStatesTwo.cpp
+++ graph/safeStatesTwo.cpp
@@ -1,9 +1,10 @@
 // https://leetcode.com/problems/find-eventual-safe-states/
 #include <bits/stdc++.h>
+#include <queue>
 #include <vector>
 using namespace std;
 
-class Solution {
+/* class Solution {
   private:
     bool traverse(int node, vector<int> &dp, vector<bool> &path, vector<vector<int>> &graph) {
         if (dp[node] != -1) {
@@ -36,6 +37,45 @@ class Solution {
             }
         }
 
+        return safe;
+    }
+}; */
+
+class Solution {
+  public:
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph) {
+        int n = (int)graph.size();
+        vector<vector<int>> rev(n);
+        vector<int> inDegree(n, 0);
+        // construct the reverse graph
+        for (int node = 0; node < n; node++) {
+            for (int v : graph[node]) {
+                rev[v].push_back(node);
+                inDegree[node]++;
+            }
+        }
+
+        vector<int> safe;
+        queue<int> que;
+        for (int node = 0; node < n; node++) {
+            if (!inDegree[node]) {
+                que.push(node);
+            }
+        }
+
+        while (!que.empty()) {
+            int node = que.front();
+            que.pop();
+            safe.push_back(node);
+
+            for (int v : rev[node]) {
+                if (!(--inDegree[v])) {
+                    que.push(v);
+                }
+            }
+        }
+
+        sort(safe.begin(), safe.end());
         return safe;
     }
 };
