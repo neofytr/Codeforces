10c9a08c98a7bfa832148832d9ceb9c5b3bab706
neofytr <imagination12344321.com@gmail.com>
2025-08-02 19:03:22 +0530

    done abc 417

--- atcoderContest/ABC/417/d.cpp
+++ atcoderContest/ABC/417/d.cpp
@@ -1,51 +1,84 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-using ll = long long;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int N;
-vector<int> P, A, B;
+    int n;
+    cin >> n;
 
-ll simulate(ll mood) {
-    for (int i = 0; i < N; ++i) {
-        if (P[i] >= mood) {
-            mood += A[i];
-        } else {
-            mood = max(0LL, mood - B[i]);
+    vector<int> p(n + 1), a(n + 1), b(n + 1);
+    int p_max = 0;
+    int a_max = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> p[i] >> a[i] >> b[i];
+        p_max = max(p_max, p[i]);
+        a_max = max(a_max, a[i]);
+    }
+
+    int k_bound = p_max + a_max;
+
+    vector<long long> csb(n + 1, 0);
+    for (int i = 1; i <= n; ++i) {
+        csb[i] = csb[i - 1] + b[i];
+    }
+
+    vector<vector<long long>> final_mood(n + 2, vector<long long>(k_bound + 1));
+
+    for (int m = 0; m <= k_bound; ++m) {
+        final_mood[n + 1][m] = m;
+    }
+
+    for (int i = n; i >= 1; --i) {
+        for (int m = 0; m <= k_bound; ++m) {
+            long long next_mood;
+            if (p[i] >= m) {
+                next_mood = m + a[i];
+            } else {
+                next_mood = m - b[i];
+                if (next_mood < 0) {
+                    next_mood = 0;
+                }
+            }
+            final_mood[i][m] = final_mood[i + 1][next_mood];
         }
     }
-    return mood;
-}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> N;
-    P.resize(N);
-    A.resize(N);
-    B.resize(N);
-
-    for (int i = 0; i < N; ++i) {
-        cin >> P[i] >> A[i] >> B[i];
-    }
-
-    const int MAX_PRE = 2000;
-    vector<ll> precomputed(MAX_PRE + 1);
-    for (int x = 0; x <= MAX_PRE; ++x) {
-        precomputed[x] = simulate(x);
-    }
-
-    int Q;
-    cin >> Q;
-    while (Q--) {
-        ll x;
+    int q;
+    cin >> q;
+    while (q--) {
+        long long x;
         cin >> x;
 
-        if (x <= MAX_PRE) {
-            cout << precomputed[x] << '\n';
+        if (x <= k_bound) {
+            cout << final_mood[1][x] << "\n";
         } else {
-            cout << simulate(x) << '\n';
+
+            long long target_k_csb = x - k_bound;
+            auto it_k = lower_bound(csb.begin(), csb.end(), target_k_csb);
+            int k_minus_1_idx = distance(csb.begin(), it_k);
+            int k = k_minus_1_idx + 1;
+
+            auto it_z = upper_bound(csb.begin(), csb.end(), x);
+            int z = distance(csb.begin(), it_z);
+
+            bool hits_k_bound = (it_k != csb.end());
+            bool hits_zero = (it_z != csb.end());
+
+            if (hits_zero && (!hits_k_bound || z < k)) {
+                cout << final_mood[z + 1][0] << "\n";
+            } else {
+                if (!hits_k_bound) {
+                    cout << x - csb[n] << "\n";
+                } else {
+                    long long mood_at_k = x - csb[k_minus_1_idx];
+                    cout << final_mood[k][mood_at_k] << "\n";
+                }
+            }
         }
     }
 
