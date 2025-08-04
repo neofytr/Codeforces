645a45301a3a96476f4a9c8795459da7e6a6d09e
neofytr <imagination12344321.com@gmail.com>
2025-08-04 06:49:47 +0530

    coded bellman ford by myself

--- graphs/algos/bellmanFord.cpp
+++ graphs/algos/bellmanFord.cpp
@@ -1,74 +1,70 @@
 #include <bits/stdc++.h>
 #include <climits>
+#include <cstdlib>
 #include <vector>
 using namespace std;
 
 int main() {
     int n, m;
-    cin >> n >> m; // n = number of vertices, m = number of edges
+    cin >> n >> m;
 
-    vector<tuple<int, int, int>> edges;
-
-    // reading all edges
-    for (int i = 0; i < m; i++) {
-        int u, v, w;
-        cin >> u >> v >> w; // edge from u to v with weight w
+    // 0-indexed edges
+    // weighted, directed graph
+    vector<tuple<int, int, int>> edges(m);
+    int u, v, w;
+    while (m--) {
+        cin >> u >> v >> w;
         edges.push_back({u, v, w});
     }
 
-    int source;
-    cin >> source; // source node for shortest paths
+    int src;
+    cin >> src;
+    if (src >= n)
+        return EXIT_FAILURE;
 
-    vector<int> dist(n, INT_MAX); // stores shortest distance from source to each node
-    vector<int> parent(n, -1);    // stores parent of each node for path reconstruction
+    vector<int> dist(n, INT_MAX);
+    vector<int> parent(n, 0);
+    for (int x = 0; x < n; x++) {
+        parent[x] = x;
+    }
+    dist[src] = 0;
 
-    dist[source] = 0; // distance to source is 0
-
-    // relax all edges n - 1 times
-    // because the shortest path can have at most n - 1 edges
-    for (int i = 1; i <= n - 1; i++) {
-        for (auto [u, v, w] : edges) {
-            // if u is reachable and going from u to v gives a shorter path
-            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
-                dist[v] = dist[u] + w; // update distance
-                parent[v] = u;         // update parent
+    for (int i = 0; i < n - 1; i++) {
+        for (auto &[u, v, w] : edges) {
+            if (dist[u] != INT_MAX && dist[u] + w < dist[v]) { // relax the edge
+                dist[v] = dist[u] + w;
+                parent[v] = u;
             }
         }
     }
 
-    // one more iteration to check for negative-weight cycles
-    bool hasNegativeCycle = false;
-    for (auto [u, v, w] : edges) {
+    bool negativeCycleDetected = false;
+    for (auto &[u, v, w] : edges) {
         if (dist[u] != INT_MAX && dist[u] + w < dist[v]) {
-            hasNegativeCycle = true;
+            negativeCycleDetected = true;
             break;
         }
     }
 
-    if (hasNegativeCycle) {
-        cout << "negative weight cycle detected" << endl;
-    } else {
-        // print shortest distances and paths
-        for (int i = 0; i < n; i++) {
-            cout << "distance to node " << i << " = ";
-            if (dist[i] == INT_MAX) {
-                cout << "INF" << endl;
-            } else {
-                cout << dist[i] << ", path = ";
-
-                // reconstruct path from source to i
-                vector<int> path;
-                for (int cur = i; cur != -1; cur = parent[cur]) {
-                    path.push_back(cur);
-                }
-                reverse(path.begin(), path.end());
-                for (int x : path) {
-                    cout << x << " ";
-                }
-                cout << endl;
+    if (negativeCycleDetected)
+        cout << "the graph has a negative cycle\n";
+    else {
+        for (int node = 0; node < n; node++) {
+            stack<int> path;
+            int x = node;
+            while (x != parent[x]) {
+                path.push(x);
+                x = parent[x];
             }
+            path.push(x);
+            cout << src << " to " << node << " -> ";
+            while (!path.empty()) {
+                cout << path.top() << " ";
+                path.pop();
+            }
+            cout << "\n";
         }
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
