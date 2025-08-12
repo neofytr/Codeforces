beac660df725a19430a8c4422aae8a0f95c46a1e
neofytr <imagination12344321.com@gmail.com>
2025-08-12 20:26:54 +0530

    practicing binary search

---
+++ binarySearch/problems/fewerBatteries/fewerBatteries.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+// Problem: fewerBatteries
+// URL: https://codeforces.com/problemset/problem/2110/D
+// Difficulty: hard
+// Tags:
+// Strategy:
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+
+    return 0;
+}
