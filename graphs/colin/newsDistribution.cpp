a3101edef151b766e17cf13e9807adee701f51fc
neofytr <imagination12344321.com@gmail.com>
2025-08-15 14:42:54 +0530

    news distribution

---
+++ graphs/colin/newsDistribution.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+// assumes undirected graphs with 0-indexed nodes
+class disjointSetUnion {
+  public:
+    int numNodes;
+    int numComp;
+    vector<int> parent;
+    vector<int> size;
+
+    inline bool checkNode(int x) { return x < numNodes && x >= 0; }
+    inline bool checkNodes(int x, int y) { return checkNode(x) && checkNode(y); }
+    int getRoot(int x) {
+        int y = x;
+        while (x != parent[x])
+            x = parent[x];
+
+        // path compression
+        int temp;
+        while (y != parent[y]) {
+            temp = parent[y]; // store the parent of the current node
+            parent[y] = x;    // make the root node parent of the current node
+            y = temp;         // go to the stored parent node
+        }
+        return x;
+    }
+    inline int getSize(int x) { return size[getRoot(x)]; }
+
+  public:
+    disjointSetUnion(int numNodes) {
+        this->numNodes = numNodes;
+        parent.resize(numNodes);
+        size.resize(numNodes);
+        for (int node = 0; node < numNodes; node++) {
+            parent[node] = node;
+            size[node] = 1; // each node is in it's own connected component
+        }
+        numComp = numNodes;
+        return;
+    }
+
+    bool join(int x, int y) { // joins node x and y via an edge if they are not already joined
+        if (!checkNodes(x, y))
+            return false;
+
+        int rootX = getRoot(x);
+        int rootY = getRoot(y);
+
+        if (rootX == rootY) // already in the same component
+            return true;
+
+        // x and y are not in the same component
+        // they will be after these operations
+        // so decrease the component number by 1
+        numComp--;
+        int sizeX = getSize(x);
+        int sizeY = getSize(y);
+
+        if (sizeX > sizeY) {
+            swap(sizeX, sizeY);
+            swap(rootX, rootY);
+        }
+
+        // from here, sizeY >= sizeX always
+        // attach X root to Y root
+        parent[rootX] = rootY;
+        size[rootY] += sizeX;
+
+        return true;
+    }
+
+    int isConnected(int x, int y) {
+        if (!checkNodes(x, y))
+            return -1;
+
+        return getRoot(x) == getRoot(y);
+    }
+
+    int componentSize(int x) { return checkNode(x) ? getSize(x) : -1; }
+    int numComponents() { return numComp; }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    disjointSetUnion dsu(n);
+
+    while (m--) {
+        int k;
+        cin >> k;
+        if (!k)
+            continue;
+        int x, y;
+        cin >> x;
+        x--;
+        for (int i = 1; i < k; i++) {
+            cin >> y;
+            y--;
+            dsu.join(x, y);
+        }
+    }
+
+    for (int node = 0; node < n; node++) {
+        int root = dsu.getRoot(node);
+        cout << dsu.size[root] << " ";
+    }
+    cout << endl;
+    return EXIT_SUCCESS;
+}
