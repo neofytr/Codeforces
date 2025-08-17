304087f1f4981687b2e8fa0f6b0fe6932a83fb2e
neofytr <imagination12344321.com@gmail.com>
2025-08-17 10:46:31 +0530

    method two

--- twoPointers/algos/smaller.cpp
+++ twoPointers/algos/smaller.cpp
@@ -17,6 +17,7 @@ int main() {
     sort(A.begin(), A.end());
     sort(B.begin(), B.end());
 
+    // method one
     int k = 0;
     int r = 0;
     vector<int> ans(m, 0);
@@ -28,5 +29,22 @@ int main() {
 
     for (int i = 0; i < m; i++)
         cout << B[i] << " -> " << ans[i] << endl;
+
+    cout << endl;
+
+    // method two
+    vector<int> ansTwo(m, 0);
+    int i = 0, j = 0;
+    while (i < n && j < m)
+        if (A[i] < B[j])
+            i++;
+        else
+            ansTwo[j++] = i;
+
+    while (j < m)
+        ansTwo[j++] = n;
+    for (int i = 0; i < m; i++)
+        cout << B[i] << " -> " << ansTwo[i] << endl;
+
     return EXIT_SUCCESS;
 }
