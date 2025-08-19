4bcdfd4fa9ace1b8f09f1972d1387aa820974be0
neofytr <imagination12344321.com@gmail.com>
2025-08-19 17:50:10 +0530

    Test

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
+#include <variant>
 using namespace std;
 class Vector {
-  public:
-    // constructors defined for various situations
-    Vector() {
-        elt = nullptr;
-        sz = 0;
-        return;
-    }
-    Vector(int s) {
-        elt = new double[s];
-        sz = s;
-        return;
-    } // constructor
-    double &operator[](int i) {
-        return elt[i];
-    } // defining what the operator [] means in context of Vectors
-      // we return a reference so that the caller can also edit the value
-    int size() { return sz; } // get the size of the vector
+public:
+  // constructors defined for various situations
+  Vector() {
+    elt = nullptr;
+    sz = 0;
+    return;
+  }
+  Vector(int s) {
+    elt = new double[s];
+    sz = s;
+    return;
+  } // constructor
+  double &operator[](int i) {
+    return elt[i];
+  } // defining what the operator [] means in context of Vectors
+    // we return a reference so that the caller can also edit the value
+  int size() { return sz; } // get the size of the vector
 
-  private:
-    double *elt; // pointer to the elements
-    int sz;      // the number of elements
+private:
+  double *elt; // pointer to the elements
+  int sz;      // the number of elements
 };
 
 /*
@@ -30,7 +31,8 @@ both classes and structs are identical in terms of their features
 except for the following two things ->
 
 1. in a struct, members are public by default; the opposite is true for classes
-2. struct uses public inheritance by default; class uses private inheritance by default
+2. struct uses public inheritance by default; class uses private inheritance by
+default
 
 */
 
@@ -45,8 +47,9 @@ blue
 };
 
 Underlying type -> usually int (but implementation-defined, can be smaller)
-Scope -> enumerator names are injected into the score surrounding the declaration
-Enumerators can be implicitly converted to int and compared with integers
+Scope -> enumerator names are injected into the score surrounding the
+declaration Enumerators can be implicitly converted to int and compared with
+integers
 
 Issues ->
 1. No type safety (different enums can be compared)
@@ -76,42 +79,74 @@ Color c = Color::Red // must qualify with scope
 No implicit conversion to int. Must cast explicitly
 int x = (int) Color::Red
 
-The class after the enum specifies that an enumeration is strongly typed and that it's enumerators
-are scoped.
-Initializing an enum with a value from it's underlying type (by default, thats int) is allowed ->
-Color x = Color{1};
-Color y {1};
+The class after the enum specifies that an enumeration is strongly typed and
+that it's enumerators are scoped. Initializing an enum with a value from it's
+underlying type (by default, thats int) is allowed -> Color x = Color{1}; Color
+y {1};
 
-By default, enum class has assignment, initialization, and comparisons defined, and only those.
-However, an enumeration is a user-defined type, so we can define operators for it ->
+By default, enum class has assignment, initialization, and comparisons defined,
+and only those. However, an enumeration is a user-defined type, so we can define
+operators for it ->
 
 */
 
 enum class trafficLight : unsigned char { red, yellow, green };
 trafficLight &operator++(trafficLight &t) {
-    using enum trafficLight; // make the score trafficLight
-    switch (t) {
-    case green:
-        return t = red;
-    case yellow:
-        return t = green;
-    case red:
-        return t = yellow;
-    }
+  using enum trafficLight; // make the score trafficLight
+  switch (t) {
+  case green:
+    return t = red;
+  case yellow:
+    return t = green;
+  case red:
+    return t = yellow;
+  }
+}
+
+/* Using a tagged / discriminated union */
+
+union Value {
+  double f;
+  int i;
+};
+
+enum class Type : unsigned char { real, integer };
+
+struct Entry {
+  string name;
+  Type t; // specifies which type of value the union v is
+  Value v;
+};
+
+/* Using variant, a type-safe union */
+/* Here, v can hold either a double, or an int, but not both */
+/* Unlike unions, the compiler enforces correctness: */
+/* you can ask holds_alternative<int>(v) -> is it an int? */
+/* you can safely get it -> get<int>(v) */
+
+struct EntryVariant {
+  string name;
+  variant<double, int> v;
+};
+
+void f(EntryVariant *ev) {
+  if (holds_alternative<int>(ev->v)) {
+    cout << get<int>(ev->v);
+  }
 }
 
 int main() {
-    int n;
-    cin >> n;
-    Vector vec(n);
-    for (int i = 0; i < vec.size(); i++) {
-        cin >> vec[i];
-    }
+  int n;
+  cin >> n;
+  Vector vec(n);
+  for (int i = 0; i < vec.size(); i++) {
+    cin >> vec[i];
+  }
 
-    double sum = 0;
-    for (int i = 0; i < vec.size(); i++) {
-        sum += vec[i];
-    }
-    cout << sum;
-    return EXIT_SUCCESS;
+  double sum = 0;
+  for (int i = 0; i < vec.size(); i++) {
+    sum += vec[i];
+  }
+  cout << sum;
+  return EXIT_SUCCESS;
 }
