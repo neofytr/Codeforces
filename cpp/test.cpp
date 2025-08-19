f4b45da80683f46ad0a02a2e5360f8a808fe5896
neofytr <imagination12344321.com@gmail.com>
2025-08-19 14:52:09 +0530

    enum class

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -34,6 +34,72 @@ except for the following two things ->
 
 */
 
+/*
+
+Traditional Enums (enum)
+
+enum Color {
+red,
+green,
+blue
+};
+
+Underlying type -> usually int (but implementation-defined, can be smaller)
+Scope -> enumerator names are injected into the score surrounding the declaration
+Enumerators can be implicitly converted to int and compared with integers
+
+Issues ->
+1. No type safety (different enums can be compared)
+2. Name pollution (all enum names are in the enclosing scope)
+
+*/
+
+/*
+
+Scoped Enums (enum class)
+
+enum class Color {
+    Red,
+    Green,
+    Blue,
+}
+
+1. Underlying type
+Default is int, but can explicitly specify:
+enum class Color : unsigned char {red, green, blue}
+
+2. Scope
+Enumerator names are scoped inside the enum
+Color c = Color::Red // must qualify with scope
+
+3. Implicit Conversions
+No implicit conversion to int. Must cast explicitly
+int x = (int) Color::Red
+
+The class after the enum specifies that an enumeration is strongly typed and that it's enumerators
+are scoped.
+Initializing an enum with a value from it's underlying type (by default, thats int) is allowed ->
+Color x = Color{1};
+Color y {1};
+
+By default, enum class has assignment, initialization, and comparisons defined, and only those.
+However, an enumeration is a user-defined type, so we can define operators for it ->
+
+*/
+
+enum class trafficLight : unsigned char { red, yellow, green };
+trafficLight &operator++(trafficLight &t) {
+    using enum trafficLight; // make the score trafficLight
+    switch (t) {
+    case green:
+        return t = red;
+    case yellow:
+        return t = green;
+    case red:
+        return t = yellow;
+    }
+}
+
 int main() {
     int n;
     cin >> n;
