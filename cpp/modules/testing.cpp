b7559ade6931216c67464473e5d85eae566dfdcc
neofytr <imagination12344321.com@gmail.com>
2025-08-20 20:13:20 +0530

    dynamic casting and other fun stuff

--- cpp/modules/testing.cpp
+++ cpp/modules/testing.cpp
@@ -3,7 +3,6 @@ import Container;
 import List;
 import <iostream>;
 using namespace std;
-
 /*
  * For a function like calc to use a Container in complete ignorance of its implementation details,
  * some other function will have to make an object on which it can operate. This is what main does.
@@ -20,6 +19,37 @@ double calc(Container &c) {
     double sum = 0;
     for (int i = 0; i < static_cast<int>(c.size()); i++)
         sum += c[i];
+
+    /*
+     * What can we do if we want to use a member function that is only provided by a particular derived class?
+     * We can ask "is this Container a kind of List" using the dynamic_cast operator:
+     */
+
+    /*
+     * If at run-time, the object pointed to by the argument of dynamic_cast (here, &c) is not of the
+     * expected type(here, List *), or a class derived from the expected type (so that it at-least has the
+     * interfaces we want to use), dynamic_cast returns nullptr
+     */
+
+    /*
+     * We use dynamic cast to a pointer type when a pointer to an object of a different derived class
+     * is a valid argument. We can test whether the result is nullptr. This test can often conveniently
+     * be placed in the initialization of a variable in a condition.
+     */
+    if (const auto lst = dynamic_cast<List *>(&c)) {
+        lst->push(696969);
+        sum += (*lst)[c.size() - 1];
+    }
+
+    /*
+     * When a different type is unacceptable, we can simply dynamic_cast to a reference type.
+     * If the object is not of the expected type, dynamic_cast throws a bad_cast exception:
+     */
+    try {
+        auto &lst = dynamic_cast<List &>(c);
+    } catch (const std::bad_cast &) {
+        // c is not a list
+    }
     return sum;
 }
 
