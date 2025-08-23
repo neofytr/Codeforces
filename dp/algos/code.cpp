2bc282189b0f4c3fe600832b0fe7e051e70bc0ea
neofytr <imagination12344321.com@gmail.com>
2025-08-23 17:26:35 +0530

    grid

--- dp/algos/code.cpp
+++ dp/algos/code.cpp
@@ -28,6 +28,33 @@ void fib() {
     cout << solveFib(n, dp) << endl;
 }
 
+int solveGrid(int r, int c, vector<vector<int>> &g, vector<vector<int>> &dp) {
+    int n = (int)g.size();
+    int m = (int)g[0].size();
+
+    if (r >= n || c >= m)
+        return LLONG_MAX;
+
+    if (dp[r][c] != -1)
+        return dp[r][c];
+
+    return g[r][c] + min(solveGrid(r + 1, c, g, dp), solveGrid(r, c + 1, g, dp));
+}
+
+void gridTwo() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> g(n, vector<int>(m));
+    for (int row = 0; row < n; row++)
+        for (int col = 0; col < m; col++)
+            cin >> g[row][col];
+
+    // dp[r][c] gives the minimum sum of values path from (r, c) to (n - 1, m - 1)
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    cout << solveGrid(0, 0, g, dp) << endl;
+}
+
 void grid() {
     int n, m;
     cin >> n >> m;
@@ -37,19 +64,21 @@ void grid() {
         for (int col = 0; col < m; col++)
             cin >> g[row][col];
 
+    // we can only move down or right from any cell
+
     // dp[r][c] gives the minimum sum of values path from (0, 0) to (r, c)
     vector<vector<int>> dp(n, vector<int>(m, LLONG_MAX));
 
     queue<tuple<int, int>> que;
-    int dr[] = {0, -1, 0, +1};
-    int dc[] = {-1, 0, +1, 0};
+    int dr[] = {0, +1};
+    int dc[] = {+1, 0};
     dp[0][0] = g[0][0];
     que.push({0, 0});
     while (!que.empty()) {
         auto [row, col] = que.front();
         que.pop();
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 2; i++) {
             int drow = row + dr[i];
             int dcol = col + dc[i];
             if (drow >= 0 && dcol >= 0 && drow < n && dcol < m) {
@@ -64,7 +93,19 @@ void grid() {
     cout << dp[n - 1][m - 1] << endl;
 }
 
+void
+
 int32_t main() {
-    grid();
+    /*
+     * States and Transitions
+     *
+     * State -> A subproblem that we want to solve. The subproblem may be complex or easy to solve
+     * but the final aim is to solve the final problem which may be defined by a relation between the smaller
+     * subproblems. A state is represented with some parameters.
+     *
+     * Transition -> Calculating the answer for a state (subproblem) by using the answers of
+     * other smaller states (subproblems). This is represented as a relation between states.
+     *
+     */
     return 0;
 }
