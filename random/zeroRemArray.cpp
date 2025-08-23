4960791f8b37c447dda5782b947c504d404cb64d
neofytr <imagination12344321.com@gmail.com>
2025-08-23 14:19:46 +0530

    zero rem array

--- random/zeroRemArray.cpp
+++ random/zeroRemArray.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 #define int long long
 
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+
 int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -17,7 +30,7 @@ int32_t main() {
         for (int &val : arr)
             cin >> val;
 
-        unordered_map<int, int> freq;
+        unordered_map<int, int, custom_hash> freq;
 
         for (const int val : arr) {
             if (const int r = val % k) {
