a7a4810b8967b40a6c973b127a4d9077f004529b
neofytr <imagination12344321.com@gmail.com>
2025-08-23 19:09:34 +0530

    minimizing coins

--- dp/problems/minimizingCoins/minimizingCoins.cpp
+++ dp/problems/minimizingCoins/minimizingCoins.cpp
@@ -13,25 +13,30 @@ int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    // consider a money system consisting of n coins
+    // each coin has a positive integer value
+    // our task is to produce a sum of money x using the available coins in such a way that the
+    // number of coins used is minimal
+
     int n, x;
     cin >> n >> x;
-
     vector<int> coins(n);
     for (int &val : coins)
         cin >> val;
 
-    // dp[r] is the minimum number of coins required to make the sum r
+    // dp[r] is the minimum number of coins used to produce the sum r
     vector<int> dp(x + 1, LLONG_MAX);
+    // all the coin denominations can be made using exactly one coin
     for (const int c : coins)
         if (c <= x)
             dp[c] = 1;
 
     for (int r = 1; r <= x; r++) {
+        // dp[r] = min (over all c in coins such that r >= c and dp[r - c] != LLONG_MAX) (dp[r - c] + 1)
         for (const int c : coins)
-            if (r - c >= 1 && dp[r - c] != LLONG_MAX)
-                dp[r] = min(dp[r], 1 + dp[r - c]);
+            if (r >= c && dp[r - c] != LLONG_MAX)
+                dp[r] = min(dp[r], dp[r - c] + 1);
     }
-
     if (dp[x] == LLONG_MAX)
         cout << -1 << endl;
     else
