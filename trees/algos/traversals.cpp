b6f13b676bde15f5b8122b3fe3beddaa6a1840db
neofytr <imagination12344321.com@gmail.com>
2025-08-30 19:20:31 +0530

    all traversals revised

--- trees/algos/traversals.cpp
+++ trees/algos/traversals.cpp
@@ -4,88 +4,87 @@ using namespace std;
 #define int long long
 
 struct Node {
+    struct Node *left;
+    struct Node *right;
     int data;
-    Node *left;
-    Node *right;
-    explicit Node(const int data) : data(data), left(nullptr), right(nullptr) {}
+
+    explicit Node(const int data) {
+        this->data = data;
+        left = nullptr;
+        right = nullptr;
+    }
 };
 
-Node *buildTree(const int node, vector<vector<int>> &children) {
-    auto *newNode = new Node(node);
-    if (children[node].size() >= 1) {
-        newNode->left = buildTree(children[node][0], children);
-    }
-    if (children[node].size() == 2) {
-        newNode->right = buildTree(children[node][1], children);
-    }
-    return newNode;
-}
-
-void preorder(const Node *node) {
-    // root, left, right
-    if (!node)
-        return;
-
-    cout << node->data << " ";
-    preorder(node->left);
-    preorder(node->right);
-}
-
-void postorder(const Node *node) {
-    // left, right, root
-    if (!node)
-        return;
-
-    postorder(node->left);
-    postorder(node->right);
-    cout << node->data << " ";
+Node *buildTree(const int nd, const vector<vector<int>> &children) {
+    const auto node = new Node(nd);
+    if (children[nd].size() >= 1)
+        node->left = buildTree(children[nd][0], children);
+    if (children[nd].size() >= 2)
+        node->right = buildTree(children[nd][1], children);
+    return node;
 }
 
 void inorder(const Node *node) {
-    // left, root, right
     if (!node)
         return;
+    // left, root, right
 
     inorder(node->left);
     cout << node->data << " ";
     inorder(node->right);
 }
 
+void preorder(const Node *node) {
+    if (!node)
+        return;
+
+    // root, left, right
+    cout << node->data << " ";
+    preorder(node->left);
+    preorder(node->right);
+}
+
+void postorder(const Node *node) {
+    if (!node)
+        return;
+
+    // left, right, root
+    postorder(node->left);
+    postorder(node->right);
+    cout << node->data << " ";
+}
+
 int32_t main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    // 0-indexed nodes
+    // guaranteed to be a binary tree
 
     int n, rt;
     cin >> n >> rt;
 
-    // 0-indexed nodes
-    // the input is guaranteed to be a binary tree
-    // we follow the convention that->
-    // if u -> v comes first and u -> w comes next, then v is the left node and w is the right node of u
+    int u, v;
     vector<vector<int>> children(n);
-    for (int i = 0; i < n - 1; i++) {
-        int u, v;
+    for (int r = 1; r <= n - 1; r++) {
         cin >> u >> v;
-        children[u].push_back(v); // only store directed edge
+        children[u].push_back(v);
     }
 
-    const Node *root = buildTree(rt, children);
+    struct Node *root = buildTree(rt, children);
     inorder(root);
     cout << endl;
-    postorder(root);
-    cout << endl;
     preorder(root);
+    cout << endl;
+    postorder(root);
     cout << endl;
 
     // BFS
     int src = rt;
-
     queue<int> que;
+
     que.push(src);
     while (!que.empty()) {
         int x = que.front();
-        que.pop();
         cout << x << " ";
+        que.pop();
         for (int v : children[x])
             que.push(v);
     }
