53c6852e28bff2aa3ead063eeebe823992dc4eec
neofytr <imagination12344321.com@gmail.com>
2025-08-30 17:44:40 +0530

    tree representation

--- trees/algos/code.cpp
+++ trees/algos/code.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 
 #define int long long
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(const int data) {
+        this->data = data;
+        return;
+    }
+    Node(const int val, struct Node *left, struct Node *right) {
+        data = val;
+        this->left = left;
+        this->right = right;
+        return;
+    }
+};
+
 int32_t main() {
     return EXIT_SUCCESS;
 }
