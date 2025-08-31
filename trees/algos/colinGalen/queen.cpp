0bf694291beddaa4eda8120c037fbf67cb0818fb
neofytr <imagination12344321.com@gmail.com>
2025-08-31 23:30:56 +0530

    removable property and invariancy

--- trees/algos/colinGalen/queen.cpp
+++ trees/algos/colinGalen/queen.cpp
@@ -5,5 +5,69 @@ using namespace std;
 #define int long long
 
 int32_t main() {
+    int n;
+    cin >> n;
+
+    int pr, root;
+    vector<vector<int>> tree(n + 1);
+    vector<int> respect(n + 1);
+    for (int r = 1; r <= n; r++) {
+        cin >> pr >> respect[r];
+        if (pr == -1)
+            root = r;
+        else
+            tree[pr].push_back(r);
+    }
+
+    // We have to delete vertices from the tree one by one
+    // In each step, we select a vertex r (r != root) such that respect[r] = 1 and respect[c] = 1 for all children c of r
+    // If there are a lot of these vertices, we choose the one with the smallest number
+    // We then delete this vertex r and attach all of its children to the parent of r
+    // We continue this process until we have no such vertices left in the tree
+
+    // We call a vertex v != root removable iff
+    // 1. respect[v] = 1
+    // 2. respect[c] = 1 for all children c of v
+
+    // If a non-root vertex v is removable initially, it will be removable after a deletion of any other removable vertex
+    // Let v and u are removable non-root vertices initially with v != u
+    // If there is no edge between v and u, removal of u won't affect the removability of v
+    // If there is an edge between u and v, then, either u is the parent of v, or v is the parent of u
+    // 1. u is the parent of v
+    // In this case, v will be attached to the parent of u and it's removability won't be affected
+    // 2. v is the parent of u
+    // In this case, the children of u will be attached to v, but since they all have respect == 1 (since u was removable),
+    // v will still be removable
+
+    // Also, if a node isn't removable initially, it can't become removable after a deletion
+
+    // Thus, the number of removable nodes is always a constant
+    // So, we just print all the removable nodes in sorted order
+
+    set<int> removable;
+
+    const int src = root;
+    queue<int> que;
+    que.push(src);
+    while (!que.empty()) {
+        int node = que.front();
+        que.pop();
+
+        bool isRem = true;
+        for (int v : tree[node]) {
+            if (!respect[v])
+                isRem = false;
+            que.push(v);
+        }
+        if (isRem && respect[node] && node != root)
+            removable.insert(node);
+    }
+    if (removable.empty()) {
+        cout << -1 << "\n";
+    } else {
+        for (const int v : removable)
+            cout << v << " ";
+        cout << "\n";
+    }
     return EXIT_SUCCESS;
 }
