6ae057fc56930c03f4aa37608ead153e102c4022
neofytr <imagination12344321.com@gmail.com>
2025-08-31 23:02:16 +0530

    tree

---
+++ trees/algos/colinGalen/notes.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+int32_t main() {
+    // Let G is a graph of n vertices
+    // G is a tree
+    // iff G is connected and acyclic
+    // iff G is connected and has n - 1 edges
+    // iff G is acyclic and has n - 1 edges
+    // iff G has a unique path between any two different vertices
+    // iff G is maximally acyclic
+    // iff G is minimally connected
+
+    // Let G is a tree
+    // Then, for every node except the root, there is a unique parent
+}
---
+++ trees/algos/colinGalen/queen.cpp
@@ -0,0 +1,9 @@
+// https://codeforces.com/gym/304970/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    return EXIT_SUCCESS;
+}
