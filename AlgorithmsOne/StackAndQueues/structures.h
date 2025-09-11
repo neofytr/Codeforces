f3ba5c5307198f4adaa6ec743bf0b8bf9085adde
neofytr <imagination12344321.com@gmail.com>
2025-09-11 16:56:40 +0530

    dijkstras two stack algorithm

--- AlgorithmsOne/StackAndQueues/structures.h
+++ AlgorithmsOne/StackAndQueues/structures.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <stdexcept>
 using namespace std;
 
@@ -112,3 +113,58 @@ template <typename type> class Queue {
     Iterator begin() { return Iterator(front); }
     Iterator end() { return Iterator(nullptr); }
 };
+
+// Stack applications
+// 1. Parsing in a compiler
+// 2. Java virtual machine
+// 3. Undo in a word processor
+// 4. Back button in a Web Browser
+// 5. PostScript language for printers
+// 6. Implementing function calls in a compiler, ... and so on
+
+// Function Calls
+// How a compiler implements a function?
+// Function Call : push local environment and return address
+// Return : pop return address and local environment
+
+// Recursive function -> function that calls itself
+// Note -> can always use an explicit stack to remove recursion
+
+int main() {
+    Stack<char> ops;
+    Stack<int> vals;
+
+    cout << "Enter expression: ";
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+    string token;
+
+    while (iss >> token) {
+        if (token == "(") {
+        } else if (token == "+" || token == "*") {
+            ops.push(token[0]);
+        } else if (token == ")") {
+            const char op = ops.pop();
+            const int v2 = vals.pop();
+            const int v1 = vals.pop();
+            int result = 0;
+            if (op == '+') {
+                result = v1 + v2;
+            } else if (op == '*') {
+                result = v1 * v2;
+            }
+            vals.push(result);
+        } else {
+            vals.push(stoi(token));
+        }
+    }
+
+    if (!vals.isEmpty()) {
+        cout << "Result: " << vals.pop() << endl;
+    } else {
+        cout << "No expression evaluated." << endl;
+    }
+
+    return 0;
+}
