989f083646b3dd327ed49e9872b54da3e48e2e48
neofytr <imagination12344321.com@gmail.com>
2025-10-18 15:02:48 +0530

    sliding window median

--- slidingWindow/algos/slidingWindowMedian.cpp
+++ slidingWindow/algos/slidingWindowMedian.cpp
@@ -65,3 +65,75 @@ void erase(const int x) {
         hi.erase(x);
     balance();
 }
+
+// Let S is a non-empty set of integers with atleast 2 elements
+// Let x1, x2, ..., xn are the elements of S in non-decreasing order, where n = |S|
+// We define the median of S as follows
+// 1. If n is even, it is x(n / 2)
+// 2. If n is odd, it is x((n + 1) / 2)
+
+// The following collection supports insertion and deletion of integers in O(log(n))
+// and can query the median of the collection in O(1), where n is the size of the collection
+// before insertion/deletion
+
+class MedCol {
+private:
+    int nelts = 0;
+    multiset<int> lo;
+    multiset<int> hi;
+    void rebalance() {
+        if (lo.size() > hi.size() + 1) {
+            int n = *lo.rbegin();
+            hi.insert(n);
+            lo.erase(prev(lo.end()));
+            loSum -= n, hiSum += n;
+        } else if (hi.size() > lo.size()) {
+            int n = *hi.begin();
+            lo.insert(n);
+            hi.erase(hi.begin());
+            loSum += n, hiSum -= n;
+        }
+    }
+
+    // We maintain the following invariants:
+    // 1. Any element of the collection is either in lo or hi
+    // 2. lo.size() >= hi.size() >= 0
+    // 3. 0 <= (lo.size() - hi.size()) <= 1
+
+    // If the above invariants are maintained and the collection isn't empty,
+    // the largest element of lo (*lo.rbegin()) is the median of the collection
+
+public:
+    int loSum = 0;
+    int hiSum = 0;
+
+    void insert(int x) {
+        if (!nelts || x <= *lo.rbegin())
+            lo.insert(x), loSum += x;
+        else 
+            hi.insert(x), hiSum += x;
+
+        rebalance();
+        nelts++;
+    }
+    void del(int x) {
+        if (lo.find(x) != lo.end())
+            lo.erase(lo.find(x)), loSum -= x;
+        else if (hi.find(x) != hi.end())
+            hi.erase(hi.find(x)), hiSum -= x;
+        else 
+            return; // x is not in the collection, or the collection is empty
+        rebalance();
+        nelts--;
+    }
+
+    int median() const {
+        if (!nelts)
+            runtime_error("median of an empty collection\n");
+        return *lo.rbegin();
+    }
+
+    int idx() const { return lo.size() - 1;}
+    bool empty() const {return !nelts;}
+    int size() const {return nelts;}
+};
