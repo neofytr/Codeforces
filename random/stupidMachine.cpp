f84685029e20a7bb4b1342707726c140dc4b0b18
neofytr <imagination12344321.com@gmail.com>
2025-10-21 10:47:15 +0530

    did stupid machine

--- random/stupidMachine.cpp
+++ random/stupidMachine.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define int long long
 
 void solve() {
     int n;
@@ -9,10 +10,28 @@ void solve() {
     int val;
     priority_queue<pair<int, int>> pq;
     for (int r = 1; r <= n; r++)
-        cin >> val, pq.push({val, r});
+        cin >> val, pq.push({-val, r});
+
+    int lastIdx = n + 1;
+    int lastVal = 0;
+    int sum = 0;
+    while (!pq.empty()) {
+        auto [val, idx] = pq.top();
+        val = -val;
+        pq.pop();
+        if (idx > lastIdx)
+            continue;
+
+        const int gain = max(0LL, val - lastVal);
+        sum += gain * (lastIdx - 1);
+        lastIdx = idx;
+        lastVal = val;
+    }
+
+    cout << sum << endl;
 }
 
-int main() {
+int32_t main() {
     int t;
     cin >> t;
 
