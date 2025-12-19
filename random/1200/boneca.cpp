b2f32e8dc5b6a484f224fad6d0fb8e9527d6a02f
neofytr <imagination12344321.com@gmail.com>
2025-12-19 21:00:01 +0530

    Practiced CF problems

---
+++ random/1200/boneca.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+void solve() {
+	int n;
+	cin >> n;
+
+	int maxi = LLONG_MIN;
+	vector<int> arr(n + 1);
+	for (int r = 1; r <= n; r++) {
+		cin >> arr[r];
+		maxi = max(maxi, arr[r]);
+	}
+
+	int res = 0;
+	for (int r = 1; r <= n; r++)
+		res += (maxi ^ arr[r]);
+
+	cout << res << endl;
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
