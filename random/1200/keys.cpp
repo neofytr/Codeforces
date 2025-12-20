f892134b80b7e8d44c30a40f1cd74de2fc43241e
neofytr <imagination12344321.com@gmail.com>
2025-12-20 13:00:01 +0530

    Solved problem

---
+++ random/1200/keys.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+void solve() {
+	int n, x;
+	cin >> n >> x;
+
+	if (n == 1) {
+		cout << x << endl;
+		return;
+	}
+
+	vector<int> arr = {x};
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
