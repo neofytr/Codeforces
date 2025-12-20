45f3cdb0d6895e28e7fffa04feaaf900aaf6c451
neofytr <imagination12344321.com@gmail.com>
2025-12-20 23:46:21 +0530

    counting pairs learnt something

--- random/1200/countingPairs.cpp
+++ random/1200/countingPairs.cpp
@@ -7,20 +7,33 @@ void solve() {
 	int n, x, y;
 	cin >> n >> x >> y;
 
+	int sum = 0;
 	vector<int> arr(n + 1);
-	for (int r = 1; r <= n; r++) cin >> arr[r];
+	for (int r = 1; r <= n; r++) cin >> arr[r], sum += arr[r];
 
 	// 1 <= i < j <= n
 	// sum - y - arr[j] <= arr[i] <= sum - x - arr[j]
 
+	// sum - y <= arr[i] + arr[j] <= sum - x
+
+	sort(arr.begin() + 1, arr.end());
+	vector<int> v;
+	v.push_back(arr[1]);
 	int cnt = 0;
-	multiset<int> ms;
-	ms.insert(arr[1]);
-	
 	for (int j = 2; j <= n; j++) {
 		int left = sum - y - arr[j];
 		int right = sum - x - arr[j];
+
+		auto leftitr = lower_bound(v.begin(), v.end(), left);
+		if (leftitr != v.end()) {
+			auto rightitr = lower_bound(v.begin(), v.end(), right + 1);
+			cnt += rightitr - leftitr;
+		}
+
+		v.push_back(arr[j]);
 	}
+
+	cout << cnt << endl;
 }
 
 int32_t main() {
