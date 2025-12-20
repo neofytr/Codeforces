ffdcca96bc4ef831c46b7fe411d729b588629fdd
neofytr <imagination12344321.com@gmail.com>
2025-12-20 14:00:02 +0530

    Solved some problems

--- random/1200/keys.cpp
+++ random/1200/keys.cpp
@@ -12,7 +12,35 @@ void solve() {
 		return;
 	}
 
-	vector<int> arr = {x};
+	vector<int> arr;
+	bool stop = false;
+	for (int r = 0; r <= n - 2 && !stop; r++) {
+		for (int bit = 0; bit <= 29; bit++)
+			if (!(x & (1ll << bit)) && (r & (1ll << bit))) {
+				stop = true;
+				break;
+			}
+		if (!stop)
+			arr.push_back(r);
+	}
+
+	if (arr.size() == n - 1) {
+		int OR = n - 1;
+		for (int e : arr)
+			OR |= e;
+		if (OR == x)
+			arr.push_back(n - 1);
+		else 
+			arr.push_back(x);
+	} else {
+		int left = n - arr.size();
+		for (int r = 1; r <= left; r++)
+			arr.push_back(x);
+	}
+
+	for (int e : arr)
+		cout << e << " ";
+	cout << endl;
 }
 
 int32_t main() {
---
+++ random/1200/setStrangers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<int>> mat(n + 1, vector<int>(m + 1));
+	for (int row = 1; row <= n; row++)
+		for (int col = 1; col <= m; col++)
+			cin >> mat[row][col];
+
+	vector<int> colors(n * m + 1);
+	vector<vector<bool>> vis(n + 1, vector<bool>(m + 1, false));
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
