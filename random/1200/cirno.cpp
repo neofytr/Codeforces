e8e46cfd88c3324c1286d41fe72ac9dd8771348c
neofytr <imagination12344321.com@gmail.com>
2025-12-20 18:00:02 +0530

    More problems solved

---
+++ random/1200/cirno.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n + 1);
+	for (int r = 1; r <= n; r++) cin >> arr[r];
+
+	if (n == 1) {
+		cout << arr[1] << endl;
+		return;
+	}
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
