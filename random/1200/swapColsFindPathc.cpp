49e0a78ba91631262a11fbc745503fddba161606
neofytr <imagination12344321.com@gmail.com>
2025-12-21 15:00:01 +0530

    Solved some problems

--- random/1200/swapColsFindPathc.cpp
+++ random/1200/swapColsFindPathc.cpp
@@ -16,6 +16,23 @@ void solve() {
 	for (int r = 1; r <= n; r++)
 		if (arr[1][r] + arr[2][r] > maxi.first)
 			maxi.first = arr[1][r] + arr[2][r], maxi.second = r;
+
+	int upsum = 0, downsum = 0;
+	for (int r = 1; r <= n; r++)
+		if (maxi.second != r)
+			upsum += arr[1][r], downsum += arr[2][r];
+
+	int upg2 = 0, downg1 = 0;
+	for (int r = 1; r <= n; r++)
+		if (maxi.second != r)
+			if (arr[1][r] <= arr[2][r]) upg2 += arr[1][r];
+			else downg1 += arr[2][r];
+
+	cout << maxi.first << endl;
+	cout << upsum << endl;
+	cout << downsum << endl;
+
+	cout << maxi.first + upsum + downsum - upg2 - downg1 << endl;
 }
 
 int32_t main() {
