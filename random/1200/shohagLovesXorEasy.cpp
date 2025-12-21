180a2f5983a7887a81189a8e2c7dadf14788f841
neofytr <imagination12344321.com@gmail.com>
2025-12-21 21:00:01 +0530

    Practiced CF problems

--- random/1200/shohagLovesXorEasy.cpp
+++ random/1200/shohagLovesXorEasy.cpp
@@ -6,6 +6,22 @@ using namespace std;
 void solve() {
 	int x, m;
 	cin >> x >> m;
+
+	set<int> res;
+	for (int r = 1; r * r <= x; r++)
+		if (!(x % r)) {
+			int y = r ^ x;
+			if (y >= 1 && y <= m && y != x)
+				res.insert(y);
+
+			if (r != x / r) {
+				y = (x / r) ^ x;
+				if (y >= 1 && y <= m && y != x)
+					res.insert(y);
+			}
+		}
+
+
 }
 
 int32_t main() {
