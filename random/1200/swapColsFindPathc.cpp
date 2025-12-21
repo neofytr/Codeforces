45ce69bacd6ee6e56e9a2b3956c0802c6cc200ef
neofytr <imagination12344321.com@gmail.com>
2025-12-21 16:00:01 +0530

    CF grind

--- random/1200/swapColsFindPathc.cpp
+++ random/1200/swapColsFindPathc.cpp
@@ -3,21 +3,9 @@ using namespace std;
 
 #define int long long 
 
-void solve() {
-	int n;
-	cin >> n;
-
-	vector<vector<int>> arr(3, vector<int>(n + 1));
-	for (int r = 1; r <= 2; r++)
-		for (int c = 1; c <= n; c++)
-			cin >> arr[r][c];
-
-	pair<int, int> maxi = {LLONG_MIN, -1};
-	for (int r = 1; r <= n; r++)
-		if (arr[1][r] + arr[2][r] > maxi.first)
-			maxi.first = arr[1][r] + arr[2][r], maxi.second = r;
-
-	int upsum = 0, downsum = 0;
+int c (pair<int, int> maxi, vector<vector<int>> &arr) {
+int upsum = 0, downsum = 0;
+int n = arr[1].size() - 1;
 	for (int r = 1; r <= n; r++)
 		if (maxi.second != r)
 			upsum += arr[1][r], downsum += arr[2][r];
@@ -27,12 +15,24 @@ void solve() {
 		if (maxi.second != r)
 			if (arr[1][r] <= arr[2][r]) upg2 += arr[1][r];
 			else downg1 += arr[2][r];
+	return maxi.first + upsum + downsum - upg2 - downg1;
+}
 
-	cout << maxi.first << endl;
-	cout << upsum << endl;
-	cout << downsum << endl;
+void solve() {
+	int n;
+	cin >> n;
 
-	cout << maxi.first + upsum + downsum - upg2 - downg1 << endl;
+	vector<vector<int>> arr(3, vector<int>(n + 1));
+	for (int r = 1; r <= 2; r++)
+		for (int c = 1; c <= n; c++)
+			cin >> arr[r][c];
+
+	int cost = LLONG_MIN;
+	pair<int, int> maxi;
+	for (int r = 1; r <= n; r++)
+		cost = max(cost, c({arr[1][r] + arr[2][r], r}, arr));
+
+	cout << cost << endl;
 }
 
 int32_t main() {
