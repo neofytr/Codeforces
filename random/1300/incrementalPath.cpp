c430c1091b4aafa6dd597a58c5a3394c10f0c45f
neofytr <imagination12344321.com@gmail.com>
2025-12-23 15:00:01 +0530

    Solved problem

--- random/1300/incrementalPath.cpp
+++ random/1300/incrementalPath.cpp
@@ -7,28 +7,43 @@ void solve() {
 	int n, m;
 	cin >> n >> m;
 
+	string str;
+	cin >> str;
 	vector<char> comm(n + 1);
-	for (int r = 1; r <= n; r++) cin >> comm[r];
+	for (int r = 1; r <= n; r++) comm[r] = str[r - 1];
 
+	set<int> black;
 	vector<int> v(m + 1);
 	for (int r = 1; r <= m; r++)
-		cin >> v[r];
+		cin >> v[r], black.insert(v[r]);
 
 	int l = 1, r = 1;
-	while (l <= r) {
+	unordered_map<int, int> mp;
+	while (l <= m) {
 		r = l;
 		int k = 0;
-		while (v[r] == v[l] + k)
+		while (r <= m && v[r] == v[l] + k)
 			k++, r++;
 
 		// [l, r - 1]
+		for (int i = l; i <= r - 1; i++)
+			mp[v[i]] = v[l] + k;
+		l = r;
 	}
 
 	int curr = 1;
 	for (int r = 1; r <= n; r++)
 		if (comm[r] == 'A') black.insert(++curr);
 		else {
-
+			if (!mp[curr + 1])
+				black.insert(++curr);
+			else 
+				curr = mp[curr + 1], black.insert(curr);
+			
+			if (mp[curr + 1])
+				curr = mp[curr + 1];
+			else
+				curr++;
 		}
 
 	cout << black.size() << endl;
---
+++ random/1300/wrongBinarySearch.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+void solve() {
+	int n;
+	cin >> n;
+
+	string str;
+	cin >> str;
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
