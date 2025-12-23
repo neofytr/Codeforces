fb3fe9437aead381bd537267fd8f4fea2a915a74
neofytr <imagination12344321.com@gmail.com>
2025-12-23 16:00:01 +0530

    Worked on Codeforces problems

--- random/1300/wrongBinarySearch.cpp
+++ random/1300/wrongBinarySearch.cpp
@@ -9,6 +9,14 @@ void solve() {
 
 	string str;
 	cin >> str;
+
+	vector<int> v(n + 1);
+	for (int r = 1; r <= n; r++) v[r] = str[r - 1] - '0';
+
+	vector<int> p(n + 1);
+	for (int r = 1; r <= n; r++) p[r] = r;
+
+	int l = 1, r = 1;
 }
 
 int32_t main() {
