dbdba75d7fc9f48c2b87b8a5fe9a72d6951a8d8b
neofytr <imagination12344321.com@gmail.com>
2025-12-23 14:00:01 +0530

    Solved a CF problem

--- random/1300/incrementalPath.cpp
+++ random/1300/incrementalPath.cpp
@@ -10,8 +10,31 @@ void solve() {
 	vector<char> comm(n + 1);
 	for (int r = 1; r <= n; r++) cin >> comm[r];
 
-	vector<int> black(m + 1);
-	for (int r = 1; r <= m; r++) cin >> black[r];
+	vector<int> v(m + 1);
+	for (int r = 1; r <= m; r++)
+		cin >> v[r];
+
+	int l = 1, r = 1;
+	while (l <= r) {
+		r = l;
+		int k = 0;
+		while (v[r] == v[l] + k)
+			k++, r++;
+
+		// [l, r - 1]
+	}
+
+	int curr = 1;
+	for (int r = 1; r <= n; r++)
+		if (comm[r] == 'A') black.insert(++curr);
+		else {
+
+		}
+
+	cout << black.size() << endl;
+	for (int b : black)
+		cout << b << " ";
+	cout << endl;
 }
 
 int32_t main() {
