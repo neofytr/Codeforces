4658eee4fa386e567258f2632bf4cc0bf6ed4045
neofytr <imagination12344321.com@gmail.com>
2025-12-24 23:00:01 +0530

    Worked on Codeforces problems

--- random/1300/subsetMultiplication.cpp
+++ random/1300/subsetMultiplication.cpp
@@ -10,19 +10,40 @@ void solve() {
 	vector<int> b(n + 1);
 	for (int r = 1; r <= n; r++) cin >> b[r];
 	
-	vector<int> div;
-	for (int r = 1; r * r <= b[n]; r++)
-		if (!(b[n] % r)) {
-			int one = r, two = b[n] / r;
-			div.push_back(one);
-			if (one != two) div.push_back(two);
-		}
-	sort(div.begin(), div.end());
+	int i = n;
+	while (i >= 2 && !(b[i] % b[i - 1])) i--;
 
-	for (int d : div) {
-		if (d > b[1]) break;
-		if (b[1] % d) continue;
+	if (i == 1) {
+		cout << 1 << endl;
+		return;
 	}
+
+	set<int> div;
+	for (int r = 1; r * r <= b[i]; r++)
+		if (!(b[i] % r))
+			div.insert(r), div.insert(b[i] / r);
+
+	set<pair<int, int>> p;
+	for (int d : div)
+		if (!(b[i - 1] % d))
+			p.insert({d, b[i - 1] / d});
+
+	for (int r = i - 2; r >= 1; r--) {
+		set<pair<int, int>> tmp;
+		for (auto &[d, x] : p) 
+			if (!(d % b[r])) {
+				tmp.insert({b[r], x});
+            }
+			else if (!(b[r] % x)) {
+				int dd = b[r] / x;
+				if (!(d % dd)) { 
+					tmp.insert({dd, x});
+				}
+			}
+		p = tmp;
+	}
+
+	cout << (p.begin())->second << endl;
 }
 
 int32_t main() {
