cc0972a2d5a56f923921dea82a0dba65c047d687
neofytr <imagination12344321.com@gmail.com>
2025-12-24 10:00:02 +0530

    Solved problem

--- random/1300/maxTree.cpp
+++ random/1300/maxTree.cpp
@@ -11,19 +11,11 @@ void solve() {
 	vector<int> p(n + 1, 0);
 	for (int r = 1; r <= n - 1; r++) {
 		cin >> u >> v >> x >> y;
-		if (v > u)
-			p[u] += x, p[v] += y;
-		else 
-			p[v] += x, p[u] += y;
+		if (x > y && p[x] <= p[y]) p[x] = p[y] + 1;
+		if (y >= x && p[y] <= p[x]) p[y] = p[x] + 1;
 	}
 
-	vector<pair<int, int>> vec;
-	for (int r = 1; r <= n; r++)
-		vec.push_back({p[r], r});
-
-	sort(vec.begin(), vec.end());
-	for (auto &[val, r] : vec)
-		cout << r << " ";
+	for (int r = 1; r <= n; r++) cout << p[r] << " ";
 	cout << endl;
 }
 
