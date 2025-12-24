4254f0974a451753830aee7f031d5782aabb9619
neofytr <imagination12344321.com@gmail.com>
2025-12-24 18:00:01 +0530

    More problems solved

--- random/1300/subsetMultiplication.cpp
+++ random/1300/subsetMultiplication.cpp
@@ -10,27 +10,15 @@ void solve() {
 	vector<int> b(n + 1);
 	for (int r = 1; r <= n; r++) cin >> b[r];
 	
-	set<int> x;
-	for (int i = 1; i * i <= b[n]; i++) 
-		if (!(b[n] % i)) {
-			int one = i, two = b[n] / i;
-			if (!(b[n - 1] % one)) x.insert(b[n - 1] / one);
-			if (one != two && !(b[n - 1] % two)) x.insert(b[n - 1] / two);
-		}
+	set<int> div;
+	for (int r = 1; r * r <= b[n]; r++)
+		if (!(b[n] % r))
+			div.insert(r), div.insert(b[n] / r);
 
-	for (int r = n - 2; r >= 1; r--) {
-		if (!(b[r + 1] % b[r]))
-			continue;
-
-		vector<int> no;
-		for (int e : x)
-			if (b[r] % e) no.push_back(e);
-			else if (b[r + 1] % (b[r] / e)) no.push_back(e);
-
-		for (int k : no) x.erase(k);
-	}
-
-	for (int e : x) cout << e << endl;
+	set<pair<int, int>> res;
+	for (int r : div)
+		if (!(b[n - 1] % r))
+			res.insert({r, b[n - 1] / r});
 }
 
 int32_t main() {
