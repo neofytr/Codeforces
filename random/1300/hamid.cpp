f7def714082a5d64a3d41a5e5c32b5aced644ee9
neofytr <imagination12344321.com@gmail.com>
2025-12-24 13:00:01 +0530

    Solved problem

--- random/1300/hamid.cpp
+++ random/1300/hamid.cpp
@@ -14,6 +14,31 @@ void solve() {
 	for (int r = 1; r <= n; r++)
 		if (str[r - 1] == '.') grid[r] = 0;
 		else grid[r] = 1;
+
+	// all walls to the left?
+	bool allLeft = true;
+	for (int r = 1; r <= x - 1; r++)
+		if (!g[r]) {
+			allLeft = false;
+			break;
+		} 
+
+	// all walls to the right?
+	bool allRight = true;
+	for (int r = x + 1; r <= n; r++)
+		if (!g[r]) {
+			allRight = false;
+			break;
+		}
+
+	if (allLeft || allRight) {
+		cout << min(x, n - x + 1) << endl;
+		return;
+	}
+
+	set<int> walls;
+	for (int r = 1; r <= n; r++)
+		if (g[r]) walls.insert(r);
 }
 
 int32_t main() {
