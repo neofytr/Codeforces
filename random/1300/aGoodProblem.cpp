58c46fa114b02cc6dffae663c522f656150df775
neofytr <imagination12344321.com@gmail.com>
2025-12-25 15:00:01 +0530

    Practiced CF problems

--- random/1300/aGoodProblem.cpp
+++ random/1300/aGoodProblem.cpp
@@ -3,17 +3,44 @@ using namespace std;
 
 #define int long long
 
-void solve() {
+void solve(int t) {
 	int n, l, r, k;
 	cin >> n >> l >> r >> k;
+
+	if (n & 1) {
+		cout << l << endl;
+		return;
+	}
+
+	if (n == 2) {
+		cout << -1 << endl;
+		return;
+	}
+
+	int highest = -1;
+	for (int b = 63; b >= 0; b--)
+		if (l & (1ll << b)) {
+			highest = b;
+			break;
+		}
+
+	if ((1ll << (highest + 1)) > r) {
+		cout << -1 << endl;
+		return;
+	}
+
+	if (k <= n - 2) {
+		cout << l << endl;
+	} else {
+		cout << (1ll << (highest + 1)) << endl;
+	}
 }
 
 int32_t main() {
 	int t;
 	cin >> t;
-
-	while (t--) {
-		solve();
+	for (int i = 1; i <= t; i++) {
+		solve(i);
 	}
 	return 0;
 }
