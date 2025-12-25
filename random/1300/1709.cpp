9aef4c02e61aa86e0a89ed30d7864dbe96de6610
neofytr <imagination12344321.com@gmail.com>
2025-12-25 13:00:01 +0530

    Solved a few problems

--- random/1300/1709.cpp
+++ random/1300/1709.cpp
@@ -4,36 +4,70 @@ using namespace std;
 #define int long long
 
 void solve() {
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	vector<int> a(n + 1), b(n + 1);
-	vector<pair<int, int>> f(2 * n + 1);
-	for (int r = 1; r <= n; r++) cin >> a[r], f[a[r]] = {0, r};
-	for (int r = 1; r <= n; r++) cin >> b[r], f[b[r]] = {1, r};
+    vector<int> a(n + 1), b(n + 1);
+    vector<pair<int,int>> f(2 * n + 1);
 
-	int alast = n, blast = n;
-	vector<pair<int, int>> ops;
-	
-	int alast = n, blast = n;
-	for (int r = 2 * n; r--; r >= 1) {
-		auto [arrnum, idx] = f[r];
-		if (r == 2 * n) {
-			if (!arrnum) {
-				for (int r = idx; r <= n - 1; r++)
-					ops.push_back({1, r}), f[a[r + 1]] = {0, r}, swap(a[r], a[r + 1]);
-			}
-		}
-	}
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        f[a[i]] = {0, i};
+    }
+    for (int i = 1; i <= n; i++) {
+        cin >> b[i];
+        f[b[i]] = {1, i};
+    }
 
-	
+    vector<pair<int,int>> ops;
+    int alast = n, blast = n;
+
+    for (int val = 2 * n; val >= 1; val--) {
+        auto [arr, idx] = f[val];
+
+        if (arr == 0) {
+            if (idx > alast) continue;   
+
+            for (int j = idx; j < alast; j++) {
+                ops.push_back({1, j});
+                swap(a[j], a[j + 1]);
+                f[a[j]] = {0, j};
+                f[a[j + 1]] = {0, j + 1};
+            }
+
+            if (alast == blast && a[alast] > b[alast]) {
+                ops.push_back({3, alast});
+                swap(a[alast], b[alast]);
+                f[a[alast]] = {0, alast};
+                f[b[blast]] = {1, blast};
+                blast--;
+                alast++;
+            }
+            alast--;
+        }
+        else {
+            if (idx > blast) continue;   
+
+            for (int j = idx; j < blast; j++) {
+                ops.push_back({2, j});
+                swap(b[j], b[j + 1]);
+                f[b[j]] = {1, j};
+                f[b[j + 1]] = {1, j + 1};
+            }
+            blast--;
+        }
+    }
+
+   	cout << ops.size() << endl;
+   	for (auto [i, j] : ops)
+   		cout << i << " " << j << endl;
 }
 
 int32_t main() {
-	int t;
-	cin >> t;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	while (t--) {
-		solve();
-	}
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
---
+++ random/1300/aGoodProblem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	int n, l, r, k;
+	cin >> n >> l >> r >> k;
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
