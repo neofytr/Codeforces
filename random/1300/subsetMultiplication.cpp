4488baa4a3fd1afd8d9dfcf944d16c291acbd812
neofytr <imagination12344321.com@gmail.com>
2025-12-25 10:00:01 +0530

    Solved a CF problem

--- random/1300/subsetMultiplication.cpp
+++ random/1300/subsetMultiplication.cpp
@@ -1,57 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <numeric>
+
 using namespace std;
 
-#define int long long
-
-void solve() {
-	int n;
-	cin >> n;
-
-	vector<int> b(n + 1);
-	for (int r = 1; r <= n; r++) cin >> b[r];
-	
-	int i = n;
-	while (i >= 2 && !(b[i] % b[i - 1])) i--;
-
-	if (i == 1) {
-		cout << 1 << endl;
-		return;
-	}
-
-	set<int> div;
-	for (int r = 1; r * r <= b[i]; r++)
-		if (!(b[i] % r))
-			div.insert(r), div.insert(b[i] / r);
-
-	set<pair<int, int>> p;
-	for (int d : div)
-		if (!(b[i - 1] % d))
-			p.insert({d, b[i - 1] / d});
-
-	for (int r = i - 2; r >= 1; r--) {
-		set<pair<int, int>> tmp;
-		for (auto &[d, x] : p) 
-			if (!(d % b[r])) {
-				tmp.insert({b[r], x});
-            }
-			else if (!(b[r] % x)) {
-				int dd = b[r] / x;
-				if (!(d % dd)) { 
-					tmp.insert({dd, x});
-				}
-			}
-		p = tmp;
-	}
-
-	cout << (p.begin())->second << endl;
+long long gcd(long long a, long long b) {
+    return std::gcd(a, b);
 }
 
-int32_t main() {
-	int t;
-	cin >> t;
+long long lcm(long long a, long long b) {
+    if (a == 0 || b == 0) return 0;
+    return (a / gcd(a, b)) * b;
+}
 
-	while (t--) {
-		solve();
-	}
-	return 0;
+void solve() {
+    int n;
+    cin >> n;
+    
+    vector<long long> b(n);
+    for(int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    
+    long long ans = 1;
+    
+    for(int i = 0; i < n - 1; i++) {
+        long long necessary_factor = b[i] / gcd(b[i], b[i+1]);
+        ans = lcm(ans, necessary_factor);
+    }
+    
+    cout << ans << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
 }
