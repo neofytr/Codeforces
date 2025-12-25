03c2ad6c8448bc3dd684e7e058cc38acbe889aa3
neofytr <imagination12344321.com@gmail.com>
2025-12-25 17:00:01 +0530

    More problems solved

---
+++ random/1300/gellyfish.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+#define MOD (998_244_353)
+
+void solve() {
+	int n;
+	cin >> n;
+
+	vector<int> p(n), q(n);
+	for (int r = 0; r < n; r++) cin >> p[r];
+	for (int r = 0; r < n; r++) cin >> q[r];
+
+
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
--- random/1300/makeItBeautiful.cpp
+++ random/1300/makeItBeautiful.cpp
@@ -15,24 +15,26 @@ void solve() {
 		for (int r = 1; r <= n; r++)
 			if (arr[r] & (1ll << b)) f[b]++;
 
-	int m = n - f[0];
-	f[0] += min(m, k);
-	k -= min(k, m);
-
-	for (int b = 1; b <= 63; b++) {
-		int more = (n - f[b]) * (1ll << (b - 1));
+	for (int b = 0; b <= 63; b++) {
+		int more = (n - f[b]) * (1ll << (b));
 		if (k >= more)
-			f[b] = n, k -= more;
+			f[b] += (n - f[b]), k -= more;
 		else {
-			cout << b << endl;
-			cout << k << " " << more << endl;
+			for (int r = 1; r <= (n - f[b]); r++)  {
+				int req = r * (1ll << (b));
+				if (req <= k)
+					continue;
+
+				f[b] += (r - 1);
+				break;
+			}
 			break;
 		}
 	}
 
 	int cnt = 0;
 	for (int b = 0; b <= 63; b++)
-		cout << "f[" << b << "] = " << f[b] << endl;
+		cnt += f[b];
 	cout << cnt << endl;
 }
 
