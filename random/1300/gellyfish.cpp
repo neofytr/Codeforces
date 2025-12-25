0f3efb1a478facefcea91da67d995bdf278672d0
neofytr <imagination12344321.com@gmail.com>
2025-12-25 18:00:01 +0530

    Practiced CF problems

--- random/1300/gellyfish.cpp
+++ random/1300/gellyfish.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 #define int long long 
-#define MOD (998_244_353)
+#define MOD (998244353)
+
+int pw(int base, int exp) {
+	int res = 1;
+	base = base % MOD;
+	while (exp) {
+		if (exp & 1)
+			res = res * base % MOD;
+		base = base * base % MOD;
+		exp >>= 1;
+	}
+	return res;
+}
 
 void solve() {
 	int n;
@@ -13,6 +25,31 @@ void solve() {
 	for (int r = 0; r < n; r++) cin >> q[r];
 
 
+	set<pair<int, int>> parr;
+	set<pair<int, int>> qarr;
+	vector<int> res(n);
+	for (int r = 0; r < n; r++) {
+		parr.insert({p[r], r}), qarr.insert({q[r], r});
+		auto [pval, pidx] = *parr.rbegin();
+		auto [qval, qidx] = *qarr.rbegin();
+
+		if (pval > qval) 
+			res[r] = (pw(2, pval) + pw(2, q[r - pidx])) % MOD;
+		else if (qval > pval)
+			res[r] = (pw(2, qval) + pw(2, p[r - qidx])) % MOD;
+		else {
+			int pval2 = q[r - pidx];
+			int qval2 = p[r - qidx];
+			if (pval2 > qval2)
+				res[r] = (pw(2, pval) + pw(2, pval2)) % MOD;
+			else
+				res[r] = (pw(2, qval) + pw(2, qval2)) % MOD;
+		}
+	}
+
+	for (int r = 0; r < n; r++)
+		cout << res[r] << " ";
+	cout << endl;
 }
 
 int32_t main() {
