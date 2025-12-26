768e085b523d5b8ebbf3b27d6a74ba617e3854b9
neofytr <imagination12344321.com@gmail.com>
2025-12-26 10:00:01 +0530

    Solved a few problems

--- topicStreamsColinGalen/DP/lectureSleep.cpp
+++ topicStreamsColinGalen/DP/lectureSleep.cpp
@@ -16,4 +16,16 @@ int32_t main() {
 	for (int r = 1; r <= n; r++) p[r] += p[r - 1] + a[r];
 
 	vector<vector<int>> dp(n + 1, vector<int>(2, LLONG_MIN));
+
+
+	dp[0][0] = dp[0][1] = 0;
+	dp[1][0] = (t[1] ? a[1] : 0);
+	if (k == 1) dp[1][1] = a[1];
+	for (int r = 2; r <= n; r++) {
+		dp[r][0] = dp[r - 1][0] + (t[r] ? a[r] : 0);
+		if (r >= k)
+			dp[r][1] = max(dp[r - 1][1] + (t[r] ? a[r] : 0), p[r] - p[r - k] + dp[r - k][0]);
+	}
+
+	cout << dp[n][1] << endl;
 }
