0bc456044ff24b48c74a684257208050604afcba
neofytr <imagination12344321.com@gmail.com>
2025-12-26 21:00:01 +0530

    Solved a few problems

--- topicStreamsColinGalen/DP/mikeAndFun.cpp
+++ topicStreamsColinGalen/DP/mikeAndFun.cpp
@@ -13,31 +13,5 @@ int32_t main() {
 		for (int c = 1; c <= m; c++)
 			cin >> mat[r][c];
 
-	set<pair<int, pair<int, int>>> s;
-	vector<vector<pair<int, int>>> where(n + 1, vector<pair<int, int>>(m + 1, {-1, -1}));
-	for (int r = 1; r <= n; r++) {
-		vector<int> &v = mat[r];
-
-		int i = 1, j = 1;
-		while (i <= j) {
-			if (!v[i]) {
-				i++;
-				continue;
-			}
-
-			while (j <= m && v[j])
-				j++;
-
-			int len = j - i, start = i;
-			for (int k = i; k < j; k++)
-				where[r][k] = {start, len};
-			s.insert({len, {r, i}});
-			j = i;
-		}
-	}
-
-	int i, j;
-	while (q--) {
-		cin >> i >> j;
-	}
+	vector<vector<int>> dp(n + 1, vector<int>(m + 1));
 }
