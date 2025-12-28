7a6f65106fcf8f27c120879d892b88253f4184f2
neofytr <imagination12344321.com@gmail.com>
2025-12-28 14:00:01 +0530

    Solved some problems

--- random/1300/arrayRecoloring.cpp
+++ random/1300/arrayRecoloring.cpp
@@ -4,51 +4,39 @@ using namespace std;
 #define int long long
 #define MAX 5000
 
-int dp[MAX + 1][MAX + 1][2];
 int arr[MAX + 1];
 
 void solve() {
     int n, k;
     cin >> n >> k;
 
+    vector<int> arr(n + 1, 0);
+    vector<pair<int, int>> s;
     for (int r = 1; r <= n; r++)
-        cin >> arr[r];
+        cin >> arr[r], s.push_back({arr[r], r});
 
-    for (int r = 1; r <= n; r++)
-        for (int s = 1; s <= k; s++)
-            for (int j = 0; j <= 1; j++)
-                dp[r][s][j] = LLONG_MIN;
+    int sum = 0;
+    sort(s.rbegin(), s.rend());
+    vector<int> idx;
+    for (int r = 0; r < k; r++)
+        idx.push_back(s[r].second), sum += s[r].first;
 
-    dp[0][0][0] = 0;
-    dp[1][0][0] = 0;
-    dp[1][1][0] = arr[1];
-    dp[2][0][0] = 0;
-    dp[2][1][0] = max(arr[1], arr[2]);
-    dp[2][2][0] = arr[1] + arr[2];
-    dp[2][1][1] = arr[1] + arr[2];
+    int j = 0, maxi = LLONG_MIN;
+    sort(idx.begin(), idx.end());
 
-    for (int r = 3; r <= n; r++)
-        for (int s = 1; s <= k; s++) {
-            if (r >= s) {
-                dp[r][s][0] = arr[r] + dp[r - 1][s - 1][0];
-                if (r - 1 >= s)
-                    dp[r][s][0] = max(dp[r][s][0], dp[r - 1][s][0]);
-            }
+    int start = 1;
+    if (idx[0] == 1) start = 2 , j = 1;
+    while (start <= n) {
+        int end = start;
+        while (end <= n && end < idx[j]) end++;
+        if (start != idx[j]) maxi = max(maxi, arr[start]);
+        if (end != start) maxi = max(maxi, arr[end]);
 
-            if (r - 1 >= s) {
-                int one = max(arr[r] + arr[r - 1] + dp[r - 2][s - 1][0], dp[r - 1][s][1]);
-                int two = max(arr[r] + arr[r - 1] + dp[r - 2][s - 1][0], arr[r] + arr[r - 1] + dp[r - 2][s - 2][1]);
-                int three = arr[r] + dp[r - 2][s - 1][1];
+        start = idx[j] + 1;
+        j++;
+    }
 
-                dp[r][s][1] = max(one, max(two, three));
-            }
-        }
-
-    int maxi = LLONG_MIN;
-    for (int r = 1; r <= n; r++)
-        maxi = max(maxi, dp[r][k][1]);
-
-    cout << maxi << endl;
+    cout << maxi + sum << endl;
 }
 
 int32_t main() {
