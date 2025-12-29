5e165e1db71d9f1fcd09f7a4cf2acaec4c347451
neofytr <imagination12344321.com@gmail.com>
2025-12-29 23:00:01 +0530

    CF grind

--- random/dp/iHate1111.cpp
+++ random/dp/iHate1111.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 #define int long long
 
+unordered_map<int, bool> f;
+
+int cnt(int num) {
+    int sum = 0;
+    while (num) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
 void solve() {
     int x;
     cin >> x;
