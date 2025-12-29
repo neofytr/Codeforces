6bac76fb35ddefb8fcea90d41a92cfabd1f487fa
neofytr <imagination12344321.com@gmail.com>
2025-12-29 14:00:01 +0530

    More problems solved

--- random/dp/mukhammadaliSmooth.cpp
+++ random/dp/mukhammadaliSmooth.cpp
@@ -2,18 +2,27 @@
 using namespace std;
 
 #define int long long
+#define MAX (8000)
+
+int arr[MAX + 1];
+int cost[MAX + 1];
+
+int dp[MAX + 1][0];
 
 void solve() {
     int n;
     cin >> n;
 
-    vector<int> arr(n + 1);
+    set<int> s;
     for (int r = 1; r <= n; r++)
-        cin >> arr[r];
-
-    vector<int> cost(n + 1);
+        cin >> arr[r], s.insert(arr[r]);
     for (int r = 1; r <= n; r++)
         cin >> cost[r];
+
+    for (auto itr = s.rend(); itr != s.rbegin(); ++itr) {
+        for (int r = 1; r <= n; r++)
+            dp[r][0] = dp[r][1] = LLONG_MAX;
+    }
 }
 
 int32_t main() {
