613ae527f7983fc3dfe6eabcbc224ed0e9008fb4
neofytr <imagination12344321.com@gmail.com>
2025-12-29 11:00:01 +0530

    Solved a few problems

--- random/dp/bersuBall.cpp
+++ random/dp/bersuBall.cpp
@@ -2,27 +2,34 @@
 using namespace std;
 
 #define int long long
-#define MAX (1000)
+#define MAX (100)
 
-int cntB[MAX + 1];
-int cntG[MAX + 1];
+// dp[i][j] is the maximum number of pairs we can get after using the first i boys and first j girls
+int dp[MAX + 1][MAX + 1];
 
-int dp[MAX + 1][2];
-int dpm[MAX + 1];
 int32_t main() {
-    int n, v;
+    int n;
     cin >> n;
+    vector<int> b(n + 1);
     for (int r = 1; r <= n; r++)
-        cin >> v, cntB[v]++;
+        cin >> b[r];
 
     int m;
     cin >> m;
+    vector<int> g(m + 1);
     for (int r = 1; r <= m; r++)
-        cin >> v, cntG[v]++;
+        cin >> g[r];
 
-    for (int r = 2; r <= MAX; r++) {
-        dp[r][0] = dpm[r - 1] + min(cntB[r], cntG[r]);
-    }
+    sort(b.begin() + 1, b.end());
+    sort(g.begin() + 1, g.end());
 
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) {
+            if (abs(b[i] - g[j]) <= 1)
+                dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+            dp[i][j] = max(dp[i][j], max(dp[i][j - 1], dp[i - 1][j]));
+        }
+
+    cout << dp[n][m] << endl;
     return 0;
 }
---
+++ random/dp/givenLengthSumDigits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    int m, s;
+    cin >> m >> s;
+
+    if (!s && m > 1) {
+        cout << -1 << " " << -1 << endl;
+        return 0;
+    }
+
+    if (!s && m == 1) {
+        cout << 0 << " " << 0 << endl;
+        return 0;
+    }
+
+    
+}
