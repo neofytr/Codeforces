758c7ca25dd734942f18575e01734b04b5b52538
neofytr <imagination12344321.com@gmail.com>
2025-12-30 14:00:01 +0530

    Solved a few problems

--- random/dp/justEatIt.cpp
+++ random/dp/justEatIt.cpp
@@ -3,6 +3,39 @@ using namespace std;
 
 #define int long long
 
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n + 1), p(n + 1, 0);
+    for (int r = 1; r <= n; r++)
+        cin >> arr[r], p[r] += p[r - 1] + arr[r];
+
+    set<pair<int, int>> s;
+    int maxi = LLONG_MIN;
+    s.insert({p[0], 0});
+    for (int r = 1; r <= n; r++) {
+        if (r != n)
+            maxi = max(maxi, p[r] - s.begin()->first);
+        else {
+            auto [v, idx] = *s.begin();
+            if (idx != 0)
+                maxi = max(maxi, p[r] - v);
+            else {
+                auto [nv, nidx] = *(++s.begin());
+                maxi = max(maxi, p[r] - nv);
+            }
+        }
+
+        s.insert({p[r], r});
+    }
+
+    if (p[n] > maxi)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
 int32_t main() {
     int t;
     cin >> t;
