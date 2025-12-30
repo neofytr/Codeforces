32642f5312699e80fd190b4a0cbfbf0c8fba20cd
neofytr <imagination12344321.com@gmail.com>
2025-12-30 22:00:01 +0530

    Solved some problems

--- random/dp/groupingIncreases.cpp
+++ random/dp/groupingIncreases.cpp
@@ -4,8 +4,17 @@ using namespace std;
 #define int long long
 #define MAX (int)(2 * 1e5)
 
+struct cmp {
+    bool operator()(const pair<int, int> &a, const pair<int, int> &b) const {
+        if (a.first != b.first)
+            return a.first < b.first; // min first
+        return a.second > b.second;   // max second
+    }
+};
+
 int arr[MAX + 1];
-int dp[MAX + 1][2];
+int dp[MAX + 1];
+int p[MAX + 1];
 
 void solve() {
     int n;
@@ -14,9 +23,12 @@ void solve() {
     for (int r = 1; r <= n; r++)
         cin >> arr[r];
 
-    for (int r = 2; r <= n; r++) {
-        
-    }
+    p[0] = p[1] = 0;
+    for (int r = 2; r <= n; r++)
+        p[r] += p[r - 1] + (arr[r] > arr[r - 1] ? 1 : 0);
+
+    for (int r = 1; r <= n; r++)
+        cout << p[r] << endl;
 }
 
 int32_t main() {
