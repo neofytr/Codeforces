2dc25c021b884e37a0c8f34502b3f1e2df937258
neofytr <imagination12344321.com@gmail.com>
2025-12-31 14:00:01 +0530

    CF grind

--- random/dp/linovaKingdom.cpp
+++ random/dp/linovaKingdom.cpp
@@ -12,25 +12,60 @@ int32_t main() {
     for (int r = 1; r <= n; r++)
         cin >> u >> v, graph[u].push_back(v), graph[v].push_back(u);
 
+    vector<int> dp(n + 1, 0);
     vector<int> type(n + 1, 0);
-    vector<int> dp(n + 1, 0);                        // dp[r] is the number of industrial cities in the tree rooted at r
-    vector<vector<int>> depth(n + 1, vector<int>()); // depth[r] are the nodes at depth r
+    vector<set<int>> depth(n + 1);
 
     int d = 1;
     queue<pair<int, int>> que;
-
     que.push({1, -1});
     while (!que.empty()) {
-        int sz = que.size();
+        int sz = (int)que.size();
         while (sz--) {
             auto [x, p] = que.front();
             que.pop();
-            depth[d].push_back(x);
+            depth[d].insert(x);
 
-            for (const int c : graph[x])
-                if (c != x)
-                    que.push({c, x});
+            for (int node : graph[x])
+                if (node != p)
+                    que.push({node, x});
         }
         d++;
     }
+
+    set<pair<int, int>> pq1, pq2;
+
+    for (int e : depth[d - 1])
+        if (k)
+            pq1.insert({0, e});
+    d--;
+
+    while (true) {
+        int sz = pq1.size();
+        while (sz--) {
+            auto [m, node] = *pq1.begin();
+            if (k)
+                dp[node] += 1, m++, k--, type[node] = 1;
+            pq1.erase(pq1.begin());
+
+            for (int x : graph[node]) {
+                if (depth[d - 1].contains(x)) {
+                    pq2.erase({dp[x], x});
+                    pq2.insert({dp[x] += dp[node], x});
+                }
+            }
+        }
+        if (pq2.empty())
+            break;
+        swap(pq1, pq2);
+        d--;
+    }
+
+    int res = 0;
+    for (int r = 1; r <= n; r++)
+        if (type[r])
+            cout << r << " " << dp[r] << endl;
+
+    cout << res << endl;
+    return 0;
 }
