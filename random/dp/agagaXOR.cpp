3d957ba511013eb1d82dc84df5b5c002a84807ab
neofytr <imagination12344321.com@gmail.com>
2025-12-31 12:00:01 +0530

    Solved a CF problem

--- random/dp/agagaXOR.cpp
+++ random/dp/agagaXOR.cpp
@@ -11,11 +11,25 @@ void solve() {
     for (int r = 1; r <= n; r++)
         cin >> arr[r], p[r] = p[r - 1] ^ arr[r];
 
-    vector<vector<int>> dp(n + 1);
-    dp[1] = {arr[1]};
-    for (int r = 2; r <= n; r++) {
-        dp[r] = dp[r - 1];
+    if (!p[n]) {
+        cout << "YES\n";
+        return;
     }
+
+    int l = 1, r = 1, cnt = 0;
+    while (l <= n) {
+        r = l;
+        while (r <= n && ((p[r] ^ p[l - 1]) != p[n]))
+            r++;
+        if (r <= n && p[r] ^ p[l - 1] == p[n])
+            cnt++;
+        l = r + 1;
+    }
+
+    if (cnt >= 3)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
 }
 
 int32_t main() {
