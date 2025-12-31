435b9fde9eabc55cbfcfee19c7a985cc30ab2719
neofytr <imagination12344321.com@gmail.com>
2025-12-31 11:00:02 +0530

    More problems solved

--- random/dp/agagaXOR.cpp
+++ random/dp/agagaXOR.cpp
@@ -6,6 +6,16 @@ using namespace std;
 void solve() {
     int n;
     cin >> n;
+
+    vector<int> arr(n + 1), p(n + 1, 0);
+    for (int r = 1; r <= n; r++)
+        cin >> arr[r], p[r] = p[r - 1] ^ arr[r];
+
+    vector<vector<int>> dp(n + 1);
+    dp[1] = {arr[1]};
+    for (int r = 2; r <= n; r++) {
+        dp[r] = dp[r - 1];
+    }
 }
 
 int32_t main() {
