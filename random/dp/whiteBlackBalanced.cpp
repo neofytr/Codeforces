f281c4128ae1430bf6d149eb9a23b1211645c413
neofytr <imagination12344321.com@gmail.com>
2026-01-01 11:00:01 +0530

    Practiced CF problems

---
+++ random/dp/whiteBlackBalanced.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long 
+
+int black(int node, string &s, vector<int> &b, vector<vector<int>> &child) {
+	if (child[node].empty())
+		return b[node] = (s[node - 1] == 'B' ? 1 : 0);
+
+	int res = 0;
+	for (int c : child[node])
+		res += black(c, s, b, child);
+
+	return b[node] = res + (s[node - 1] == 'B' ? 1 : 0);
+}
+
+void solve() {
+	int n;
+	cin >> n;
+
+	int p;
+	vector<vector<int>> child(n + 1, -1);
+	for (int c = 2; c <= n; c++)
+		cin >> p, child[p].push_back(c);
+
+	string s;
+	cin >> s;
+
+	vector<int> w(n + 1); // w[r] is the number of white vertices in the subtree rooted at r
+	vector<int> b(n + 1); // b[r] is the number of black vertices in the subtree rooted at r
+
+	dfs(1, s, child);
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+} 
