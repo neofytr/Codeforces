aff109c14845a8ce27fdd59f76ac55158f52a2cb
neofytr <imagination12344321.com@gmail.com>
2026-01-02 21:00:50 +0530

    Solved a few problems

--- random/dp/arrayOps.cpp
+++ random/dp/arrayOps.cpp
@@ -5,35 +5,31 @@ using namespace std;
 #define MAX (int)(2 * 1e5)
 
 int cnt[MAX + 1];
-
 void solve() {
     int n, k;
     cin >> n >> k;
 
-    for (int r = 1; r <= MAX; r++)
-        cnt[r] = 0;
+    set<pair<int, int>> p;
+    set<int> elts;
 
     int v;
     for (int r = 1; r <= n; r++)
         cin >> v, cnt[v]++;
 
-    int i = 1, j = 1;
-    while (k) {
-        j = i + 1;
-        while (cnt[i] && k) {
-            int one = cnt[i], two = cnt[j], three = k;
-            cnt[j] -= min(one, min(two, three));
-            cnt[i] -= min(one, min(two, three));
-            k -= min(one, min(two, three));
-            if (!cnt[j])
-                j++;
-        }
-        i = j;
+    for (int r = 1; r <= MAX; r++)
+        if (cnt[r])
+            p.insert({r * cnt[r], r}), elts.insert(r);
+
+    int res = 0;
+    while (elts.size() > 1) {
+        int x = *elts.begin();
+        auto itr =
     }
 }
 
 int32_t main() {
     int t;
+    cin >> t;
 
     while (t--) {
         solve();
