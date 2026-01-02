4400d1e28ef302baf4a3805cbbb829514d2211b8
neofytr <imagination12344321.com@gmail.com>
2026-01-02 22:00:01 +0530

    CF grind

--- random/dp/arrayOps.cpp
+++ random/dp/arrayOps.cpp
@@ -2,29 +2,25 @@
 using namespace std;
 
 #define int long long
-#define MAX (int)(2 * 1e5)
+#define MAX (int)(100)
 
-int cnt[MAX + 1];
 void solve() {
     int n, k;
     cin >> n >> k;
 
-    set<pair<int, int>> p;
-    set<int> elts;
-
-    int v;
+    vector<int> arr(n + 1);
     for (int r = 1; r <= n; r++)
-        cin >> v, cnt[v]++;
-
-    for (int r = 1; r <= MAX; r++)
-        if (cnt[r])
-            p.insert({r * cnt[r], r}), elts.insert(r);
+        cin >> arr[r];
+    sort(arr.begin() + 1, arr.end());
 
     int res = 0;
-    while (elts.size() > 1) {
-        int x = *elts.begin();
-        auto itr =
-    }
+    for (int r = 1; r <= n - 2 * k; r++)
+        res += arr[r];
+
+    for (int r = n - 2 * k + 1; r <= n - k; r++)
+        res += (arr[r] / arr[r + k]);
+
+    cout << res << endl;
 }
 
 int32_t main() {
