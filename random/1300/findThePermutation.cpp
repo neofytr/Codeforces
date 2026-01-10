6cf0ece2150fc0ee0c7635706d7dec9680b0e42c
neofytr <imagination12344321.com@gmail.com>
2026-01-10 11:00:01 +0530

    Solved problem

--- random/1300/findThePermutation.cpp
+++ random/1300/findThePermutation.cpp
@@ -14,6 +14,32 @@ void solve() {
         for (int c = 1; c <= n; c++)
             adj[r][c] = col[c - 1] - '0';
     }
+
+    vector<int> indegree(n + 1, 0);
+    for (int i = 1; i <= n; i++)
+        for (int j = i + 1; j <= n; j++)
+            if (adj[i][j])
+                indegree[j]++;
+
+    priority_queue<int> pq;
+    for (int r = 1; r <= n; r++)
+        if (!indegree[r])
+            pq.push(r);
+
+    vector<int> p;
+    while (!pq.empty()) {
+        const int x = pq.top();
+        pq.pop();
+
+        p.push_back(x);
+        for (int i = x + 1; i <= n; i++)
+            if (adj[x][i] && !(--indegree[i]))
+                pq.push(i);
+    }
+
+    for (const int e : p)
+        cout << e << " ";
+    cout << endl;
 }
 
 int32_t main() {
--- random/dp/workingOut.cpp
+++ random/dp/workingOut.cpp
@@ -5,10 +5,64 @@ using namespace std;
 #define MAX (int)(1e3)
 
 int grid[MAX + 1][MAX + 1];
+int topLeft[MAX + 1][MAX + 1];
+int topRight[MAX + 1][MAX + 1];
+int bottomLeft[MAX + 1][MAX + 1];
+int bottomRight[MAX + 1][MAX + 1];
 
 int32_t main() {
     int n, m;
     cin >> n >> m;
 
+    for (int r = 1; r <= n; r++)
+        for (int c = 1; c <= m; c++)
+            cin >> grid[r][c];
+
+    for (int r = 1; r <= n; r++)
+        for (int c = 1; c <= m; c++) {
+            if (r >= 2)
+                topLeft[r][c] = topLeft[r - 1][c];
+            if (c >= 2)
+                topLeft[r][c] = max(topLeft[r][c - 1], topLeft[r][c]);
+            topLeft[r][c] += grid[r][c];
+        }
+
+    for (int r = 1; r <= n; r++)
+        for (int c = m; c >= 1; c--) {
+            if (r >= 2)
+                topRight[r][c] = topRight[r - 1][c];
+            if (c <= m - 1)
+                topRight[r][c] = max(topRight[r][c], topRight[r][c + 1]);
+            topRight[r][c] += grid[r][c];
+        }
+
+    for (int r = n; r >= 1; r--)
+        for (int c = 1; c <= m; c++) {
+            if (c >= 2)
+                bottomLeft[r][c] = bottomLeft[r][c - 1];
+            if (r <= n - 1)
+                bottomLeft[r][c] = max(bottomLeft[r + 1][c], bottomLeft[r][c]);
+            bottomLeft[r][c] += grid[r][c];
+        }
+
+    for (int r = n; r >= 1; r--)
+        for (int c = m; c >= 1; c--) {
+            if (c <= m - 1)
+                bottomRight[r][c] = bottomRight[r][c + 1];
+            if (r <= n - 1)
+                bottomRight[r][c] = max(bottomRight[r][c], bottomRight[r + 1][c]);
+            bottomRight[r][c] += grid[r][c];
+        }
+
+    int maxi = LLONG_MIN;
+    for (int r = 2; r <= n - 1; r++)
+        for (int c = 1; c <= m; c++) {
+            // (r, c) is the meeting point
+            int lahub = topLeft[r][c] + bottomRight[r][c] - 2 * grid[r][c];
+            int lahubina = bottomLeft[r][c] + topRight[r][c] - 2 * grid[r][c];
+            maxi = max(maxi, lahub + lahubina);
+        }
+
+    cout << maxi << endl;
     return 0;
 }
