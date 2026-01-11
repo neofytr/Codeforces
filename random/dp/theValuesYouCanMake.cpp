c6748c9e9754d9a89132459c7c70c67b28f5156b
neofytr <imagination12344321.com@gmail.com>
2026-01-11 20:00:01 +0530

    Practiced CF problems

--- random/dp/theValuesYouCanMake.cpp
+++ random/dp/theValuesYouCanMake.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long 
-#define MAX (int)(5 * 1e2)
+#define int long long
+#define MAX 500
 
 int c[MAX + 1];
+bool dp[MAX + 1];
+set<pair<int, int>> where[MAX + 1];
 
 int32_t main() {
 	int n, k;
@@ -12,5 +14,18 @@ int32_t main() {
 
 	for (int r = 1; r <= n; r++)
 		cin >> c[r];
+
+	dp[0] = true;
+	for (int r = 1; r <= n; r++) {
+		vector<int> up;
+		for (int s = 0; s <= k; s++)
+			if (dp[s] && (s + c[r] <= k))
+				up.push_back(s + c[r]), where[s + c[r]].insert({s, c[r]});
+		for (int e : up)
+			dp[e] = true;
+	}
+
+	for (auto &[s, e] : where[k])
+		cout << s << " " << e << endl;
 	return 0;
 }
