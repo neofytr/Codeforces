065cd7b4caff47cca81f3636227f75cf4ad3e972
neofytr <imagination12344321.com@gmail.com>
2026-01-12 12:00:01 +0530

    CF grind

--- random/dp/bottles.cpp
+++ random/dp/bottles.cpp
@@ -8,45 +8,6 @@ using namespace std;
 int A[MAX + 1], B[MAX + 1];
 int C[MAX + 1];
 int dp[2][MAX + 1][MAX * MAX + 1];
-int prefOne[MAX + 1][MAX * MAX + 1], prefTwo[MAX + 1][MAX * MAX + 1];
-
-template<typename T>
-class SparseTable {
-private:
-	// table[r][j] = minimum of the range [arr[r], ..., arr[r + (1LL << j) - 1]] 
-	// for 0 <= j <= LOG2(MAX_N) and 1 <= r <= (MAX_N + 1 - (1LL << j))
-	vector<vector<T>> table;
-	function<T(T, T)> op;
-
-public:
-	// table[r][j] = min(table[r][j - 1], table[r + (1LL << (j - 1))][j - 1]) 
-	// table[r][0] = arr[r] 
-	SparseTable(vector<T> &arr, function<T(T, T)> operation) {
-		int n = (int)arr.size(); // 1-based indexed array
-		n--;
-		if (!n) return; // handle empty case
-
-		int lg = LOG2(n);
-		table.resize(n + 1, vector<T>(lg + 1));
-
-		op = operation;
-		// build base case for j = 0
-		for (int r = 1; r <= n; r++)
-			table[r][0] = arr[r];
-
-		// build sparse table for higher powers of 2
-		for (int j = 1; j <= lg; j++)
-			for (int r = 1; r <= (n + 1 - (1LL << j)); r++)
-				table[r][j] = op(table[r][j - 1], table[r + (1LL << (j - 1))][j - 1]);
-	}
-
-	// query the minimum value in range [l, r] (1-based indexing)
-	int query(int l, int r) {
-		int lg = LOG2(r - l + 1);
-		// min of the two overlapping intervals covering [l, r]
-		return op(table[l][lg], table[r - (1LL << lg) + 1][lg]);
-	}
-};
 
 int32_t main() {
 	int n;
@@ -75,17 +36,12 @@ int32_t main() {
 		prev = prev ^ 1, curr = curr ^ 1;
 		for (int c = 1; c <= k; c++) {
 			dp[curr][c][0] = 0; 
+			deque<int> d1, d2;
+
 			for (int m = 1; m <= sum; m++) {
-				// either i can use the rth bottle, or not use it
-				dp[curr][c][m] = dp[prev][c][m];
-				for (int last = 1; last <= min(m, B[r]); last++) 
-					if (last <= A[r]) dp[curr][c][m] = min(dp[curr][c][m], dp[prev][c - 1][m - last]);
-					else dp[curr][c][m] = min(dp[curr][c][m], dp[prev][c - 1][m - last] + (last - A[r]));
+				
 			}
 		}
-
-		for (int c = 0; c <= k - 1; c++)
-
 	}
 
 	cout << dp[curr][k][sum] << endl;
---
+++ rangeQueries/algos/slidingRangeMinimum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Let n, k >= 1
+// Suppose you have a sequence of values v[1, n]
+
+// Suppose, at each step i, starting from 1 and reaching till k, you want
+// min { v[j] + f(j) | L(i) <= j <= R(i)}
+// , where, 
+// 1. R(i) increases by exactly 1 as i increases by 1
+// 2. L(i) never decreases as i increases
+// 3. f(j) is a function of j only and does not depend on i
+
+// This is the sliding window minimum problem
+// We solve this using a monotonic deque that maintains the following invariants
+// during each step:
+
+// 1. The deque stores all the indices which are inside the current window
+// 2. The deque is sorted (in non-decreasing order) from front to the back according
+//	  to the value of (v + f) at the index in the deque
+
+// This ensures that the front of the deque contains the index of the minimum of (v + f) inside the 
+// current window
+
+deque<int> dq;
+for (int i = 1; i <= k; i++) {	
+	int right = R(i);
+	int left = L(i);
+
+	while (!dq.empty() && (v[dq.back()] + f(dq.back())) >= v[right] + f(right))
+		dq.pop_back();
+	dq.push_back(right);
+
+	while (!dq.empty() && dq.front() < left)
+		dq.pop_front();
+
+	int mIdx = dq.front();
+	int mVal = v[mIdx] + f(mIdx); // minimum of the window at step i 
+}
