8b78d1a6b632f099446a7bf35b0df708639cb83a
neofytr <imagination12344321.com@gmail.com>
2026-01-12 16:00:01 +0530

    CF grind

--- random/dp/fire.cpp
+++ random/dp/fire.cpp
@@ -17,7 +17,7 @@ int32_t main() {
 
 	int sz = 0;
 	for (int r = 1; r <= n; r++)
-		if (b[r] >= t[r]) vec[++sz] = {b[r] - t[r], r};
+		if (b[r] >= t[r]) vec[++sz] = {b[r], r};
 
 	if (!sz) {
 		cout << 0 << endl;
@@ -29,30 +29,34 @@ int32_t main() {
 	// dp[i][j] is the maximum value we can get out of the saved items in vec[1, i]
 	// with total time spent = j
 
+	for (int i = 0; i <= sz; i++)
+	for (int j = 0; j <= TMAX; j++)
+		dp[i][j] = -1e15;
+
+	dp[0][0] = 0;
 	for (int i = 1; i <= sz; i++) {
 		int idx = vec[i].second;
 
-		dp[i][0] = 0;
-		for (int j = t[idx]; j <= b[idx]; j++) {
-			dp[i][j] = max(dp[i][j], dp[i - 1][j - t[idx]] + v[idx]);
+		for (int j = 0; j <= TMAX; j++)
+			dp[i][j] = dp[i - 1][j];
+
+		for (int j = t[idx]; j < b[idx]; j++) {
+			if (dp[i - 1][j - t[idx]] >= 0)
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - t[idx]] + v[idx]);
 		}
-		for (int j = 1; j <= 2000; j++)
-			dp[i][j] = max(dp[i][j], dp[i - 1][j]);
 	}
 
-	
 	int maxi = LLONG_MIN, time = -1;
-	for (int j = 0; j <= 2000; j++) {
+	for (int j = 0; j <= TMAX; j++) {
 		if (dp[sz][j] > maxi) time = j, maxi = dp[sz][j];
 	}
 	cout << maxi << endl;
 
 	vector<int> s;
-
 	int r = sz;
 	while (r >= 1) {
 		int idx = vec[r].second;
-		if (time >= t[idx] && dp[r][time] == dp[r - 1][time - t[idx]] + v[idx])
+		if (time >= t[idx] && time < b[idx] && dp[r][time] == dp[r - 1][time - t[idx]] + v[idx])
 			s.push_back(idx), r--, time -= t[idx];
 		else r--;
 	}
