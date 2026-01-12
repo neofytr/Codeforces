aedfaf46a25cff309fbe60771558a196b6c4ce6e
neofytr <imagination12344321.com@gmail.com>
2026-01-12 22:00:01 +0530

    Solved problem

--- random/dp/writingCode.cpp
+++ random/dp/writingCode.cpp
@@ -1,31 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long 
-#define MAX (int)(5 * 1e2)
+#define int long long
+#define MAX 500
 
 int dp[MAX + 1][MAX + 1][MAX + 1];
 int arr[MAX + 1];
+int p[MAX + 1][MAX + 1];
 
 int32_t main() {
-	int n, m, b, mod;
-	cin >> n >> m >> b >> mod;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	for (int r = 1; r <= n; r++) cin >> arr[r];
+    int n, m, b, mod;
+    cin >> n >> m >> b >> mod;
 
-	dp[0][0][0] = 1;
-	for (int i = 1; i <= n; i++) {
-		for (int k = 0; k <= b; k++)
-			dp[i][0][k] = 1;
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
 
-		for (int j = 1; j <= m; j++) {
-			dp[i][j][0] = 0;
-			for (int k = 1; k <= b; k++)
-				for (int v = 0; v <= j; v++)
-					dp[i][j][k] = (dp[i][j][k] + (k - arr[i] * v >= 0 ? dp[i - 1][j - v][k - arr[i] * v] : 0)) % mod;
-		}
-	}
+    dp[0][0][0] = 1;
+    for (int i = 1; i <= n; i++) 
+        for (int j = 0; j <= m; j++) 
+            for (int k = 0; k <= b; k++) {
+                dp[i][j][k] = 0;
+                for (int v = 0; v <= j; v++) {
+                    int cost = arr[i] * v;
+                    if (k >= cost) 
+                        dp[i][j][k] = (dp[i][j][k] + dp[i - 1][j - v][k - cost]) % mod;
+                }
+            }
+        
+    int ans = 0;
+    for (int k = 0; k <= b; k++)
+        ans = (ans + dp[n][m][k]) % mod;
 
-	cout << dp[n][m][b] << endl;
-	return 0;
+    cout << ans << "\n";
+    return 0;
 }
