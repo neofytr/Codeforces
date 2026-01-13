7b6393f9c7801fa386d4338f59dc6dc35b1377c2
neofytr <imagination12344321.com@gmail.com>
2026-01-13 11:00:01 +0530

    Practiced CF problems

--- random/1400/groupingIncreases.cpp
+++ random/1400/groupingIncreases.cpp
@@ -5,9 +5,8 @@ using namespace std;
 #define MAX (int)(2 * 1e5)
 #define INF (int)(1e16)
 
-int arr[MAX + 1];
-int dp[MAX + 1][2];
-int p[MAX + 1];
+int arr[MAX];
+pair<int, int> dp[MAX + 1];
 
 void solve() {
 	int n;
@@ -17,23 +16,17 @@ void solve() {
 	for (int r = 1; r <= n; r++) cin >> arr[r];
 
 	p[0] = 0;
-	for (int r = 2; r <= n; r++)
-		p[r] = (p[r - 1] + (arr[r] > arr[r - 1] ? 1 : 0));
+	for (int r = 1; r <= n; r++)
+		p[r] += p[r - 1] + (arr[r] > arr[r - 1] ? 1 : 0);
 
-	// dp[i][0] is the minimum possible penality for arr[1, r] if r goes into s
-	// dp[i][1] is the minimum possible penality for arr[1, r] if r goes into t
-	for (int i = 0; i <= n; i++)
-		dp[i][0] = dp[i][1] = INF;
-
-	dp[0][0] = dp[0][1] = 0;
-	for (int i = 1; i <= n; i++) 
-		for (int j = 0; j <= i - 1; j++) {
-			dp[i][0] = min(dp[i][0], dp[j][0] + p[i - 1] - p[j + 1] + (arr[i] > arr[j] ? 1 : 0));
-			dp[i][1] = min(dp[i][1], dp[j][1] + p[i - 1] - p[j + 1] + (arr[i] > arr[j] ? 1 : 0));
+	dp[1] = {0, 0};
+	for (int i = 2; i <= n; r++) {
+		// all the elements are in the other array
+		dp[i] = {p[i - 1] - p[1], i - 1};
+		for (int j = 1; j <= i - 1; j++) {
+			auto [v, idx] = dp[j];
 		}
-
-
-	cout << min(dp[n][0], dp[n][1]) << endl;
+	}
 }
 
 int32_t main() {
