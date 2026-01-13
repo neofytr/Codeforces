5f13ea387a9a87716ec2af03f4065f771e40ba1d
neofytr <imagination12344321.com@gmail.com>
2026-01-13 10:00:01 +0530

    Problem solving session

--- random/1400/groupingIncreases.cpp
+++ random/1400/groupingIncreases.cpp
@@ -13,10 +13,10 @@ void solve() {
 	int n;
 	cin >> n;
 
-	arr[0] = -INF;
+	arr[0] = INF;
 	for (int r = 1; r <= n; r++) cin >> arr[r];
 
-	p[1] = 0;
+	p[0] = 0;
 	for (int r = 2; r <= n; r++)
 		p[r] = (p[r - 1] + (arr[r] > arr[r - 1] ? 1 : 0));
 
@@ -25,10 +25,15 @@ void solve() {
 	for (int i = 0; i <= n; i++)
 		dp[i][0] = dp[i][1] = INF;
 
-	for (int i = 1; i <= n; r++) 
-		for (int j = 0; j <= i; i++) { // index j is the last element in s
-
+	dp[0][0] = dp[0][1] = 0;
+	for (int i = 1; i <= n; i++) 
+		for (int j = 0; j <= i - 1; j++) {
+			dp[i][0] = min(dp[i][0], dp[j][0] + p[i - 1] - p[j + 1] + (arr[i] > arr[j] ? 1 : 0));
+			dp[i][1] = min(dp[i][1], dp[j][1] + p[i - 1] - p[j + 1] + (arr[i] > arr[j] ? 1 : 0));
 		}
+
+
+	cout << min(dp[n][0], dp[n][1]) << endl;
 }
 
 int32_t main() {
