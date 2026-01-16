58e2584c27ffc63fdd973475d9c76da15ac7d3a3
neofytr <imagination12344321.com@gmail.com>
2026-01-16 14:00:01 +0530

    Solved problem

--- random/1400/ivaPav.cpp
+++ random/1400/ivaPav.cpp
@@ -7,13 +7,19 @@ void solve() {
 	int n;
 	cin >> n;
 
-	vector<int> arr(n + 1);
+	vector<int> arr(n + 2, 0);
 	for (int r = 1; r <= n; r++) cin >> arr[r];
 
 	vector<vector<int>> p(n + 2, vector<int>(32, 0));
-	for (int r = n; r >= 1; r--) 
-		for (int bit = 0; bit < 32; bit++)
-			p[r][bit] += p[r + 1][bit] + (bool)((1ll << bit) & arr[r]);
+	for (int r = n; r >= 1; r--) {
+	for (int bit = 0; bit < 32; bit++) {
+		if (arr[r] & (1LL << bit))
+			p[r][bit] = 1 + p[r + 1][bit];
+		else
+			p[r][bit] = 0;
+	}
+}
+
 
 	int q;
 	cin >> q;
@@ -26,7 +32,7 @@ void solve() {
 		for (int bit = 31; bit >= 0; bit--)
 			if (k & (1ll << bit)) {
 				leadingOne = bit;
-				break;s
+				break;
 			}
 
 		if (leadingOne == -1) {
@@ -34,12 +40,27 @@ void solve() {
 			continue;
 		}
 
-		beforeZero = 0;
+		int front = 0;
+		int one = 0, latestZero = 0;
 		for (int bit = 31; bit > leadingOne; bit--)
-			beforeZero = max(beforeZero, p[l][bit]);
+			front = max(front, p[l][bit]);
+		one = p[l][leadingOne];
 
-		int one = 0, zero = 0;
+		for (int bit = leadingOne - 1; bit >= 0; bit--) {
+			int j = p[l][bit];
+			if (k & (1ll << bit)) 
+				if (j < one) one = max(latestZero, j);
+			else 
+				latestZero = max(latestZero, j), latestZero = min(latestZero, one);
+		}
+
+		int final = max({front, one});
+		if (!final)
+			cout << -1 << " ";
+		else 
+			cout << l + final - 1 << " ";
 	}
+	cout << endl;
 }
 
 int32_t main() {
