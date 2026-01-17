a627af70d871c53fedea94d135d0c719f6612ec4
neofytr <imagination12344321.com@gmail.com>
2026-01-17 22:00:01 +0530

    Worked on Codeforces problems

--- atcoderContest/ABC/441/e.cpp
+++ atcoderContest/ABC/441/e.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 #define int long long 
+#define MAX (int)(5 * 1e5)
+#define OFFSET MAX
 
+int str[MAX + 1];
+int cnt[MAX * 2 + 1];
 int32_t main() {
 	int n;
 	cin >> n;
@@ -10,11 +14,5 @@ int32_t main() {
 	string s;
 	cin >> s;
 
-	vector<int> str(n + 1);
-	for (int r = 1; r <= n; r++) str[r] = s[r - 1] - 'A';
-
-	vector<int> A(n + 1, 0), B(n + 1, 0);
- 	for (int r = 1; r <= n; r++)
- 		A[r] += A[r - 1] + (!str[r] ? 1 : 0), B[r] += B[r - 1] + (str[r] == 1 ? 0 : 1);
 	return 0;
 }
