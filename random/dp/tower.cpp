f311d9edfd9f6c957f6a3775166e8f396697750c
neofytr <imagination12344321.com@gmail.com>
2026-01-17 16:00:01 +0530

    Solved some problems

--- random/dp/tower.cpp
+++ random/dp/tower.cpp
@@ -2,37 +2,44 @@
 using namespace std;
 
 #define int long long
-#define MAX_N (int)(1e3)
-#define MAX_W (int)(1e4)
-#define INF (int)(1e15)
+#define INF (int)1e18
+#define MAX_W 10000000  // 1e7
 
-int dp[MAX_N + 1][MAX_W + 1];
-int w[MAX_N + 1], s[MAX_N + 1], v[MAX_N + 1];
-pair<int, int> cpy[MAX_N + 1];
+int dp[MAX_W + 1];
 
 int32_t main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
 	int n;
 	cin >> n;
 
-	for (int r = 1; r <= n; r++) cin >> w[r] >> s[r] >> v[r], cpy[r] = {s[r], r};
-	sort(cpy + 1, cpy + n + 1);
+	vector<int> w(n), s(n), v(n);
+	vector<int> ord(n);
 
-	for (int r = 0; r <= n; r++)
-		for (int x = 0; x <= MAX_W; x++)
-			dp[r][x] = -INF;
+	for (int i = 0; i < n; i++) {
+		cin >> w[i] >> s[i] >> v[i];
+		ord[i] = i;
+	}
 
-	dp[0][0] = 0;
-	for (int r = 1; r <= n; r++) {
-		dp[r][0] = 0;
-		for (int x = 1; x <= MAX_W; x++) {
-			dp[r][x] = dp[r - 1][x];
-			if (x >= w[r] && x - w[r] <= s[r]) dp[r][x] = max(dp[r][x], dp[r - 1][x - w[r]] + v[r]);
+	sort(ord.begin(), ord.end(), [&](int a, int b) {
+		return w[a] + s[a] < w[b] + s[b];
+	});
+
+	for (int i = 0; i <= MAX_W; i++) dp[i] = -INF;
+	dp[0] = 0;
+
+	for (int idx : ord) {
+		for (int x = s[idx]; x >= 0; x--) {
+			if (dp[x] == -INF) continue;
+			dp[x + w[idx]] = max(dp[x + w[idx]], dp[x] + v[idx]);
 		}
 	}
 
-	int maxi = LLONG_MIN;
-	for (int x = 0; x <= MAX_W; x++)
-		maxi = max(maxi, dp[n][x]);
-	cout << maxi << endl;
+	int ans = 0;
+	for (int i = 0; i <= MAX_W; i++)
+		ans = max(ans, dp[i]);
+
+	cout << ans << '\n';
 	return 0;
 }
