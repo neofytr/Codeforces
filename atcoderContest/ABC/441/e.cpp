d9444ecc9f577d3a2034bb467cf1437e48277f53
neofytr <imagination12344321.com@gmail.com>
2026-01-18 22:00:01 +0530

    More problems solved

--- atcoderContest/ABC/441/e.cpp
+++ atcoderContest/ABC/441/e.cpp
@@ -14,5 +14,20 @@ int32_t main() {
 	string s;
 	cin >> s;
 
+	for (int r = 1; r <= n; r++) str[r] = s[r - 1] - 'A';
+
+	int sum = 0, d = 0, res = 0;
+	cnt[d + OFFSET] = 1;
+
+	for (int r = 1; r <= n; r++) {
+		if (!str[r])
+			sum += cnt[d + OFFSET], d++, cnt[d + OFFSET]++, res += sum;
+		if (str[r] == 1)
+			sum -= cnt[d - 1 + OFFSET], d--, cnt[d + OFFSET]++, res += sum;
+		if (str[r] == 2)
+			cnt[d + OFFSET]++, res += sum;
+	}
+
+	cout << res << endl;
 	return 0;
 }
