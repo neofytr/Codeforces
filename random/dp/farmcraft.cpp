5a1ec7d4b7130a96daa6ce5c4ddfc90aac4719ad
neofytr <imagination12344321.com@gmail.com>
2026-01-21 16:00:01 +0530

    Solved some problems

--- random/dp/farmcraft.cpp
+++ random/dp/farmcraft.cpp
@@ -5,9 +5,14 @@ using namespace std;
 #define MAX (int)(5 * 1e5)
 
 int c[MAX + 1];
+vector<int> tree(MAX + 1);
+
 int32_t main() {
 	int n;
 	cin >> n;
 
+	int a, b;
+	for (int r = 1; r <= n - 1; r++)
+		cin >> a >> b, tree[a].push_back(b), tree[b].push_back(a);
 	return 0;
 }
