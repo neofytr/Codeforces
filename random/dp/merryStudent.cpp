ae6e1f56374318879b0d613ae209c99a1a8f8d7f
neofytr <imagination12344321.com@gmail.com>
2026-01-22 19:00:01 +0530

    Solved a CF problem

--- random/dp/merryStudent.cpp
+++ random/dp/merryStudent.cpp
@@ -1,89 +1,112 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long 
-#define MAX_N (int)(5 * 1e2)
-#define MAX_K (int)(1e2)
-#define MAX_T (int) (MAX_N * MAX_K)
+#define int long long
+#define MAX_N 500
+#define MAX_K 100
 
 int k[MAX_N + 1];
 int p[MAX_N + 1][MAX_K + 1];
 int w[MAX_N + 1][MAX_K + 1];
-int s[MAX_N + 1][MAX_K + 1];
+
 int ord[MAX_N + 1][MAX_K + 1];
-int ordsum[MAX_N + 1];
 int cls[MAX_N + 1];
+
+int ordsum[MAX_N + 1];
 int wsum[MAX_N + 1];
 int psum[MAX_N + 1];
 
-int curr = -1;
-auto cmpwithin = [](int one, int two) -> bool {
-	int t1 = w[curr][one] * p[curr][one] + w[curr][two] * (p[curr][one] + p[curr][two]);
-	int t2 = w[curr][two] * p[curr][two] + w[curr][one] * (p[curr][one] + p[curr][two]);
-	if (t1 < t2) return true;
-	return false;
+int curr;
+
+auto cmpwithin = [](int a, int b) {
+	int t1 = (int)w[curr][a] * p[curr][a]
+	            + (int)w[curr][b] * (p[curr][a] + p[curr][b]);
+	int t2 = (int)w[curr][b] * p[curr][b]
+	            + (int)w[curr][a] * (p[curr][a] + p[curr][b]);
+	return t1 < t2;
 };
 
-auto cmp = [](int one, int two) -> bool {
-	int t1 = ordsum[one] + psum[one] * wsum[two] + ordsum[two];
-	int t2 = ordsum[two] + psum[two] * wsum[one] + ordsum[one];
-	if (t1 < t2) return true;
-	return false;
+auto cmpclass = [](int a, int b) {
+	int t1 = ordsum[a] + (int)psum[a] * wsum[b] + ordsum[b];
+	int t2 = ordsum[b] + (int)psum[b] * wsum[a] + ordsum[a];
+	return t1 < t2;
 };
 
 int32_t main() {
-	// freopen("student.in", "r", stdin);
-	// freopen("student.out", "w", stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
 
 	int n;
 	cin >> n;
 
-	for (int c = 1; c <= n; c++)
-		cin >> k[c];
+	for (int i = 1; i <= n; i++)
+		cin >> k[i];
 
-	for (int c = 1; c <= n; c++)
-		for (int l = 1; l <= k[c]; l++)
-			cin >> p[c][l];
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= k[i]; j++)
+			cin >> p[i][j];
 
-	for (int c = 1; c <= n; c++)
-		for (int l = 1; l <= k[c]; l++)
-			cin >> w[c][l];
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= k[i]; j++)
+			cin >> w[i][j];
 
-	for (int c = 1; c <= n; c++) {
-		wsum[c] = 0;
-		for (int l = 1; l <= k[c]; l++)
-			wsum[c] += w[c][l];
-	}
-
-	for (int c = 1; c <= n; c++) {
-		psum[c] = 0;
-		for (int l = 1; l <= k[c]; l++)
-			psum[c] += p[c][l];
-	}
-
-	for (int c = 1; c <= n; c++) 
-		for (int l = 1; l <= k[c]; l++)
-			ord[c][l] = l;
-
-	for (int c = 1; c <= n; c++) {
-		curr = c;
-		sort(ord[c] + 1, ord[c] + k[c] + 1, cmp);
-
-		int t = 0;
-		ordsum[c] = 0;
-		for (int l = 1; l <= k[c]; l++) {
-			int elt = ord[c][l];
-			t += p[c][elt];
-			ordsum[c] += t * w[c][elt];
+	for (int i = 1; i <= n; i++) {
+		wsum[i] = psum[i] = 0;
+		for (int j = 1; j <= k[i]; j++) {
+			wsum[i] += w[i][j];
+			psum[i] += p[i][j];
+			ord[i][j] = j;
 		}
 	}
 
-	for (int c = 1; c <= n; c++)
-		cls[c] = c;
+	for (int i = 1; i <= n; i++) {
+		curr = i;
+		sort(ord[i] + 1, ord[i] + k[i] + 1, cmpwithin);
 
-	sort(cls + 1, cls + n + 1, cmp);
-	for (int c = 1; c <= n; c++)
-		cout << cls[c] << " ";
-	cout << endl;
+		int t = 0;
+		ordsum[i] = 0;
+		for (int j = 1; j <= k[i]; j++) {
+			int lab = ord[i][j];
+			t += p[i][lab];
+			ordsum[i] += t * w[i][lab];
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+		cls[i] = i;
+
+	sort(cls + 1, cls + n + 1, cmpclass);
+
+	int gid = 1;
+	static int lab_id[MAX_N + 1][MAX_K + 1];
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= k[i]; j++)
+			lab_id[i][j] = gid++;
+
+	long long total_cost = 0;
+	long long cur_time = 0;
+
+	for (int i = 1; i <= n; i++) {
+		int c = cls[i];
+		for (int j = 1; j <= k[c]; j++) {
+			int lab = ord[c][j];
+			cout << lab_id[c][lab] << " ";
+			cur_time += p[c][lab];
+			total_cost += cur_time * w[c][lab];
+		}
+	}
+
+	for (int i = 1; i <= n; i++) {
+		int c = cls[i];
+		for (int j = 1; j <= k[c]; j++) {
+			int lab = ord[c][j];
+			cout << lab_id[c][lab] << " ";
+			cur_time += p[c][lab];
+			total_cost += cur_time * w[c][lab];
+		}
+	}
+	cout << "\n";
+	cout << total_cost << "\n";
+
 	return 0;
 }
