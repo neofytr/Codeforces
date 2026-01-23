6cafc47c45a77c1ae08810d536ea4cbe8a95f255
neofytr <imagination12344321.com@gmail.com>
2026-01-23 17:00:01 +0530

    Worked on Codeforces problems

--- random/1400/addModulo10.cpp
+++ random/1400/addModulo10.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 #define int long long
 
+int calc(int x) {
+	int ones = x % 10;
+	int twos = ((x % 100) - ones) / 10;
+
+	switch (ones) {
+	case 1:
+	case 2:
+	case 4:
+	case 8:
+		return (twos & 1);
+
+	case 3:
+	case 6:
+	case 7:
+	case 9:
+		return !(twos & 1);
+	}
+
+	return 0;
+}
+
 void solve(int t) {
 	int n;
 	cin >> n;
@@ -18,44 +39,33 @@ void solve(int t) {
 		cin >> v, s.insert(v);
 
 	bool found = false;
-	int alpha = -1;
 	for (int e : s)
-		if (!found && !(e % 10)) found = true, alpha = e;
+		if (!found && !(e % 10)) found = true;
 		else if (found && !(e % 10)) {
 			cout << "NO" << endl;
 			return;
 		}
 
 	if (found) {
-		s.erase(alpha);
-		for (int e : s) {
-			int rem = e % 10;
-			if (e > alpha || ((alpha - e) % rem)) {
-				cout << "NO" << endl;
-				return;
-			}
-	
-			int k = ((alpha - e) / rem) + 1;
-			if (__builtin_popcountll(k) > 1) {
-				cout << "NO" << endl;
-				return;
-			}
-		}
-		cout << "YES" << endl;
-		return;
+		
 	}
 
-	int a1 = *s.begin(), rem1 = a1 % 10;
-	int a2 = *(++s.begin()), rem2 = a2 % 10;
+	// no remainder 0 from here
+	for (int e : s)
+		if (e % 10 == 5) {
+			for (int g : s)
+				if (g != e) {
+					cout << "NO" << endl;
+					return;
+				}
 
-	set<int> psbl;
-	for (int j1 = 0; j1 <= 21; j1++)
-		for (int j2 = 0; j2 <= 21; j2++) 
-			if (a2 - a1 + rem2 - rem1 == (1ll << j1) * rem1 - (1ll << j2) * rem2)
-				psbl.insert(a1 + ((1ll << j1) - 1) * rem1);
+			cout << "YES" << endl;
+			return;
+		}
 
-	s.erase(a1);
-	s.erase(a2);
+	// no remainder 0 and 5 from here
+	int a1 = *s.begin();
+	
 }
 
 int32_t main() {
