25f1691758b527bdaaf0808b3645c563e869fb67
neofytr <imagination12344321.com@gmail.com>
2026-01-24 03:00:01 +0530

    Solved some problems

--- random/1400/arrangingTheSheep.cpp
+++ random/1400/arrangingTheSheep.cpp
@@ -15,7 +15,27 @@ void solve() {
 	for (int r = 1; r <= n; r++)
 		vec[r] = (str[r - 1] == '*' ? (cnt++, 1) : 0);
 
-	
+	int i = 1;
+	vector<int> s(cnt + 1);
+	unordered_map<int, int> f;
+	for (int r = 1; r <= n; r++)
+		if (vec[r]) s[i] = r, f[n - cnt + i - s[i]]++, i++;
+
+	int cost = 0;
+	int d = 0;
+
+	for (int k = 1; k <= cnt; k++)
+		cost += abs(n - cnt + k - s[k]);
+
+	d += f[0];
+	int mini = cost;
+	for (int r = 2; r <= n - cnt + 1; r++) {
+		cost += (2 * d - cnt);
+		d += f[r - 1];
+		mini = min(mini, cost);
+	}
+
+	cout << mini << endl;
 }
 
 int32_t main() {
