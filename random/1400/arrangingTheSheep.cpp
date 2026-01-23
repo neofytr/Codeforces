4e0d5aaa0e6f082e69233b327109a1d125096dc1
neofytr <imagination12344321.com@gmail.com>
2026-01-24 02:00:01 +0530

    Solved a CF problem

--- random/1400/arrangingTheSheep.cpp
+++ random/1400/arrangingTheSheep.cpp
@@ -15,12 +15,7 @@ void solve() {
 	for (int r = 1; r <= n; r++)
 		vec[r] = (str[r - 1] == '*' ? (cnt++, 1) : 0);
 
-	int i = 1;
-	vector<int> g(cnt + 1);
-	for (int r = 1; r <= n; r++)
-		if (vec[r]) g[i++] = n - cnt + k - r;
-
-	cout << mini << endl;
+	
 }
 
 int32_t main() {
