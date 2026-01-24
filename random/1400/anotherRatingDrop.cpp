87df196b044ecfb941e2c7f486d474f9feb514f3
neofytr <imagination12344321.com@gmail.com>
2026-01-24 18:00:01 +0530

    Solved problem

--- random/1400/anotherRatingDrop.cpp
+++ random/1400/anotherRatingDrop.cpp
@@ -10,10 +10,14 @@ void solve() {
 	int res = 0;
 	for (int a = 0; a <= 62; a++) {
 		int b = (1ll << a);
-		res += a * ((n - b + 1) / b + 1);
+		if (n - b >= 0) {
+			int g = ((n - b) / b + 1);
+			g -= g / 2;
+			res += a * g;
+		}
 	}
 
-	cout << res << endl;
+	cout << res + n << endl;
 }
 
 int32_t main() {
