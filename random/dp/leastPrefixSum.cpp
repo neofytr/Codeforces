14dbb44b09c33a6a8a4e5578a7f0686936bd56cc
neofytr <imagination12344321.com@gmail.com>
2026-01-28 00:00:01 +0530

    CF grind

--- random/dp/leastPrefixSum.cpp
+++ random/dp/leastPrefixSum.cpp
@@ -10,32 +10,50 @@ void solve() {
 	vector<int> arr(n + 1);
 	for (int r = 1; r <= n; r++) cin >> arr[r];
 
-	int sm = 0;
-	for (int r = 1; r <= m; r++)
-		sm += arr[r];
-
 	int cnt = 0;
-	int s = 0;
-	int mini = LLONG_MAX;
-	for (int r = 1; r <= m - 1; r++) {
-		s += arr[r];
-		mini = min(s, mini);
+
+	priority_queue<int> pq;
+	int current_suffix_sum = 0;
+
+	for (int i = m; i >= 2; i--) {
+		current_suffix_sum += arr[i];
+
+		if (arr[i] > 0) {
+			pq.push(arr[i]);
+		}
+
+		while (current_suffix_sum > 0) {
+			int largest = pq.top();
+			pq.pop();
+			current_suffix_sum -= 2 * largest;
+			cnt++;
+		}
 	}
 
-	multiset<int> ms;
-	for (int r = 1; r <= m; r++)
-		ms.insert(arr[r]);
+	int s = 0;
+	priority_queue<int, vector<int>, greater<int>> min_pq;
 
-	for (auto itr = ms.rbegin(); itr != ms.rend(); ++itr)
-		if (sm > mini) cnt++, sm += -2 * (*itr);
+	for (int r = m + 1; r <= n; r++) {
+		s += arr[r];
+		if (arr[r] < 0) min_pq.push(arr[r]);
 
-	cout << cnt << endl;	
+		while (s < 0) {
+			auto elt = min_pq.top();
+			min_pq.pop();
+			s -= 2 * elt;
+			cnt++;
+		}
+	}
+
+	cout << cnt << endl;
 }
 
 int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int t;
 	cin >> t;
-
 	while (t--) solve();
 	return 0;
 }
---
+++ random/dp/natashaSasha.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MOD (int)(998244853)
+
+int32_t main() {
+	int n, m;
+	cin >> n >> m;
+}
