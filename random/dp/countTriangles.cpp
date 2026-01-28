f8d706d28ccf43d8e63c4cafcb3bbf4990941d69
neofytr <imagination12344321.com@gmail.com>
2026-01-28 12:00:01 +0530

    Solved a CF problem

--- random/dp/countTriangles.cpp
+++ random/dp/countTriangles.cpp
@@ -4,10 +4,29 @@ using namespace std;
 #define int long long
 
 int32_t main() {
-	int a, b, c, d;
-	cin >> a >> b >> c >> d;
+	int A, B, C, D;
+	cin >> A >> B >> C >> D;
 
-	res = 0;
-	for (int r = max(A + B, C); r <= D; r++)
+	int res = 0;
+	for (int r = C; r <= D + 1; r++) {
+		int x = 0;
+		x += max(0ll, (r - (A + B) + 1));
+		x -= max(0ll, r - 2 * B);
+		x -= max(0ll, r - A - C);
+		x += max(0ll, r - B - C - 1);
 
+		res += x * max(0ll, r - C);
+	}
+
+	for (int r = D + 2; r <= B + C; r++) {
+		int x = 0;
+		x += max(0ll, (r - (A + B) + 1));
+		x -= max(0ll, r - 2 * B);
+		x -= max(0ll, r - A - C);
+		x += max(0ll, r - B - C - 1);
+
+		res += x * max(0ll, D - C + 1);
+	}
+
+	cout << res << endl;
 }
---
+++ random/dp/unstableString.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	string s;
+	cin >> s;
+
+	int n = (int)s.length();
+	vector<int> v(n + 1);
+	for (int r = 1; r <= n; r++)
+		if (s[r - 1] == '?') v[r] = 2;
+		else v[r] = s[r - 1] - '0';
+
+	vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+	if (v[1] == 1) dp[1][1] = 1;
+	if (v[1] == 0) dp[1][0] = 1;
+	if (v[1] == 2) dp[1][0] = dp[1][1] = 1;
+
+	for (int r = 2; r <= n; r++) {
+		if (v[r] == 0) {
+			dp[r][0] = dp[r - 1][1] + 1;
+		}
+		else if (v[r] == 1) {
+			dp[r][1] = dp[r - 1][0] + 1;
+		}
+		else {
+			dp[r][0] = dp[r - 1][1] + 1;
+			dp[r][1] = dp[r - 1][0] + 1;
+		}
+	}
+
+	int res = 0;
+	for (int r = 1; r <= n; r++)
+		res += (dp[r][0] + dp[r][1]);
+	cout << res << endl;
+}
+
+int32_t main() {
+	int t;
+	cin >> t;
+
+	while (t--) solve();
+	return 0;
+}
