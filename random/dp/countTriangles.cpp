8f4f50b3e788077492e075265dc58aa2645feac5
neofytr <imagination12344321.com@gmail.com>
2026-01-28 11:00:02 +0530

    Worked on Codeforces problems

---
+++ random/dp/countTriangles.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	res = 0;
+	for (int r = max(A + B, C); r <= D; r++)
+
+}
