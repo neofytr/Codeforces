3a697ba25d9bccff426fbadcf0b50c54db6f3949
neofytr <imagination12344321.com@gmail.com>
2026-01-31 22:00:01 +0530

    Solved problem

--- segmentTrees/problems/firstElementAtleastX2.cpp
+++ segmentTrees/problems/firstElementAtleastX2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define int long long
 #define MAX (int)(1e5)
 
-pair<int, int> tree[MAX + 1];
+pair<int, int> tree[4 * MAX + 1];
 void build(int idx, int l, int r, vector<int> &arr) {
 	if (l == r) {
 		tree[idx] = {arr[l], l};
@@ -42,8 +42,17 @@ void update(int idx, int l, int r, int i, int v) {
 	else tree[idx] = {left, min(leftidx, rightidx)};
 }
 
-int query(int idx, int l, int r, int ql, int qr, int x) {
-	if (r < ql || l > qr) return LLONG_MAX;
+int query(int idx, int l, int r, int ql, int x) {
+	if (tree[idx].first < x) return LLONG_MAX;
+	if (r < ql) return LLONG_MAX;
+	if (l == r) return l;
+
+	int m = (l + r) >> 1;
+	int left = LLONG_MAX;
+	if (tree[2 * idx].first >= x)
+		left = query(2 * idx, l, m, ql, x);
+	if (left != LLONG_MAX) return left;
+	return query(2 * idx + 1, m + 1, r, ql, x);
 }
 
 int32_t main() {
@@ -53,14 +62,17 @@ int32_t main() {
 	vector<int> arr(n + 1);
 	for (int r = 1; r <= n; r++) cin >> arr[r];
 
+	build(1, 1, n, arr);
 	while(m--) {
 		int q, v, r;
 		cin >> q >> v >> r;
 
 		if (q == 1) 
 			update(1, 1, n, v + 1, r);
-		else 
-			cout << query(1, 1, n, r + 1, v) - 1 << endl;
+		else { 
+			int j = query(1, 1, n, r + 1, v);
+			cout << (j == LLONG_MAX ? -1 : j - 1) << endl;
+		}
 	}
 	return 0;
 }
