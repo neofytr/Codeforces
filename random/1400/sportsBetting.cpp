5352790afe5d72e7d205da771541e35242bd2b6e
neofytr <imagination12344321.com@gmail.com>
2026-02-06 14:00:01 +0530

    More problems solved

--- random/1400/sportsBetting.cpp
+++ random/1400/sportsBetting.cpp
@@ -5,6 +5,39 @@ using namespace std;
 
 void solve() {
 	int n; cin >> n;
+	unordered_map<int, int> f;
+
+	int v;
+	for (int r = 1; r <= n; r++) cin >> v, f[v]++;
+
+	for (auto &[e, c] : f) {
+		if (c >= 4) {
+			cout << "YES" << endl;
+			return;
+		}
+
+		if (c >= 2 && (f[e - 1] >= 2 || f[e + 1] >= 2)) {
+			cout << "YES" << endl;
+			return;
+		}
+
+		if (c >= 1 && f[e - 1] >= 2 && f[e + 1] >= 2) {
+			cout << "YES" << endl;
+			return;
+		}
+
+		if (c >= 3 && (f[e - 1] >= 1 || f[e + 1] >= 1)) {
+			cout << "YES"  << endl;
+			return;
+		}
+
+		if (c >= 1 && f[e - 1] >= 2 && f[e + 1] >= 1 && f[e + 2] >= 2) {
+			cout << "YES" << endl;
+			return;
+		}
+	}
+
+	cout << "NO" << endl;
 }
 
 int32_t main() {
