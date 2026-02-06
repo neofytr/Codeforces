63ee7b95e54e8816270be5e34f475345287e569d
neofytr <imagination12344321.com@gmail.com>
2026-02-06 15:00:02 +0530

    Practiced CF problems

--- random/1400/sportsBetting.cpp
+++ random/1400/sportsBetting.cpp
@@ -1,47 +1,78 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-#define int long long
-
 void solve() {
-	int n; cin >> n;
-	unordered_map<int, int> f;
+    int n;
+    if (!(cin >> n)) return;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
 
-	int v;
-	for (int r = 1; r <= n; r++) cin >> v, f[v]++;
+    sort(a.begin(), a.end());
+    for (int i = 0; i + 3 < n; ++i) {
+        if (a[i] == a[i+3]) {
+            cout << "Yes" << "\n";
+            return;
+        }
+    }
 
-	for (auto &[e, c] : f) {
-		if (c >= 4) {
-			cout << "YES" << endl;
-			return;
-		}
+ 
+    
+    vector<pair<int, int>> counts;
+    if (n > 0) {
+        int current_val = a[0];
+        int current_count = 1;
+        for (int i = 1; i < n; ++i) {
+            if (a[i] == current_val) {
+                current_count++;
+            } else {
+                counts.push_back({current_val, current_count});
+                current_val = a[i];
+                current_count = 1;
+            }
+        }
+        counts.push_back({current_val, current_count});
+    }
 
-		if (c >= 2 && (f[e - 1] >= 2 || f[e + 1] >= 2)) {
-			cout << "YES" << endl;
-			return;
-		}
+    bool active_chain = false;
+    
+    for (size_t i = 0; i < counts.size(); ++i) {
+        int day = counts[i].first;
+        int count = counts[i].second;
+        
+        if (i > 0 && day != counts[i-1].first + 1) {
+            active_chain = false;
+        }
 
-		if (c >= 1 && f[e - 1] >= 2 && f[e + 1] >= 2) {
-			cout << "YES" << endl;
-			return;
-		}
+        if (count >= 2) {
+            if (active_chain) {
+                cout << "Yes" << "\n";
+                return;
+            }
+            active_chain = true;
+        } else {
+           
+        }
+    }
 
-		if (c >= 3 && (f[e - 1] >= 1 || f[e + 1] >= 1)) {
-			cout << "YES"  << endl;
-			return;
-		}
-
-		if (c >= 1 && f[e - 1] >= 2 && f[e + 1] >= 1 && f[e + 2] >= 2) {
-			cout << "YES" << endl;
-			return;
-		}
-	}
-
-	cout << "NO" << endl;
+    cout << "No" << "\n";
 }
 
-int32_t main() {
-	int t; cin >> t;
-	while (t--) solve();
-	return 0;
+int main() {
+    // Optimize I/O operations
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
 }
