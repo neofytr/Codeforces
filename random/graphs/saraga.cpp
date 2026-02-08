4a19117f497e1adc0c6a49f7a014ba2c83e0a985
neofytr <imagination12344321.com@gmail.com>
2026-02-08 23:00:01 +0530

    CF grind

--- random/graphs/saraga.cpp
+++ random/graphs/saraga.cpp
@@ -14,6 +14,33 @@ int32_t main() {
 	for (int r = 1; r <= m; r++)
 		T[r] = t[r - 1] - 'a';
 
+	if (m == 1 || n == 1) {
+		cout << -1 << endl;
+		return 0;
+	}
 
+	vector<int> f(26, LLONG_MIN);
+	for (int r = 1; r <= m - 1; r++)
+		f[T[r]] = r;
+
+	int mini = LLONG_MAX;
+	int p;
+	for (int r = 1; r <= n - 1; r++) {
+		int nxt = S[r + 1];
+		if (f[nxt] == LLONG_MIN) continue;
+		if (r + 1 + (m - (f[nxt] + 1) + 1) < mini)
+			mini = r + 1 + (m - (f[nxt] + 1) + 1), p = r;
+	}
+	if (mini == LLONG_MAX) {
+		cout << -1 << endl;
+		return 0;
+	}
+
+	string res;
+	for (int r = 1; r <= p + 1; r++)
+		res.push_back(char(S[r] + 'a'));
+	for (int r = f[S[p + 1]] + 1; r <= m; r++)
+		res.push_back(char(T[r] + 'a'));
+	cout << res << endl;
 	return 0;
 }
