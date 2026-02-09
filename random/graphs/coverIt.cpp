2522e230da6b10501235c34b88aeb9bcd974b8d2
neofytr <imagination12344321.com@gmail.com>
2026-02-09 23:00:01 +0530

    Problem solving session

--- random/graphs/coverIt.cpp
+++ random/graphs/coverIt.cpp
@@ -8,26 +8,44 @@ void solve() {
 
 	int u, v;
 	vector<vector<int>> graph(n + 1);
-	vector<int> deg(n + 1, 0);
 	while (m--)
-		cin >> u >> v, graph[u].push_back(v), graph[v].push_back(u), deg[u]++, deg[v]++;
+		cin >> u >> v, graph[u].push_back(v), graph[v].push_back(u);
 
-	set<pair<int, int>> pq;
-	vector<bool> sel(n + 1, false);
-	for (int r = 1; r <= n; r++)
-		pq.insert({-deg[r], r});
+	int src = 1;
+	vector<bool> sel(n + 1, false), vis(n + 1, false);
+	queue<pair<int, int>> que;
 
-	int cnt = 0;
-	while (!pq.empty()) {
-		auto [d, x] = *pq.begin();
-		pq.erase(pq.begin());
-
-		sel[x] = true;
-		cnt++;
+	vis[src] = true, que.push({src, 0});
+	while (!que.empty()) {
+		auto [x, d] = que.front(); que.pop();
+		if (!(d & 1)) sel[x] = true;
 		for (int v : graph[x])
-			pq.erase({-deg[v], v});
+			if (!vis[v]) vis[v] = true, que.push({v, d + 1}); 
 	}
 
+	int cnt = 0;
+	for (int r = 1; r <= n; r++)
+		if (sel[r]) cnt++;
+	if (cnt <= n / 2) {
+		cout << cnt << endl;
+		for (int r = 1; r <= n; r++)
+			if (sel[r]) cout << r << " ";
+		cout << endl;
+		return;
+	}
+
+	for (int r = 1; r <= n; r++) sel[r] = vis[r] = false;
+	vis[src] = true, que.push({src, 0});
+	while (!que.empty()) {
+		auto [x, d] = que.front(); que.pop();
+		if (d & 1) sel[x] = true;
+		for (int v : graph[x])
+			if (!vis[v]) vis[v] = true, que.push({v, d + 1});
+	}
+
+	cnt = 0;
+	for (int r = 1; r <= n; r++)
+		if (sel[r]) cnt++;
 	cout << cnt << endl;
 	for (int r = 1; r <= n; r++)
 		if (sel[r]) cout << r << " ";
