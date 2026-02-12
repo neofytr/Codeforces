9cf96bd74a64c169cb25f291fd8d472ca6920cd6
neofytr <imagination12344321.com@gmail.com>
2026-02-12 21:00:01 +0530

    Practiced CF problems

--- random/1400/coinsAndQueries.cpp
+++ random/1400/coinsAndQueries.cpp
@@ -5,5 +5,33 @@ using namespace std;
 
 int32_t main() {
 	int n, q; cin >> n >> q;
-	vector<int> d(64 + 1, 0);
+	vector<int> d(63 + 1, 0);
+
+	int v;
+	for (int r = 1; r <= n; r++) {
+		cin >> v;
+		for (int b = 0; b <= 63; b++)
+			if (v & (1ll << b)) {
+				d[b]++;
+				break;
+			}
+	}
+
+	while (q--) {
+		cin >> v;
+		int cnt = 0;
+		int left = 0;
+		for (int b = 63; b >= 0; b--) {
+			if (v & (1ll << b)) left += (1ll << b);
+			if (d[b]) {
+				int req = left >> b;
+				int j = min(d[b], req);
+				cnt += j;
+				left -= j * (1ll << b);
+			}
+		}
+		if (left) cout << -1 << endl;
+		else cout << cnt << endl;
+	}
+	return 0;
 }
---
+++ random/graphs/movingToTheCapital.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	int n, m; cin >> n >> m;
+	vector<vector<int>> graph(n + 1), inv(n + 1);
+	int u, v;
+	while (m--) 
+		cin >> u >> v, graph[u].push_back(v);
+
+	queue<int> que;
+	vector<int> dist(n + 1, LLONG_MAX);
+
+	dist[1] = 0, que.push(1);
+	while (!que.empty()) {
+		int x = que.front(); que.pop();
+		for (int v : graph[x])
+			if (dist[v] > dist[x] + 1)
+				dist[v] = dist[x] + 1, que.push(v);
+	}
+
+	set<pair<int, int>> s;
+	for (int node = 1; node <= n; node++)
+		for (int v : graph[node])
+			if (dist[v] <= dist[node])
+				s.insert({dist[v], node});
+			else
+				inv[v].push_back(node);
+
+	for (auto [d, v] : s)
+		dist[v] = d;
+	while (!s.empty()) {
+		auto [d, node] = *s.begin();
+		s.erase(s.begin());
+		for (int v : inv[node])
+			if (dist[v] > d)
+				dist[v] = d, s.insert({d, v});
+	}
+
+	for (int r = 1; r <= n; r++)
+		cout << dist[r] << " ";
+	cout << endl;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
