e9ea816b165cbb5f5e9f44c978ecd4d5bc85b8db
neofytr <imagination12344321.com@gmail.com>
2026-02-16 21:00:01 +0530

    Solved a few problems

--- random/graphs/minimumMaximumDist.cpp
+++ random/graphs/minimumMaximumDist.cpp
@@ -6,33 +6,17 @@ using namespace std;
 void solve() {
 	int n, k; cin >> n >> k;
 
-	queue<pair<int, int>> que;
-	vector<int> dist(n + 1, 0);
-	vector<int> which(n + 1, -1);
-
+	set<pair<int, pair<int, int>>> que;
+	vector<int> dist(n + 1, LLONG_MIN);
 	int v, u;
 	for (int r = 1; r <= k; r++) 
-		cin >> v, dist[v] = 0, que.push({v, v}), which[v] = v;
+		cin >> v, dist[v] = 0, que.insert({0, {v, -1}});
 
 	vector<vector<int>> tree(n + 1);
 	for (int r = 1; r <= n - 1; r++)
 		cin >> u >> v, tree[u].push_back(v), tree[v].push_back(u);
 
-	while (!que.empty()) {
-		auto [x, p] = que.front(); que.pop();
-		for (int v : tree[x])
-			if (which[v] == -1) {
-				which[v] = p;
-				cout << v << " " << x << " " << p << endl;
-				dist[v] = dist[x] + 1;
-				que.push({v, p});
-			} else if (which[v] != p && dist[x] + 1 > dist[v])
-				dist[v] = dist[x] + 1, which[v] = p, que.push({v, p}), cout << v << " " << p << endl;
-	}
-
-	for (int r = 1; r <= n; r++)
-		cout << dist[r] << " ";
-	cout << endl;
+	vector<vector<int>> modi(n + 1);
 }
 
 int32_t main() {
