ca2e18b885716aa396907d1ca96d30ade7924756
neofytr <imagination12344321.com@gmail.com>
2026-02-17 13:00:01 +0530

    Solved some problems

--- random/1400/trinity.cpp
+++ random/1400/trinity.cpp
@@ -3,22 +3,17 @@ using namespace std;
 
 #define int long long
 
-void solve() {
+void solve(int t) {
 	int n; cin >> n;
 	vector<int> arr(n + 1);
 	for (int r = 1; r <= n; r++)
 		cin >> arr[r];
 
-	int cnt = 0;
 	sort(arr.begin() + 1, arr.end());
-	int a = arr[1], b = arr[2];
-	for (int r = n; r >= 3; r--)
-		if (arr[r] >= a + b) arr[r] = b, cnt++;
-	cout << cnt << endl;
 }
 
 int32_t main() {
 	int t; cin >> t;
-	while (t--) solve();
+	for (int r = 1; r <= t; r++) solve(r);
 	return 0;
 }
