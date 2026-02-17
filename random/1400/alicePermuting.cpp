625e1a6b80fa26c9b9ced2b624ccbcaa0a6894a5
neofytr <imagination12344321.com@gmail.com>
2026-02-17 17:00:01 +0530

    More problems solved

--- random/1400/alicePermuting.cpp
+++ random/1400/alicePermuting.cpp
@@ -5,6 +5,28 @@ using namespace std;
 
 void solve() {
 	int n, b, c; cin >> n >> b >> c;
+	if (!b) {
+		if (c >= n) {
+			cout << n << endl;
+			return;
+		}
+
+		if (c == n - 1 || c == n - 2) {
+			cout << n - 1 << endl;
+			return;
+		}
+
+		cout << -1 << endl;
+		return;
+	}
+
+	int left = max((2 * b - 1 - c) / b, 1ll);
+	int right = min((n + b - c - 1) / b, n);
+
+	if (right >= left)
+		cout << n - (right - left + 1) << endl;
+	else 
+		cout << n << endl;
 }
 
 int32_t main() {
---
+++ random/1400/bitwiseBalancing.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	int b, c, d; cin >> b >> c >> d;
+	int a = 0;
+	for (int bit = 0; bit <= 60; bit++) {
+		int bbit = ((b & (1ll << bit)) ? 1 : 0);
+		int cbit = ((c & (1ll << bit)) ? 1 : 0);
+		int dbit = ((d & (1ll << bit)) ? 1 : 0);
+
+		if (bbit) {
+			if (dbit)
+				a &= ~(1ll << bit);
+			else if (cbit)
+				a |= (1ll << bit);
+			else {
+				cout << -1 << endl;
+				return;
+			}
+		}
+	}
+	cout << a << endl;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
