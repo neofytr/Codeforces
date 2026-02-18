05ab1b704a84a57db7b9fb001eca281632be5933
neofytr <imagination12344321.com@gmail.com>
2026-02-18 21:00:01 +0530

    CF grind

--- random/1400/lightSwitches.cpp
+++ random/1400/lightSwitches.cpp
@@ -6,26 +6,24 @@ using namespace std;
 void solve() {
 	int n, k; cin >> n >> k;
 	vector<int> arr(n + 1);
-	for (int r = 1; r <= n; r++) cin >> arr[r];
 
-	sort(arr.begin() + 1, arr.end());
-	for (int r = 1; r <= n - 1; r++) {
-		int left = (k + arr[r + 1] - arr[r]) / (2 * k);
-		int right = (k - 1 + arr[r + 1] - arr[r]) / (2 * k);
-		if (left > right) {
-			cout << -1 << endl;
-			return;
-		}
+	int maxi = LLONG_MIN;
+	int left = 0, right = k - 1;
+	for (int r = 1; r <= n; r++) cin >> arr[r], maxi = max(maxi, arr[r]);
+	for (int i = 1; i <= n; i++) {
+		int r = (maxi - arr[i] + (k - 1)) % (2 * k);
+		int lft = (k - 1) - r;
+		int rght = (2 * k - 2 - r);
+		left = max(left, lft);
+		right = min(right, rght);
 	}
 
-	int mini = LLONG_MAX;
-	int m = (k + arr[n] - arr[n - 1]) / (2 * k);
-	for (int y = 0; y <= k - 1; y++) {
-		int x = arr[n - 1] + 2 * k * m + y - arr[n];
-		if (x >= 0 && x <= k - 1) mini = min(mini, arr[n] + x);
+	if (left > right) {
+		cout << -1 << endl;
+		return;
 	}
 
-	cout << mini << endl;
+	cout << maxi + left << endl;
 }
 
 int32_t main() {
