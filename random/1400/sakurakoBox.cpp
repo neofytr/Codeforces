9713b451e46f75daf65c647c45b140eea66e2d37
neofytr <imagination12344321.com@gmail.com>
2026-02-18 18:00:01 +0530

    Problem solving session

--- random/1400/sakurakoBox.cpp
+++ random/1400/sakurakoBox.cpp
@@ -22,7 +22,14 @@ void solve() {
 	int sum = 0;
 	vector<int> arr(n + 1);
 	for (int r = 1; r <= n; r++)
-		cin >> arr[r], sum += arr[r];
+		cin >> arr[r], sum = (sum + arr[r]) % MOD;
+
+	int res = 0;
+	for (int r = 1; r <= n; r++) 
+		res = (res + arr[r] * sum % MOD - arr[r] * arr[r] % MOD + MOD) % MOD;
+	
+	int inv = pw(n * (n - 1), MOD - 2);
+	cout << res * inv % MOD << endl;
 }
 
 int32_t main() {
