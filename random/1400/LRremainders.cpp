6009619d290671bf6ee46743489433ce3261b00e
neofytr <imagination12344321.com@gmail.com>
2026-02-21 15:00:01 +0530

    CF grind

--- random/1400/LRremainders.cpp
+++ random/1400/LRremainders.cpp
@@ -13,23 +13,25 @@ void build(int idx, int l, int r) {
 		return;
 	}
 
-	int m = (l + r) >> 1;
-	build(2 * idx, l, m);
-	build(2 * idx + 1, m + 1, r);
-	tree[idx] = (tree[2 * idx] * tree[2 * idx + 1]) % m;
+	int mid = (l + r) >> 1;
+	build(2 * idx, l, mid);
+	build(2 * idx + 1, mid + 1, r);
+
+	if (tree[2 * idx] == -1) tree[idx] = tree[2 * idx + 1];
+	else if (tree[2 * idx + 1] == -1) tree[idx] = tree[2 * idx];
+	else tree[idx] = (tree[2 * idx] * tree[2 * idx + 1]) % m;
 }
 
 int query(int ql, int qr, int l, int r, int idx) {
 	if (r < ql || l > qr) return -1;
 	if (l >= ql && r <= qr) return tree[idx];
 
-	int m = (l + r) >> 1;
-	int left = query(ql, qr, l, m, 2 * idx);
-	int right = query(ql, qr, m + 1, r, 2 * idx + 1);
+	int mid = (l + r) >> 1;
+	int left = query(ql, qr, l, mid, 2 * idx);
+	int right = query(ql, qr, mid + 1, r, 2 * idx + 1);
 
 	if (left == -1) return right;
 	if (right == -1) return left;
-
 	return (left * right) % m;
 }
 
@@ -37,8 +39,10 @@ void solve() {
 	cin >> n >> m;
 	for (int r = 1; r <= n; r++) cin >> arr[r];
 	string str; cin >> str;
+	for (int r = 1; r <= 4 * n; r++) tree[r] = -1;
 
 	int l = 1, r = n;
+	build(1, 1, n);
 	for (int c : str) {
 		cout << query(l, r, 1, n, 1) << " ";
 		if (c == 'L') l++;
