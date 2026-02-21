967816b6cc795b2d32d27d345cfe229dea054dca
neofytr <imagination12344321.com@gmail.com>
2026-02-21 13:00:01 +0530

    Problem solving session

---
+++ random/1400/ticketHoarding.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int unsigned long long
+
+void solve() {
+	int n, m, k; cin >> n >> m >> k;
+	vector<int> arr(n + 1);
+	for (int r = 1; r <= n; r++) cin >> arr[r];
+
+	int cnt = 0;
+	int left = k;
+	int last;
+	sort(arr.begin() + 1, arr.end());
+	for (int r = 1; r <= n; r++) {
+		if (left >= m) left -= m, cnt += (arr[r] - m) * (arr[r] - m);
+		else cnt += (arr[r] - left) * (arr[r] - left), left = 0;
+
+		if (!left) {
+			last = r;
+			break;
+		}
+	}
+	
+	int sum = 0;
+	for (int r = 1; r <= last; r++)
+		sum += arr[r] * arr[r];
+
+	cout << (k * k + sum - cnt) / 2 << endl;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
--- random/dp/miceAndHoles.cpp
+++ random/dp/miceAndHoles.cpp
@@ -2,62 +2,76 @@
 using namespace std;
 
 #define int long long
-#define MAX (int)(5 * 1e3)
-#define INF (int)(1e12)
+#define MAX 5000
+#define INF (int)1e12
 
-int dp[MAX + 1][MAX + 1];
 int x[MAX + 1];
-int cost[MAX + 1][MAX + 1];
 pair<int, int> h[MAX + 1];
 
-void solve() {
-	int n, m; cin >> n >> m;
-	for (int r = 1; r <= n; r++) cin >> x[r];
-	for (int r = 1; r <= m; r++)
-		cin >> h[r].first >> h[r].second;
-
-	sort(x + 1, x + n + 1);
-	sort(h + 1, h + m + 1);
-
-	for (int j = 1; j <= m; j++) {
-		cost[j][0] = 0; 
-		for (int i = 1; i <= n; i++)
-			cost[j][i] = cost[j][i - 1] + llabs(h[j].first - x[i]);
-	}
-
-	for (int j = 0; j <= m; j++)
-		for (int i = 0; i <= n; i++)
-			dp[j][i] = INF;
-
-	// dp[j][i] is the min cost to fill the first j holes with the first i mice
-	dp[0][0] = 0;
-	for (int i = 1; i <= n; i++) dp[0][i] = INF;
-	for (int j = 1; j <= m; j++) dp[j][0] = 0;
-
-	for (int j = 1; j <= m; j++) {
-		deque<int> dq;
-		for (int i = 1; i <= n; i++) {
-			dp[j][i] = dp[j - 1][i];
-			int left = i - min(i, h[j].second);
-			int right = i - 1;
-
-			while (!dq.empty() && dq.front() < left)
-				dq.pop_front();
-
-			int ins = dp[j - 1][right] - cost[j][right];
-			while (!dq.empty() && dp[j - 1][dq.back()] - cost[j][dq.back()] >= ins)
-				dq.pop_back();
-			dq.push_back(right);
-
-			dp[j][i] = min(dp[j][i], dp[j - 1][dq.front()] - cost[j][dq.front()]) + cost[j][i];
-		}
-	}
-
-	if (dp[m][n] >= INF) cout << -1 << endl;
-	else cout << dp[m][n] << endl;
-}
-
 int32_t main() {
-	solve();
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) cin >> x[i];
+    for (int i = 1; i <= m; i++)
+        cin >> h[i].first >> h[i].second;
+
+    sort(x + 1, x + n + 1);
+    sort(h + 1, h + m + 1);
+
+    // merge holes with same position
+    int nm = 0;
+    for (int i = 1; i <= m; i++) {
+        if (nm == 0 || h[nm].first != h[i].first)
+            h[++nm] = h[i];
+        else
+            h[nm].second += h[i].second;
+    }
+    m = nm;
+
+    vector<int> prev(n + 1, INF), cur(n + 1, INF);
+    vector<int> cost(n + 1, 0);
+
+    prev[0] = 0;
+
+    for (int j = 1; j <= m; j++) {
+        // build cost prefix for hole j
+        cost[0] = 0;
+        for (int i = 1; i <= n; i++)
+            cost[i] = cost[i - 1] + llabs(h[j].first - x[i]);
+
+        deque<int> dq;
+        cur[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            // option 1: don't use this hole
+            cur[i] = prev[i];
+
+            int left = i - min(i, h[j].second);
+            int right = i - 1;
+
+            while (!dq.empty() && dq.front() < left)
+                dq.pop_front();
+
+            int val = prev[right] - cost[right];
+            while (!dq.empty() &&
+                   prev[dq.back()] - cost[dq.back()] >= val)
+                dq.pop_back();
+            dq.push_back(right);
+
+            cur[i] = min(cur[i],
+                         prev[dq.front()] - cost[dq.front()] + cost[i]);
+        }
+
+        swap(prev, cur);
+        fill(cur.begin(), cur.end(), INF);
+    }
+
+    if (prev[n] >= INF) cout << -1 << '\n';
+    else cout << prev[n] << '\n';
+
+    return 0;
 }
