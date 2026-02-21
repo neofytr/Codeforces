fe56679cd5f1061ad48edcbf93db757f6577c8ec
neofytr <imagination12344321.com@gmail.com>
2026-02-21 20:00:01 +0530

    Practiced CF problems

---
+++ random/1400/XORdistance.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	int a, b, r; cin >> a >> b >> r;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
--- random/1400/findB.cpp
+++ random/1400/findB.cpp
@@ -4,13 +4,50 @@ using namespace std;
 #define int long long
 
 void solve() {
-	int n, q; cin >> n >> q;
+	int n, q; 
+	cin >> n >> q;
+
 	vector<int> arr(n + 1);
 	for (int r = 1; r <= n; r++) cin >> arr[r];
+
+	vector<int> ones(n + 1, 0), p(n + 1, 0);
+	for (int r = 1; r <= n; r++) {
+		ones[r] = ones[r - 1] + (arr[r] == 1);
+		p[r] = p[r - 1] + arr[r];
+	}
+
+	int l, r;
+	while (q--) {
+		cin >> l >> r;
+		if (r - l == 0) {
+			cout << "NO\n";
+			continue;
+		}
+
+		int o = ones[r] - ones[l - 1];
+		int s = (p[r] - p[l - 1]) - o;
+		s -= (r - l + 1 - o);
+
+		if (o > s) {
+			cout << "NO\n";
+			continue;
+		}
+
+		s += (r - l + 1 - o);
+		int len = r - l + 1 - o;
+
+		if (s == 2 * len && len < o) {
+			cout << "NO" << endl;
+			continue;
+		}
+
+		cout << "YES" << endl;
+	}
 }
 
 int32_t main() {
-	int t; cin >> t;
+	int t; 
+	cin >> t;
 	while (t--) solve();
 	return 0;
 }
