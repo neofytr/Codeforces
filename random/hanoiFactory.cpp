1a66aa0cf4f4ba3d95447b62b3f7e7469412550a
neofytr <imagination12344321.com@gmail.com>
2026-02-24 11:00:01 +0530

    Solved a few problems

---
+++ random/hanoiFactory.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MAX (int)(1e5)
+#define INF (int)(1e15)
+
+int tree[4 * MAX + 1];
+
+void build(int l = 0, int r = MAX, int idx = 1) {
+	tree[idx] = -INF;
+	if (l == r) return;
+	int m = (l + r) >> 1;
+	build(l, m, idx << 1), build(m + 1, r, (idx << 1) | 1);
+}
+
+int query(int elt, int l = 0, int r = MAX, int idx = 1) {
+	// [elt, MAX]
+	if (MAX < l || elt > r) return -INF;
+	if (l >= elt && r <= MAX) return tree[idx];
+
+	int m = (l + r) >> 1;
+	return max(query(elt, l, m, idx << 1), query(elt, m + 1, r, (idx << 1) | 1));
+}
+
+void update(int i, int val, int l = 0, int r = MAX, int idx = 1) {
+	if (l == r) {
+		tree[idx] = max(tree[idx], val);
+		return;
+	}
+
+	int m = (l + r) >> 1;
+	if (i <= m) update(i, val, l, m, idx << 1);
+	else update(i, val, m + 1, r, (idx << 1) | 1);
+	tree[idx] = max(tree[idx << 1], tree[(idx << 1) | 1]);
+}
+
+int32_t main() {
+	int n; cin >> n;
+	vector<tuple<int, int, int>> v(n + 1);
+	for (int r = 1; r <= n; r++) {
+		auto &[x, y, z] = v[r];
+		cin >> x >> y >> z;
+	}
+
+	sort(v.begin() + 1, v.end(), [](const tuple<int, int, int> &t1, const tuple<int, int, int> &t2) {
+		auto [a, x, b] = t1;
+		auto [c, y, d] = t2;
+		return x < y;
+	});
+
+	int i = 1;
+	unordered_map<int, int> f;
+	vector<int> vec;
+	f[get<1>(v[1])] = i++, vec.push_back(get<1>(v[1]));
+	for (int r = 2; r <= n; r++) {
+		auto [a, x, b] = v[r - 1];
+		auto [c, y, d] = v[r];
+		if (y != x) f[y] = i++, vec.push_back(y);
+	}
+
+	build();
+	vector<int> dp(n + 1);
+	dp[1] = get<2>(v[1]), update(f[get<1>(v[1])], dp[1]);
+	int maxi = dp[1];
+	for (int r = 2; r <= n; r++) {
+		auto [a, b, h] = v[r];
+		int i = upper_bound(vec.begin(), vec.end(), a) - vec.begin();
+		dp[r] = h;
+		if (i < vec.size()) j = max(j, query(f[vec[i]]));
+		dp[r] += j;
+		update(f[b], dp[r]);
+		maxi = max(maxi, dp[r]);
+	}
+
+	cout << maxi << endl;
+	return 0;
+}
