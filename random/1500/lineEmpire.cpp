44f419c6b7dcd540fc0197d815e0627434d68c9d
neofytr <imagination12344321.com@gmail.com>
2026-02-25 19:00:02 +0530

    CF grind

---
+++ random/1500/lineEmpire.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int dp[1000][1000];
+
+void solve() {
+	int n, a, b; cin >> n >> a >> b;
+	vector<int> x(n + 1);
+	for (int r = 1; r <= n; r++) cin >> x[r];
+
+	int cost = 0;
+	if (a >= b) {
+		for (int r = 1; r <= n; r++)
+			cost += b * x[r];
+
+		cout << cost << endl;
+		return;
+	}
+
+	int c = 0;
+	for (int r = 1; r <= n; r++)
+		cost += a * (x[r] - c), cost += b * (x[r] - c), c = x[r];
+
+	cout << cost << endl;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
