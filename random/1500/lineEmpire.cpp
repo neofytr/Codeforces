35a6bd56ed8b44ca92b6899f91014a3178aa7742
neofytr <imagination12344321.com@gmail.com>
2026-02-25 22:00:01 +0530

    Problem solving session

--- random/1500/lineEmpire.cpp
+++ random/1500/lineEmpire.cpp
@@ -10,7 +10,16 @@ void solve() {
 	vector<int> x(n + 1);
 	for (int r = 1; r <= n; r++) cin >> x[r];
 
-	
+	int c = 0;
+	int cost = b * x[1];
+	for (int r = 2; r <= n; r++) {
+		int shift = a * (x[r - 1] - x[c]) + b * (x[r] - x[r - 1]);
+		int dont = b * (x[r] - x[c]);
+		if (shift <= dont) c = r - 1;
+		cost += min(shift, dont);
+	}
+
+	cout << cost << endl;
 }
 
 int32_t main() {
