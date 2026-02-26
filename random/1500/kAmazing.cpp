196c336b1eebfc22aa184eec13edadbd07e1c0c5
neofytr <imagination12344321.com@gmail.com>
2026-02-26 13:00:01 +0530

    Problem solving session

---
+++ random/1500/kAmazing.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MAX (int)(3 * 1e5)
+
+int tree[4 * MAX + 1];
+int a[MAX + 1];
+
+void build(int l, int r, int idx) {
+	if (l == r) {
+		tree[idx] = a[l];
+		return;
+	}
+
+	int m = (l + r) >> 1;
+	build(l, m, idx << 1);
+	build(m + 1, r, (idx << 1) | 1);
+	tree[idx] = min(tree[idx << 1], tree[(idx << 1) | 1]);
+}
+
+int query(int ql, int qr, int l, int r, int idx) {
+	if (qr < l || ql > r) return LLONG_MAX;
+	if (l >= ql && r <= qr) return tree[idx];
+
+	int m = (l + r) >> 1;
+	return min(query(ql, qr, l, m, idx << 1), query(ql, qr, m + 1, r, (idx << 1) | 1));
+}
+
+void solve() {
+	int n; cin >> n;
+	for (int r = 1; r <= n; r++) cin >> a[r];
+
+	build(1, n, 1);
+	for (int k = 1; k <= n; k++) {
+		if (k < (n + 2) / 2) {
+			cout << -1 << " ";
+			continue;
+		}
+
+		cout << query(n - k + 1, k, 1, n, 1) << " ";
+	}
+	cout << endl;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
--- random/1500/mortalKombat.cpp
+++ random/1500/mortalKombat.cpp
@@ -2,22 +2,33 @@
 using namespace std;
 
 #define int long long
+#define INF (int)(1e18)
 
 void solve() {
 	int n; cin >> n;
 	vector<int> a(n + 1);
 	for (int r = 1; r <= n; r++) cin >> a[r];
 
-	bool allzero = true;
-	for (int r = 1; r <= n; r++)
-		if (a[r]) {
-			allzero = false;
-			break;
-		}
-	if (allzero) {
-		cout << 0 << endl;
+	if (n == 1) {
+		cout << a[1] << endl;
 		return;
 	}
+
+	// dp[i][0] is the minimum cost to kill the first i monsters with the ith monster
+	// killed by A
+	// dp[i][1] is the minimum cost to kill the first i monsters with the ith monster
+	// killed by B
+	vector<vector<int>> dp(n + 1, vector<int>(2));
+	dp[1][0] = a[1];
+	dp[1][1] = INF;
+	dp[2][0] = a[1] + a[2];
+	dp[2][1] = a[1];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i][1] = min(dp[i - 1][0], dp[i - 2][0]);
+		dp[i][0] = min(dp[i - 1][1] + a[i], dp[i - 2][1] + a[i - 1] + a[i]);
+	}
+	cout << min(dp[n][0], dp[n][1]) << endl;
 }
 
 int32_t main() {
