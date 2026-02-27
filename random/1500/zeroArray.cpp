72914c1ce37946ead8e7339efa694c29961b1bae
neofytr <imagination12344321.com@gmail.com>
2026-02-27 23:00:01 +0530

    Solved problem

--- random/1500/zeroArray.cpp
+++ random/1500/zeroArray.cpp
@@ -5,25 +5,7 @@ using namespace std;
 
 int32_t main() {
 	int n; cin >> n;
-	vector<int> a(n + 1);
-	for (int r = 1; r <= n; r++) cin >> a[r];
 
-	sort(a.begin() + 1, a.end());
-
-	int l = 1, r = n;
-	while (l < r) {
-		int sum = 0;
-		while (l < r && sum + a[l] <= a[r])
-			sum += a[l++];
-
-		if (l == r) {
-			if (sum == a[r]) cout << "YES" << endl;
-			else cout << "NO" << endl;
-			return;
-		}
-
-		if (sum == a[r])
-			l++, r--;
-	}
+	
 	return 0;
 }
