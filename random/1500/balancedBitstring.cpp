fa835d2de1cc07bbe4eb1e5ab600d9d06d6cbb96
neofytr <imagination12344321.com@gmail.com>
2026-02-27 17:00:01 +0530

    CF grind

--- random/1500/balancedBitstring.cpp
+++ random/1500/balancedBitstring.cpp
@@ -7,74 +7,32 @@ void solve() {
 	int n, k; cin >> n >> k;
 	string s; cin >> s;
 
-	vector<int> str(n + 1), ones(n + 2, 0), zeroes(n + 2, 0);
+	vector<int> a(n + 1), ones(n + 2, 0), zeroes(n + 2, 0);
 	for (int r = 1; r <= n; r++)  
-		if (s[r - 1] == '0') str[r] = 0;
-		else if (s[r - 1] == '1') str[r] = 1;
-		else str[r] = 2;
+		if (s[r - 1] == '0') a[r] = 0;
+		else if (s[r - 1] == '1') a[r] = 1;
+		else a[r] = 2;
 
-	int o = 0, z = 0;
-	int l = 1;
-	for (int r = 1; r <= n; r++) {
-		if (!str[r]) z++;
-		else if (str[r] == 1) o++;
-
-		while (r - l + 1 > k) {
-			if (!str[l]) z--;
-			else if (str[l] == 1) o--;
-			l++;
-		}
-
-		if (r - l + 1 == k) 
-			if (o > k / 2 || z > k / 2) {
+	int fz = 0, fo = 0;
+	for (int r = 1; r <= k; r++) {
+		bool z = false, o = false;
+		for (int i = r; i <= n; i += k)
+			if (!a[i] && o) {
 				cout << "NO" << endl;
 				return;
-			} else if (o == k / 2) {
-				zeroes[l] += 1;
-				zeroes[r + 1] -= 1;
-			} else if (z == k / 2) {
-				ones[l] += 1;
-				ones[r + 1] -= 1;
-			}
-	}
-
-	for (int r = 1; r <= n; r++) {
-		ones[r] += ones[r - 1];
-		zeroes[r] += zeroes[r - 1];
-
-		if (str[r] == 2 && ones[r] && zeroes[r]) {
-			cout << "NO" << endl;
-			return;
-		}
-	}
-
-	for (int r = 1; r <= n; r++) 
-		if (str[r] == 2) {
-			if (ones[r]) str[r] = 1;
-			else if (zeroes[r]) str[r] = 0;
-		}
-	
-	
-	o = z = 0;
-	l = 1;
-	for (int r = 1; r <= n; r++) {
-		if (!str[r]) z++;
-		else if (str[r] == 1) o++;
-
-		while (r - l + 1 > k) {
-			if (!str[l]) z--;
-			else if (str[l] == 1) o--;
-			l++;
-		}
-
-		if (r - l + 1 == k) 
-			if (o > k / 2 || z > k / 2) {
+			} else if (a[i] == 1 && z) {
 				cout << "NO" << endl;
 				return;
-			}
+			} else if (a[i] == 1 && !o) o = true;
+			else if (!a[i] && !z) z = true;
+
+		if (z) fz++;
+		if (o) fo++;
 	}
 
-	cout << "YES" << endl;
+	if (fz > k / 2 || fo > k / 2) 
+		cout << "NO" << endl;
+	else cout << "YES" << endl;
 	return;
 }
 
---
+++ random/1500/poweredAddition.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	int n; cin >> n;
+	vector<int> a(n + 1);
+	for (int r = 1; r <= n; r++) cin >> a[r];
+
+	priority_queue<pair<int, int>> pq;
+	for (int r = 1; r <= n; r++) pq.push({a[r], r});
+
+	int last = n;
+	int maxi = 0;
+	while (!pq.empty()) {
+		auto [elt, idx] = pq.top(); pq.pop();
+		if (idx > last) continue;
+
+		int mini = LLONG_MAX;
+		for (int r = idx; r <= last; r++) mini = min(mini, a[r]);
+
+		int d = elt - mini;
+		last = idx;
+		maxi = max(maxi, 64ll - __builtin_clzll(d));
+	}
+
+	cout << maxi << endl;
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
