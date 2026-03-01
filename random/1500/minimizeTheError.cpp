5881b827f93c50f648f9aeda08bc785d7a323e17
neofytr <imagination12344321.com@gmail.com>
2026-03-01 16:00:02 +0530

    Solved problem

--- random/1500/minimizeTheError.cpp
+++ random/1500/minimizeTheError.cpp
@@ -4,43 +4,30 @@ using namespace std;
 #define int long long
 #define MAX (int)(1e3)
 
-int n, k1, k2;
-int a[MAX + 1], b[MAX + 1];
-int dp[10][MAX + 1][MAX + 1];
-
+int A[MAX + 1], B[MAX + 1];
 int32_t main() {
-	cin >> n >> k1 >> k2;
-	for (int i = 1; i <= n; i++) cin >> a[i];
-	for (int i = 1; i <= n; i++) cin >> b[i];
+	int n, k1, k2; cin >> n >> k1 >> k2;
+	int t = k1 + k2;
 
-	for (int i = 1; i <= n; i++)
-		for (int j = 0; j <= k1; j++)
-			for (int k = 0; k <= k2; k++) {
-				dp[i][j][k] = dp[i - 1][j][k] + (a[i] - b[i]) * (a[i] - b[i]);
+	for (int r = 1; r <= n; r++) cin >> A[r];
+	for (int r = 1; r <= n; r++) cin >> B[r];
 
-				if (j >= 1)
-					dp[i][j][k] = min({dp[i][j][k], 
-					dp[i - 1][j - 1][k] + (a[i] + 1 - b[i]) * (a[i] + 1 - b[i]),
-					dp[i - 1][j - 1][k] + (a[i] - 1 - b[i]) * (a[i] - 1 - b[i])});
+	priority_queue<pair<int, int>> pq;
+	for (int r = 1; r <= n; r++)  
+		pq.push({llabs(2 * (A[r] - B[r])), r});
+	
 
-				if (k >= 1)
-					dp[i][j][k] = min({dp[i][j][k], 
-					dp[i - 1][j][k - 1] + (a[i] + 1 - b[i]) * (a[i] + 1 - b[i]),
-					dp[i - 1][j][k - 1] + (a[i] - 1 - b[i]) * (a[i] - 1 - b[i])});
+	while (t) {
+		auto [d, r] = pq.top(); pq.pop();
+		if (A[r] > B[r]) 
+			A[r]--, pq.push({llabs(2 * (A[r] - B[r])), r}), t--;
+		else  
+			A[r]++, pq.push({llabs(2 * (A[r] - B[r])), r}), t--;
+	}
 
-				if (j >= 1 && k >= 1)
-					dp[i][j][k] = min({dp[i][j][k], 
-					dp[i - 1][j - 1][k - 1] + (a[i] - b[i]) * (a[i] - b[i]),
-					dp[i - 1][j - 1][k - 1] + (a[i] - 2 - b[i]) * (a[i] - 2 - b[i]),
-					dp[i - 1][j - 1][k - 1] + (a[i] + 2 - b[i]) * (a[i]) + 2 - b[i]});
-			}
-
+	int res = 0;
 	for (int r = 1; r <= n; r++)
-		for (int j = 0; j <= k1; j++)
-			for (int k = 0; k <= k2; k++) {
-				cout << r << " " << j << " " << k << " " << dp[r][j][k] << endl;
-			}
-
-	cout << dp[n][k1][k2] << endl;
-	return 0;	
+		res += (A[r] - B[r]) * (A[r] - B[r]);
+	cout << res << endl;
+	return 0;
 }
