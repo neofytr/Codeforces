15d9b22194aa77589860a001efe638d22776b033
neofytr <imagination12344321.com@gmail.com>
2026-03-03 01:00:01 +0530

    Solved problem

--- random/1500/faultTolerant.cpp
+++ random/1500/faultTolerant.cpp
@@ -4,73 +4,46 @@ using namespace std;
 #define int long long
 
 void solve() {
-	int n; cin >> n;
-	vector<pair<int, int>> A, B;
-	vector<int> doneA(n + 1, false);
-	int v;
-	for (int r = 1; r <= n; r++)
-		cin >> v, A.push_back({v, r});
-	for (int r = 1; r <= n; r++)
-		cin >> v, B.push_back({v, r});
+    int n; 
+    cin >> n;
+    vector<int> a(n), b(n);
+    
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
 
-	int a1 = A[0].first, an = A[n - 1].first;
-	int b1 = B[0].first, bn = B[n - 1].first;
+    auto get_best = [&](int val, const vector<int>& arr) {
+        int best = 2e9; 
+        for (int x : arr) {
+            best = min(best, llabs(val - x));
+        }
+        return best;
+    };
 
-	int cost = 0;
-	sort(A.begin(), A.end()), sort(B.begin(), B.end());
-	for (auto &[b, i] : B) {
-		int j = lower_bound(A.begin(), A.end(), make_pair(b, 0ll)) - A.begin();
-		if (j >= n) j = n - 1;
+    int best_a1 = get_best(a[0], b);
+    int best_an = get_best(a[n - 1], b);
+    int best_b1 = get_best(b[0], a);
+    int best_bn = get_best(b[n - 1], a);
 
-		if (!j) {
-			auto [a, k] = A[j];
-			doneA[k] = true;
-			cost += llabs(a - b);
-		} else if (j == n - 1) {
-			auto [a, k] = A[j];
-			doneA[k] = true;
-			cost += llabs(a - b);
-		} else {
-			auto [t, r] = A[j];
-			auto [x, y] = A[j - 1];
+    int ans = best_a1 + best_an + best_b1 + best_bn;
 
-			if (llabs(b - t) < llabs(b - x)) {
-				doneA[r] = true;
-				cost += llabs(b - t);
-			} else {
-				doneA[y] = true;
-				cost += llabs(b - x);
-			}
-		}
-	}
+    ans = min(ans, llabs(a[0] - b[0]) + llabs(a[n - 1] - b[n - 1])); 
+    ans = min(ans, llabs(a[0] - b[n - 1]) + llabs(a[n - 1] - b[0]));
 
-	for (auto &[a, i] : A)
-		if (!doneA[i]) {
-			int j = lower_bound(B.begin(), B.end(), make_pair(a, 0ll)) - B.begin();
-			if (j >= n) j = n - 1;
-			if (!j) {
-				auto [b, k] = B[j];
-				cost += llabs(a - b);
-			} else if (j == n - 1) {
-				auto [b, k] = B[j];
-				cost += llabs(a - b);
-			} else {
-				auto [t, r] = B[j];
-				auto [x, y] = B[j - 1];
-	
-				if (llabs(a - t) < llabs(a - x)) 
-					cost += llabs(a - t);
-				else 
-					cost += llabs(a - x);
-			}
-			cout << i << " " << j << endl;
-		}
+    ans = min(ans, llabs(a[0] - b[0]) + best_an + best_bn);
+    ans = min(ans, llabs(a[n - 1] - b[n - 1]) + best_a1 + best_b1);
+    ans = min(ans, llabs(a[0] - b[n - 1]) + best_an + best_b1);
+    ans = min(ans, llabs(a[n - 1] - b[0]) + best_a1 + best_bn);
 
-	cout << min(cost, llabs(a1 - b1) + llabs(an - bn)) << endl;
+    cout << ans << '\n';
 }
 
 int32_t main() {
-	int t; cin >> t;
-	while (t--) solve();
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; 
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
