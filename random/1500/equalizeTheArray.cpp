9f902559b3be6ff3f317d958a0de5955cdd1dedd
neofytr <imagination12344321.com@gmail.com>
2026-03-02 21:00:02 +0530

    Problem solving session

--- random/1500/equalizeTheArray.cpp
+++ random/1500/equalizeTheArray.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 #define int long long
 
+void solve() {
+	int n; cin >> n;
+	unordered_map<int, int> f;
+
+	int v;
+	for (int r = 1; r <= n; r++) cin >> v, f[v]++;
+
+	int maxi = LLONG_MIN;
+	vector<int> vec(1);
+	for (auto &[e, c] : f) maxi = max(maxi, c), vec.push_back(c);
+
+	int sz = v.size() - 1;
+	int mini = LLONG_MAX;
+	sort(vec.begin() + 1, vec.end());
+	for (int c = 0; c <= maxi + 1; c++) {
+		int i = lower_bound(vec.begin() + 1, vec.end(), c) - vec.begin();
+	}
+}
+
 int32_t main() {
 	int t; cin >> t;
 	while (t--) solve();
+	return 0;
 }
