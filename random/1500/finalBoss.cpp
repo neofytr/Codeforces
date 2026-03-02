72eaef14c23c7f48a34bc989652e60c0d45391d3
neofytr <imagination12344321.com@gmail.com>
2026-03-02 15:00:02 +0530

    Solved a few problems

--- random/1500/finalBoss.cpp
+++ random/1500/finalBoss.cpp
@@ -6,10 +6,32 @@ using namespace std;
 void solve() {
 	int h, n; cin >> h >> n;
 	vector<int> a(n + 1), c(n + 1);
-	for (int r = 1; r <= n; r++) cin >> a[r];
-	for (int r = 1; r <= n; r++) cin >> c[r];
+	int mini = LLONG_MAX, cmini;
+	for (int r = 1; r <= n; r++) cin >> a[r], mini = min(mini, a[r]);
+	for (int r = 1; r <= n; r++) {
+		cin >> c[r];
+		if (a[r] == mini)	
+			cmini = c[r];
+	}
 
-	
+	auto can = [&a, &c, &n, &h] (int x) -> bool {
+		int t = 0;
+		for (int i = 1; i <= n; i++) {
+			t += ((x - 1) / c[i] + 1) * a[i];
+			if (t >= h) return true;
+		}
+		return false;
+	};
+
+	int left = 0;
+	int right = (h / mini) * cmini + 1;
+	while (right != left + 1) {
+		int mid = left + ((right - left) >> 1);
+		if (can(mid)) right = mid;
+		else left = mid;
+	}
+
+	cout << right << endl;
 }
 
 int32_t main() {
