90160d5be0390846b5b3bec9135add83d19b23e5
neofytr <imagination12344321.com@gmail.com>
2026-03-07 00:00:01 +0530

    More problems solved

--- random/1500/smaller.cpp
+++ random/1500/smaller.cpp
@@ -16,11 +16,41 @@ void solve() {
 			f[c - 'a']++;
 
 		if (d == 1)
-			for (int i = 0; i < 26; i++) s[i] += f[i];
+			for (int i = 0; i < 26; i++) s[i] += k * f[i];
 		else
-			for (int i = 0; i < 26; i++) t[i] += f[i];
+			for (int i = 0; i < 26; i++) t[i] += k * f[i];
 
-		int c = 0;
+
+		// find the highest character of t
+		int ht;
+		for (int i = 25; i >= 0; i--)
+			if (t[i]) {
+				ht = i;
+				break;
+			}
+
+		if (ht > 0) {
+			cout << "YES" << endl;
+			continue;
+		}
+
+		// now the highest character of t is 0 (a), which is also the 
+		// lowest character of s
+
+		// the string t is all a's
+		int lens = 0, lent = 0;
+		for (int i = 0; i < 26; i++) lens += s[i];
+		for (int i = 0; i < 26; i++) lent += t[i];
+
+		if (s[0] == lens) {
+			if (lens < lent) {
+				cout << "YES" << endl;
+			} else cout << "NO" << endl;
+			continue;
+		}
+
+		// s has a character other than a, t has all its characters a
+		cout << "NO" << endl;
 	}
 }
 
