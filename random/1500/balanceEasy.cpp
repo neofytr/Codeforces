2d447093cdc4dbef95ce30067da9d367af3df030
neofytr <imagination12344321.com@gmail.com>
2026-03-06 22:00:01 +0530

    Solved some problems

---
+++ random/1500/balanceEasy.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+	int q; cin >> q;
+	while (q--) {
+		string t; cin >> t;
+
+		if (t == "+") {
+			int x; cin >> x;
+
+		
+		} else {
+			int k; cin >> k;
+		}
+	}
+
+	return 0;
+}
--- random/1500/garage.cpp
+++ random/1500/garage.cpp
@@ -11,19 +11,18 @@ int32_t main() {
 		return 0;
 	}
 
-	auto cnt = [](int x) {
-		if (x < 3) return 0;
-
-		int res = 0;
-		for (int b = 1; (x - b * b) / (2 * b) >= 1; b++)
-			res += (x - b * b) / (2 * b);
-		return res;
-	};	
-
 	int left = 3;
 	int right = 1e12;
 	while (right != left + 1) {
 		int mid = left + (right - left) / 2;
+
+		int cnt = 0;
+		if (mid >= 5) cnt += ((mid - 5) / 4 + 1);
+		if (mid >= 3) cnt += ((mid - 3) / 4 + 1);
+		if (mid >= 8) cnt += ((mid - 8) / 4 + 1);
+		if (cnt < n) left = mid;
+		else right = mid;
+
 	}
 
 	cout << right << endl;
