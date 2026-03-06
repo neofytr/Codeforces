e850bee20d9970e3bf75215de7a06986c2b67977
neofytr <imagination12344321.com@gmail.com>
2026-03-06 18:00:01 +0530

    Practiced CF problems

---
+++ random/1500/garage.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+	int n; cin >> n;
+
+	int start = 1;
+	int a = 1;
+	while (true) {
+		// find the minimum x such that x^2 + 2 * a * x >= 2 * a + 2
+	}
+}
