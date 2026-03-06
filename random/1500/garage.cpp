76099b51ffa2f3006cbddb5ab26d5863d5503683
neofytr <imagination12344321.com@gmail.com>
2026-03-06 21:00:01 +0530

    CF grind

--- random/1500/garage.cpp
+++ random/1500/garage.cpp
@@ -6,9 +6,26 @@ using namespace std;
 int32_t main() {
 	int n; cin >> n;
 
-	int start = 1;
-	int a = 1;
-	while (true) {
-		// find the minimum x such that x^2 + 2 * a * x >= 2 * a + 2
+	if (n == 1) {
+		cout << 3 << endl;
+		return 0;
 	}
+
+	auto cnt = [](int x) {
+		if (x < 3) return 0;
+
+		int res = 0;
+		for (int b = 1; (x - b * b) / (2 * b) >= 1; b++)
+			res += (x - b * b) / (2 * b);
+		return res;
+	};	
+
+	int left = 3;
+	int right = 1e12;
+	while (right != left + 1) {
+		int mid = left + (right - left) / 2;
+	}
+
+	cout << right << endl;
+	return 0;
 }
