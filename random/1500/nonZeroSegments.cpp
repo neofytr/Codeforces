ca89f00eed61720c40dc0fefd83af1ba929581f5
neofytr <imagination12344321.com@gmail.com>
2026-03-16 16:00:01 +0530

    Solved a CF problem

--- random/1500/nonZeroSegments.cpp
+++ random/1500/nonZeroSegments.cpp
@@ -5,14 +5,28 @@ using namespace std;
 
 int32_t main() {
 	int n; cin >> n;
-	vector<int> a(n + 1);
+	vector<int> a(n + 1), p(n + 1, 0);
 	for (int i = 1; i <= n; i++) cin >> a[i];
+	for (int i = 1; i <= n; i++) p[i] = p[i - 1] + a[i];
 
-	int sum = 0, cnt = 0;
-	for (int i = 1; i <= n; i++) {
-		sum += a[i];
-		if (!sum)
-			cnt++, sum = a[i];
+	unordered_map<int, vector<int>> f;
+	f[0].push_back(0);
+	for (int i = 1; i <= n; i++) f[p[i]].push_back(i);
+	
+	int cnt = 0, i = 1, mini = LLONG_MAX;
+	while (i <= n) {
+		int target = p[i - 1];
+		if (!f[target].empty()) {
+			auto itr = lower_bound(f[target].begin(), f[target].end(), i);
+			if (itr != f[target].end())
+				mini = min(mini, *itr);
+		}
+
+		if (i == mini - 1) {
+			mini = LLONG_MAX;
+			cnt++;
+		}
+		i++;
 	}
 
 	cout << cnt << endl;
