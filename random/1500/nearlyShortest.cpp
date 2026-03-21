b50c97209450d2ad139db78db36819771cb992bc
neofytr <imagination12344321.com@gmail.com>
2026-03-21 10:00:02 +0530

    CF grind

--- random/1500/nearlyShortest.cpp
+++ random/1500/nearlyShortest.cpp
@@ -2,41 +2,18 @@
 using namespace std;
 
 #define int long long
+#define MOD (int)(1e9 + 7)
 
 void solve() {
 	int n; cin >> n;
 	string s; cin >> s;
 	vector<int> str(n + 1);
-	for (int i = 1; i <= n; i++) str[i] = s[i] - 'a';
-
-	vector<vector<int>> p(n + 1, vector<int>(26, 0));
-	for (int i = 1; i <= n; i++)
-		for (int j = 0; j < 26; j++)
-			p[i][j] = p[i - 1][j] + (str[i] == j);
-
-	auto check = [&](int x) -> bool {
-		int div = n / x;
-		int diff = 0;
-		for (int j = 0; j < 26; j++)
-			diff += llabs(p[x][j] * div - p[n][j]);
-		return diff <= 2;
-	};
-
-	vector<bool> done(n + 1, false);
-	for (int i = 1; i * i <= n; i++)
-		if (!(n % i)) {
-			done[i] = check(i), done[n / i] = check(n / i);
-		}
-
-	for (int i = 1; i <= n; i++)
-		if (done[i]) {
-			cout << i << endl;
-			return;
-		}
+	for (int i = 1; i <= n; i++) str[i] = s[i - 1] - 'a' + 1;
+		
 }
 
 int32_t main() {
 	int t; cin >> t;
 	while (t--) solve();
 	return 0;
-}
+} 
