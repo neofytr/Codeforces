42dc3016a86740204766df29c4c4794bfe77c934
neofytr <imagination12344321.com@gmail.com>
2026-03-21 12:00:01 +0530

    Practiced CF problems

--- random/1500/runningForGold.cpp
+++ random/1500/runningForGold.cpp
@@ -5,50 +5,49 @@ using namespace std;
 
 void solve() {
 	int n; cin >> n;
-	vector<vector<int>> r(n + 1, vector<int>(5, 0));
-	vector<int> one, two;
-	bool found = false, ans = -1;
+	vector<vector<int>> r(n + 1, vector<int>(6, 0)), v(6, vector<int>(n + 1, 0));
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= 5; j++)
+			cin >> r[i][j], v[j][i] = r[i][j];
+
+	for (int j = 1; j <= 5; j++) sort(v[j].begin() + 1, v[j].end());
+
+	vector<int> c;
 	for (int i = 1; i <= n; i++) {
 		int cnt = 0;
 		for (int j = 1; j <= 5; j++) {
-			cin >> r[i][j];
-			if (r[i][j] == 1) cnt++;
+			int rank = r[i][j];
+			int k = lower_bound(v[j].begin() + 1, v[j].end(), rank) - v[j].begin();
+			cnt += (n - k + 1);
 		}
 
-		if (cnt >= 3) {
-			found = true;
-			ans = i;
-		}
-		if (cnt == 2) two.push_back(i);
-		if (cnt == 1) one.push_back(i);
+		if (cnt >= 3 * n) c.push_back(i);
 	}
 
-	if (found) {
-		cout << ans << endl;
-		return;
-	}
+	for (int i : c) {
+		bool ok = true;
 
-	for (int i : two) {
-		vector<int> b(n + 1, 2);
-		b[i]++;
+		for (int k = 1; k <= n; k++) {
+			if (i == k) continue;
 
-		for (int j = 1; j <= 5; j++)
-			if (r[i][j] != 1)
-				for (int k = 1; k <= n; k++)
-					if (r[i][j] < r[k][j]) b[k]++;
-
-		bool done = true;
-		for (int j = 1; j <= n; j++)
-			if (b[j] < 3) {
-				done = false;
-				break;
+			int better = 0;
+			for (int j = 1; j <= 5; j++) {
+				if (r[i][j] < r[k][j]) better++;
 			}
 
-		if (done) {
+			if (better < 3) {
+				ok = false;
+				break;
+			}
+		}
+
+		if (ok) {
 			cout << i << endl;
 			return;
 		}
 	}
+
+	cout << -1 << endl;
 }
 
 int32_t main() {
