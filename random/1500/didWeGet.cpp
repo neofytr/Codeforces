e800f8194980c95a886be1f956672ea94d91649c
neofytr <imagination12344321.com@gmail.com>
2026-03-24 07:00:01 +0530

    Solved problem

---
+++ random/1500/didWeGet.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N_MAX (26)
+#define M_MAX (1000)
+
+int dp[M_MAX][N_MAX];
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
