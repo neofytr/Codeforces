57f21207d08db878f1b095c57b2a054d91390314
neofytr <imagination12344321.com@gmail.com>
2026-04-02 11:00:01 +0530

    CF grind

--- random/1500/tenzingBalls.cpp
+++ random/1500/tenzingBalls.cpp
@@ -2,11 +2,19 @@
 using namespace std;
 
 #define int long long
+#define INF (int)(1e16)
 
 void solve() {
 	int n; cin >> n;
 	vector<int> a(n + 1);
 	for (int i = 1; i <= n; i++) cin >> a[i];
+
+	vector<int> dp(n + 1, -INF), m(n + 1, -INF);
+	dp[1] = 0, m[a[1]] = 0 - 1;
+	for (int i = 2; i <= n; i++) 
+		dp[i] = max(dp[i - 1], m[a[i]] + i + 1), m[a[i]] = max(m[a[i]], dp[i - 1] - i);
+
+	cout << dp[n] << endl;	
 }
 
 int32_t main() {
