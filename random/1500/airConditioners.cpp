19b536a4b013c4611487c9a8e718f0f65dee0a55
neofytr <imagination12344321.com@gmail.com>
2026-04-02 13:00:01 +0530

    Worked on Codeforces problems

---
+++ random/1500/airConditioners.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MAX (int)(3 * 1e5)
+
+int tree[4 * MAX + 1];
+
+int query(int ql, int qr, int l, int r, int idx) {
+	if (qr < l || r < ql) return LLONG_MAX;
+	if (l >= ql && r <= qr) return tree[idx];
+
+	int m = (l + r) >> 1;
+	return min(query(ql, qr, l, m, idx << 1), query(ql, qr, m + 1, r, (idx << 1) | 1));
+}
+
+void solve() {
+	int n, k; cin >> n >> k;
+	vector<pair<int, int>> a(k + 1, LLONG_MAX);
+	for (int i = 1; i <= k; i++) cin >> a[i].first;
+	for (int i = 1; i <= k; i++) cin >> a[i].second;
+
+	sort(a.begin() + 1, a.end());
+	vector<int> v1(k + 1), v2(k + 1);
+	for (int i = 1; i <= k; i++)
+		v1[i] = a[i].second + a[i].first, v2[i] = a[i].second - a[i].first;
+	for (int i = 1; i <= 4 * k; i++) tree[i] = LLONG_MAX;
+
+	for (int i = 1; i <= n; i++) {
+		int s = lower_bound(a.begin() + 1, a.end(), {i, 0ll});
+	}
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
