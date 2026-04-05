de313664952577af6f19587b1d0126820d334b44
neofytr <imagination12344321.com@gmail.com>
2026-04-05 15:00:01 +0530

    Worked on Codeforces problems

---
+++ random/1500/killjoy.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+void solve() {
+	int n, x; cin >> n >> x;
+	vector<int> r(n + 1);
+	for (int i = 1; i <= n; i++) cin >> r[i];
+}
+
+int32_t main() {
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
