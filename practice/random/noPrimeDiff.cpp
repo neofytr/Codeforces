6aad2fce26954c1df0302523a6f6e17964954e3b
neofytr <imagination12344321.com@gmail.com>
2026-05-19 12:00:02 +0530

    CF grind

--- practice/random/noPrimeDiff.cpp
+++ practice/random/noPrimeDiff.cpp
@@ -8,6 +8,18 @@ vector<bool> nprime(MAX + 1);
 
 void solve() {
 	int n, m; cin >> n >> m;
+	vector<vector<int>> mat(n + 1, vector<int>(m + 1));
+	if (nprime[n]) {
+		int s = 1;
+		for (int c = 1; c <= m; c++)
+			for (int r = 1; r <= n; r++)
+				mat[r][c] = s++;
+	} else if (nprime[m]) {
+		int s = 1;
+		for (int r = 1; r <= n; r++)
+			for (int c = 1; c <= m; c++)
+				mat[r][c] = s++;
+	}
 }
 
 int32_t main() {
