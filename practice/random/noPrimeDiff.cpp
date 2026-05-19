f47ac22eae937e462cc67849a9363d72e01aa730
neofytr <imagination12344321.com@gmail.com>
2026-05-19 18:00:01 +0530

    Practiced CF problems

--- practice/random/noPrimeDiff.cpp
+++ practice/random/noPrimeDiff.cpp
@@ -19,14 +19,33 @@ void solve() {
 		for (int r = 1; r <= n; r++)
 			for (int c = 1; c <= m; c++)
 				mat[r][c] = s++;
+	} else {
+		// both n and m are prime in this case (n, m >= 4)
+		// so, if n is prime, then n - 1 >= 3 and is thus composite
+
+		vector<int> p;
+		for (int i = 1; i <= n; i += 2) p.push_back(i);
+		for (int i = 2; i <= n; i += 2) p.push_back(i);
+		
+		int cur = 1;
+		for (int r : p) 
+			for (int c = 1; c <= m; c++) 
+				mat[r][c] = cur++;
 	}
+
+	for (int r = 1; r <= n; r++) {
+		for (int c = 1; c <= m; c++)
+			cout << mat[r][c] << " ";
+		cout << endl;
+	}
+	cout << endl;
 }
 
 int32_t main() {
 	nprime[0] = nprime[1] = true;
 	for (int i = 2; i <= MAX; i++)
 		if (!nprime[i])
-			for (int j = i * i; j <= MAX; j++)
+			for (int j = i * i; j <= MAX; j += i)
 				nprime[j] = true;
 	int t; cin >> t;
 	while (t--) solve();
