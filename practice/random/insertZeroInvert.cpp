0ac488caa1b10c010d5927dcc4c5dd4cecb335e8
neofytr <imagination12344321.com@gmail.com>
2026-05-20 16:00:01 +0530

    Solved problem

--- practice/random/insertZeroInvert.cpp
+++ practice/random/insertZeroInvert.cpp
@@ -2,11 +2,35 @@
 using namespace std;
 
 #define int long long
+#define endl "\n"
 
 void solve() {
 	int n; cin >> n;
-	vector<int> a(n + 1);
+	vector<int> a(n + 1), p(n + 1);
 	for (int i = 1; i <= n; i++) cin >> a[i];
+
+	if (a[n] == 1) {
+		cout << "NO" << endl;
+		return;
+	}
+
+	int i = n; 
+	while (i >= 0) {
+		if (!a[i]) {
+			p[n - i + 1] = 0;
+			i--;
+			continue;
+		}
+
+		int j = i;
+		while (j >= 0 && a[j])
+			j--;
+
+		int len = i - j;
+
+
+		i = j;
+	}
 }
 
 int32_t main() {
