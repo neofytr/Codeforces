b5e1c492648500f8ad94ca73097b0c4bd21267bf
neofytr <imagination12344321.com@gmail.com>
2026-05-21 14:00:01 +0530

    Problem solving session

--- practice/random/rudolfTree.cpp
+++ practice/random/rudolfTree.cpp
@@ -5,28 +5,15 @@ using namespace std;
 
 void solve() {
 	int n, d, h; cin >> n >> d >> h;
-	vector<int> y(n + 1), p(n + 1, 0);
+	vector<int> y(n + 1);
 	for (int i = 1; i <= n; i++) cin >> y[i];
-	for (int i = 1; i <= n; i++) p[i] = p[i - 1] + y[i];
 
-	double area = 0;
-	for (int i = 1; i <= n; i++) {
-		area += 0.5 * d * h;
+	double area = 0.5 * d * h;
+	for (int i = 1; i <= n - 1; i++) 
+		if (y[i + 1] >= y[i] + h) area += 0.5 * d * h;
+		else area += 0.5 * (d + (((double)d) / h) * (y[i] + h - y[i + 1])) * (y[i + 1] - y[i]);
 
-		int low = y[i] + 1, high = y[i] + h - 1;
-		int l = lower_bound(y.begin() + 1, y.end(), low) - y.begin();
-		int r = lower_bound(y.begin() + 1, y.end(), high + 1) - y.begin();
-		if (r - 1 < l) continue;
-
-		r--;
-		int num = r - l + 1;
-		int overlap = (0.5 * d * (y[i] + h) * num - 0.5 * d * (p[r] - p[l - 1])); 
-		cout << l << " " << r << endl;
-		cout << i << " " << overlap << endl;
-		area -= overlap; 
-	}
-
-	cout << area << endl;
+	cout << setprecision(24) << area << endl;
 }
 
 int32_t main() {
