fd77798048951f302e5dce9801860cd2b22844fd
neofytr <imagination12344321.com@gmail.com>
2026-05-22 16:00:01 +0530

    CF grind

--- practice/random/subXor.cpp
+++ practice/random/subXor.cpp
@@ -12,26 +12,59 @@ struct Node {
 		for (int i = 0; i < BIT; i++) arr[i] = nullptr;
 		cnt = 0;
 	}
-}
+};
 
-void _insert(Node *node, int num, int bit) {
-	if (bit == 31) {
-		node->cnt = 1;
-	}
-	int c = (num >> bit) & 1;
-	if (!node->arr[c]) node->arr[c] = new Node();
-}
+Node *root;
 
 void insert(int num) {
-	_insert(root, num, 30);
+	Node *curr = root;
+	for (int bit = 30; bit >= 0; bit--) {
+		int c = (num >> bit) & 1;
+		if (!curr->arr[c]) curr->arr[c] = new Node();
+		curr->arr[c]->cnt++;
+		curr = curr->arr[c];
+	}
 }
 
 void solve() {
 	int n, k; cin >> n >> k;
-	vector<int> a(n + 1);
-	for (int i = 1; i <= n; i++) cin >> a[i];
+	vector<int> a(n + 1), p(n + 1, 0);
+	for (int i = 1; i <= n; i++) cin >> a[i], p[i] = p[i - 1] ^ a[i];
 
+	root = new Node(); root->cnt = 1;
+	insert(p[0]);
 
+	int cnt = 0; k--;
+	for (int i = 1; i <= n; i++) {
+		Node *curr = root; 
+		bool ok = true;
+		for (int bit = 30; bit >= 0; bit--) {
+			if (!((k >> bit) & 1)) {
+				int want = (p[i] >> bit) & 1;
+				if (!curr->arr[want]) {
+					ok = false;
+					break;
+				}
+				curr = curr->arr[want];
+			} else {
+				int one = !((p[i] >> bit) & 1);
+				int zero = (p[i] >> bit) & 1;
+
+				if (curr->arr[zero]) 
+					cnt += curr->arr[zero]->cnt;
+				if (!curr->arr[one]) {
+					ok = false;
+					break;
+				}
+
+				curr = curr->arr[one];
+			}
+		}
+		if (ok) cnt += curr->cnt;
+		insert(p[i]);
+	}
+
+	cout << cnt << endl;
 }
 
 int32_t main() {
