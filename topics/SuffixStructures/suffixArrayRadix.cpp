b462e53375b13067d72b0ce2a05191c964d0f8a9
neofytr <imagination12344321.com@gmail.com>
2026-05-25 21:00:01 +0530

    Solved a CF problem

--- topics/SuffixStructures/suffixArrayRadix.cpp
+++ topics/SuffixStructures/suffixArrayRadix.cpp
@@ -5,72 +5,19 @@ using namespace std;
 
 vector<int> build_sa(string &s) {
 	int n = s.length();
+	vector<int> sa(n + 1), rank(n + 1), tmp(n + 1), t(n + 1), cnt(n + 1);
 
-	vector<int> sa(n + 1), rank(n + 1), tmp(n + 1);
-	vector<pair<char, int>> vals;
-	for (int i = 1; i <= n; i++)
-		vals.push_back({s[i - 1], i});
-	sort(vals.begin(), vals.end());
-
-	int r = 1;
-	rank[vals[0].second] = r;
-	for (int i = 1; i < n; i++) {
-		if (vals[i].first != vals[i - 1].first)
-			r++;
-		rank[vals[i].second] = r;
-	}
-
-	for (int i = 0; i < n; i++)
-		sa[i + 1] = vals[i].second;
 
 	for (int k = 1; k < n; k <<= 1) {
-		auto cmp = [&](int a, int b) {
-			if (rank[a] != rank[b])
-				return rank[a] < rank[b];
-
-			int ra = (a + k <= n ? rank[a + k] : 0);
-			int rb = (b + k <= n ? rank[b + k] : 0);
-			return ra < rb;
-		};
-
-		vector<int> cntrank(n + 1, 0), startrank(n + 1, 0);
-		for (int suf = 1; suf <= n; suf++)
-			cntrank[((suf + k <= n) ? rank[suf + k] : 0)]++;
-
-		startrank[0] = 1;
-		for (int r = 1; r <= n; r++)
-			startrank[r] = startrank[r - 1] + cntrank[r - 1];
-
-		for (int suf = 1; suf <= n; suf++) {
-			int r = (suf + k <= n ? rank[suf + k] : 0);
-			sa[startrank[r]++] = suf;
-		}
-
-		for (int i = 0; i <= n; i++) 
-			cntrank[i] = startrank[i] = 0;
-
-		for (int i = 1; i <= n; i++) {
-			int suf = sa[i];
-			cntrank[rank[suf]]++;
-		}
-
-		startrank[0] = 1;
-		for (int r = 1; r <= n; r++)
-			startrank[r] = startrank[r - 1] + cntrank[r - 1];
-		for (int i = 1; i <= n; i++) {
-			int suf = sa[i]; int r = rank[suf];
-			tmp[startrank[r]++] = suf;
-		}
-		swap(tmp, sa);
-
-		tmp[sa[1]] = 1;
-		for (int i = 2; i <= n; i++)
-			tmp[sa[i]] = tmp[sa[i - 1]] + cmp(sa[i - 1], sa[i]);
-		swap(tmp, rank);
+		int ptr = 1;
+		for (int i = n - k + 1; i <= n; i++)
+			t[ptr++] = i;
+		for (int i = 1; i <= n; i++)
+			if (sa[i] - k >= 1)
+				t[ptr++] = sa[i] - k;
+		fill(count.begin(), count.end(), 0);
+		for (int i = 1; i <= n; i++)
 	}
-
-	sa[0] = n;
-	for (int i = 1; i <= n; i++) --sa[i];
 	return sa;
 }
 
