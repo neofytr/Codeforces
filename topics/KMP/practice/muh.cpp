3f6aea856b6a1caec8c3c029909e5d8f43ff7bfa
neofytr <imagination12344321.com@gmail.com>
2026-06-28 12:00:01 +0530

    Solved a CF problem

--- topics/KMP/practice/muh.cpp
+++ topics/KMP/practice/muh.cpp
@@ -11,6 +11,31 @@ int32_t main() {
 	for (int i = 1; i <= w; i++)
 		cin >> b[i];
 
-	map<int, int> f;
+	vector<int> p(w + 1, 0);
+	int j = 0;
+	for (int i = 2; i <= w; i++) {
+		while (j > 0 && b[j + 1] != b[i])
+			j = p[j];
+		if (b[j + 1] == b[i])
+			++j;
+		p[i] = j;
+	}
+
+	int x = 0;
+	j = 0;
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		while (j > 0 && b[j + 1] + x != a[i])
+			j = p[j];
+		if (b[j + 1] + x == a[i])
+			++j;
+		if (!j) 
+			j = 1, x = a[i] - b[j];
+		cout << i << " " << j << " " << x << endl;
+		if (j == w)
+			j = p[j], cnt++;
+	}
+
+	cout << cnt << endl;
 	return 0;
 }
