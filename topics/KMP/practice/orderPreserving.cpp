595f30e80f6034158019b1078b8daaa04389bead
neofytr <imagination12344321.com@gmail.com>
2026-07-02 11:00:02 +0530

    Worked on Codeforces problems

---
+++ topics/KMP/practice/orderPreserving.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+	int n; cin >> n;
+	vector<int> cnt(n + 1);
+	for (int i = 1; i <= n; i++)
+		cin >> cnt[i];
+	return 0;
+}
