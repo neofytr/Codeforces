67227ff63fe744cfc9da87173156fe60522e4a51
neofytr <imagination12344321.com@gmail.com>
2026-07-03 18:00:01 +0530

    CF grind

--- topics/KMP/practice/mesTransError.cpp
+++ topics/KMP/practice/mesTransError.cpp
@@ -6,7 +6,3 @@ using namespace std;
 int32_t main() {
 
 }
-
-we have to put A3 on A4 when we print the cheroku (there is a tool for that); we can get exact dimensions too;
-the view is 400 by 300 (length 400 mm, widht 300 mm); we need to fit the cheroku in there but not the whole
-view, i should be able to configure the size
