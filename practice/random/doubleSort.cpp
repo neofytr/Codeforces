8397d510e6b2c965654711a4b6e6e11fbe9b35f4
neofytr <imagination12344321.com@gmail.com>
2026-09-02 16:00:01 +0530

    Practiced CF problems

--- practice/random/doubleSort.cpp
+++ practice/random/doubleSort.cpp
@@ -4,22 +4,72 @@ using namespace std;
 #define int long long
 
 void solve() {
-	int n; cin >> n;
-	vector<pair<int, int>> a(n + 1), b(n + 1);
-	for (int i = 1; i <= n; i++)
-		cin >> a[i].first, a[i].second = i;
-	for (int i = 1; i <= n; i++)
-		cin >> b[i].first, b[i].second = i;
-	sort(a.begin() + 1, a.end()), sort(b.begin() + 1, b.end());
-	for (int i = 1; i <= n; i++)
-		if (a[i].second != b[i].second) {
-			cout << -1 << endl;
-			return;
-		}
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n);
+
+    for (int &x : a) cin >> x;
+    for (int &x : b) cin >> x;
+
+    vector<int> sa = a;
+    vector<int> sb = b;
+
+    sort(sa.begin(), sa.end());
+    sort(sb.begin(), sb.end());
+
+    vector<pair<int, int>> original, target;
+
+    for (int i = 0; i < n; i++) {
+        original.push_back({a[i], b[i]});
+        target.push_back({sa[i], sb[i]});
+    }
+
+    sort(original.begin(), original.end());
+    sort(target.begin(), target.end());
+
+    if (original != target) {
+        cout << -1 << '\n';
+        return;
+    }
+
+    vector<pair<int, int>> moves;
+
+    for (int i = 0; i < n; i++) {
+        int pos = i;
+
+        while (pos < n &&
+               !(a[pos] == sa[i] && b[pos] == sb[i])) {
+            pos++;
+        }
+
+        while (pos > i) {
+            swap(a[pos], a[pos - 1]);
+            swap(b[pos], b[pos - 1]);
+
+            moves.push_back({pos + 1, pos});
+
+            pos--;
+        }
+    }
+
+    cout << moves.size() << '\n';
+
+    for (auto [x, y] : moves) {
+        cout << x << ' ' << y << '\n';
+    }
 }
 
 int32_t main() {
-	int t; cin >> t;
-	while(t--) solve();
-	return 0;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
 }
