087404ee26b635efea01cec927dc3db026e61f28
neofytr <imagination12344321.com@gmail.com>
2026-09-03 11:00:01 +0530

    More problems solved

--- practice/random/binaryDequeue.cpp
+++ practice/random/binaryDequeue.cpp
@@ -5,21 +5,22 @@ using namespace std;
 
 void solve() {
 	int n, s; cin >> n >> s;
-	vector<int> a(n + 1);
+	vector<int> a(n + 1, 0);
 	for (int i = 1; i <= n; i++)
 		cin >> a[i];
 
-	vector<int> pref(n + 1, 0), suff(n + 2, 0);
-	for (int i = 1;  i <= n; i++)
-		pref[i] += pref[i - 1] + arr[i];
-	for (int i = n; i >= 1; i--)
-		suff[i] += suff[i + 1] + arr[i];
-
-	vector<int> p = pref, sf = suff;
-	sort(p.begin(), p.end()), sort(sf.begin(), sf.end() - 1);
-	for (int i = 0; i <= n; i++) {
-		int target = s - p[i];
+	int sum = 0;
+	int j = 1;
+	int maxi = LLONG_MIN;
+	for (int i = 1; i <= n; i++) {
+		sum += a[i];
+		while (j <= i && sum > s)
+			sum -= a[j], j++;
+		if (j <= i && sum == s)
+			maxi = max(maxi, i - j + 1);
 	}
+
+	cout << (maxi == LLONG_MIN ? -1 : n - maxi) << endl;
 }
 
 int32_t main() {
